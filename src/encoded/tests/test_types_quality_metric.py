c4258254927ce545d53fa38154fa80be0bf73590
William Ronchetti <wrr33@cornell.edu>
2019-10-07 09:41:02 -0400

    adding some more tests

--- src/encoded/tests/test_types_quality_metric.py
+++ src/encoded/tests/test_types_quality_metric.py
@@ -3,6 +3,39 @@ pytestmark = [pytest.mark.setone, pytest.mark.working, pytest.mark.schema]
 
 
 @pytest.fixture
+def qc_metric_fastqc(institution, project):
+    return {
+        'institution': institution['@id'],
+        'project': project['@id'],
+        'Total Sequences': 123456,
+        'Sequences flagged as poor quality': 123,
+        'Per base sequence quality': 'PASS'
+    }
+
+
+@pytest.fixture
+def qc_metric_fastqc_no_project(institution):
+    return {
+        'institution': institution['@id'],
+        'project': 'not_a_project',
+        'Total Sequences': 123456,
+        'Sequences flagged as poor quality': 123,
+        'Per base sequence quality': 'PASS'
+    }
+
+
+@pytest.fixture
+def qc_metric_fastqc_bad_status(institution, project):
+    return {
+        'institution': institution['@id'],
+        'project': project['@id'],
+        'Total Sequences': 123456,
+        'Sequences flagged as poor quality': 123,
+        'Per base sequence quality': 'OOF'
+    }
+
+
+@pytest.fixture
 def qc_bamcheck_data1(institution, project):
     return {
         "uuid": "af8e47c1-35bd-46fd-8a2e-e5d7b89560aa",
@@ -22,6 +55,16 @@ def qc_bamcheck_data2(institution, project):
         "number_of_lines": 1234568,
         "quickcheck": " not OK"
     }
+
+
+def test_post_qc_metric(testapp, qc_metric_fastqc):
+    res = testapp.post_json('/quality_metric_fastqc', qc_metric_fastqc, status=201)
+    assert res.json['@graph'][0]['overall_quality_status'] == "PASS"
+
+
+def test_post_bad_qc(testapp, qc_metric_fastqc_bad_status, qc_metric_fastqc_no_project):
+    testapp.post_json('/quality_metric_fastqc', qc_metric_fastqc_bad_status, status=422)
+    testapp.post_json('/quality_metric_fastqc', qc_metric_fastqc_no_project, status=422)
 
 
 def test_overall_quality_pass(testapp, qc_bamcheck_data1):
---
+++ src/encoded/tests/test_types_sample.py
@@ -0,0 +1,83 @@
+import pytest
+pytestmark = [pytest.mark.setone, pytest.mark.working, pytest.mark.schema]
+
+@pytest.fixture
+def MIndividual(testapp, project, institution):
+    ind = {
+        'project': 'encode-project',
+        'institution': 'encode-institution',
+        'sex': 'M'
+    }
+    return testapp.post_json('/individual', ind, status=201).json['@graph'][0]
+
+
+
+@pytest.fixture
+def WIndividual(testapp, project, institution):
+    ind = {
+        'project': 'encode-project',
+        'institution': 'encode-institution',
+        'sex': 'F'
+    }
+    return testapp.post_json('/individual', ind, status=201).json['@graph'][0]
+
+
+@pytest.fixture
+def sample_one(project, institution, MIndividual):
+    return {
+        'project': project['@id'],
+        'institution': institution['@id'],
+        'individual': MIndividual['@id'],
+        'specimen_type': 'blood',
+        'date_received': '2018-12-1'
+    }
+
+
+@pytest.fixture
+def sample_two(project, institution, WIndividual):
+    return {
+        'project': project['@id'],
+        'institution': institution['@id'],
+        'individual': WIndividual['@id'],
+        'specimen_type': 'saliva',
+        'date_received': '2015-12-7'
+    }
+
+
+@pytest.fixture
+def sample_invalid_specimen_type(project, institution, MIndividual):
+    return {
+        'project': project['@id'],
+        'institution': institution['@id'],
+        'individual': MIndividual['@id'],
+        'specimen_type': 'skin',
+        'date_received': '2015-12-7'
+    }
+
+
+@pytest.fixture
+def sample_no_project(institution, MIndividual):
+    return {
+        'project': 'does not exist',
+        'institution': institution['@id'],
+        'individual': MIndividual['@id'],
+        'specimen_type': 'tissue',
+        'date_received': '2015-12-7'
+    }
+
+
+def test_post_valid_samples(testapp, sample_one, sample_two):
+    testapp.post_json('/sample', sample_one, status=201)
+    testapp.post_json('/sample', sample_two, status=201)
+
+
+def test_post_invalid_samples(testapp, sample_invalid_specimen_type, sample_no_project):
+    testapp.post_json('/sample', sample_no_project, status=422)
+    testapp.post_json('/sample', sample_invalid_specimen_type, status=422)
+
+
+def test_post_valid_patch_error(testapp, sample_one):
+    res = testapp.post_json('/sample', sample_one, status=201).json['@graph'][0]
+    testapp.patch_json(res['@id'], {'date_received': '12-3-2003'}, status=422)
+    testapp.patch_json(res['@id'], {'project': 'does_not_exist'}, status=422)
+    testapp.patch_json(res['@id'], {'specimen_type': 'hair'}, status=422)
