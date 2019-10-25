892caa58331f8138aff538edf0aa280d27a72bc4
William Ronchetti <wrr33@cornell.edu>
2019-10-25 15:51:11 -0400

    remaining parts of part 1 + tests

--- src/encoded/commands/build_annotation_schema.py
+++ src/encoded/commands/build_annotation_schema.py
@@ -1,8 +1,9 @@
 import csv
 import sys
+import six
 import json
 import logging
-from collections import OrderedDict
+from collections import OrderedDict, Mapping
 
 logger = logging.getLogger(__name__)
 
@@ -283,6 +284,202 @@ def build_annotation_field_schema(fields):
     return json.dumps(schema)
 
 
+def process_inserts(fname, fields):
+    """
+        Processes the annotation fields in the mapping table to produce inserts
+        Creates a list of inserts
+        Currently should create 887 inserts based on latest MP
+    """
+    inserts = []
+    with open(fname, 'r') as f:
+        reader = csv.reader(f)
+        for row_idx, row in enumerate(reader):
+            insert = {}
+            if row_idx < 3: # skip header rows
+                continue
+            for field_name, entry in zip(fields, row):
+                # handle int fields
+                if field_name in ['field_priority', 'column_priority', 'facet_priority']:
+                    if entry:
+                        insert[field_name] = int(entry)
+                # handle bool fields
+                elif field_name in ['is_list', 'mvp']:
+                    if entry:
+                        if entry == 'Y':
+                            insert[field_name] = True
+                        else:
+                            insert[field_name] = False
+                elif field_name in ['enum_list']:
+                    if entry:
+                        field_type = row[3] # hardcoded, must change if field_type is moved on mapping table
+                        val_list = []
+                        if field_type == 'string':
+                            val_list = [en.strip() for en in entry.split(',') if en.strip()]
+                        elif field_type == 'number':
+                            val_list = [float(en.strip()) for en in entry.split(',') if en.strip()]
+                        elif field_type == 'integer':
+                            val_list = [int(en.strip()) for en in entry.split(',') if en.strip()]
+                        insert[field_name] = val_list
+                else: # handle all other fields if they exist
+                    if entry:
+                        insert[field_name] = entry
+            insert['project'] = '12a92962-8265-4fc0-b2f8-cf14f05db58b'
+            insert['institution'] = 'hms-dbmi'
+            insert['submitted_by'] = 'koray_kirli@hms.harvard.edu'
+            inserts.append(insert)
+    return inserts
+
+
+def generate_sample_json(inserts):
+    """
+    Generates sample JSON given the inserts, which are first pruned and sorted
+    based on priority.
+    """
+    sample_props = OrderedDict()
+    mvp_list = [i for i in inserts if i.get('mvp')]
+    samples = [i for i in mvp_list if i.get('scope') == 'sample']
+    samples = sorted(samples, key = lambda i: i.get('field_priority', 1000000))
+
+    def get_prop(item):
+        update = OrderedDict()
+        prop_name = item['field_name']
+        features = OrderedDict()
+        features.update({
+            "title": item['schema_title'],
+            "vcf_name": item['vcf_name'],
+            "type": item['field_type']
+        })
+        if item.get('schema_description'):
+            features['description'] = item['schema_description']
+        if item.get('links_to'):
+            features['linkTo'] = item['links_to']
+        if item.get('enum_list'):
+            features['enum'] = item['enum_list']
+        if item.get('field_priority'):
+            features['lookup'] = item['field_priority']
+        for a_field in ['scale', 'domain', 'method', 'seperator', 'source_name', 'source_version']:
+            if item.get(a_field):
+                features[a_field] = item[a_field]
+
+        # convert array structure
+        if item.get('is_list'):
+            array_item = OrderedDict()
+            array_item.update( {
+                "title": item['schema_title'],
+                "type": "array",
+                "vcf_name": item['vcf_name']
+            })
+            if item.get('schema_description'):
+                array_item['description'] = item['schema_description']
+            if item.get('field_priority'):
+                array_item['lookup'] = item['field_priority']
+            array_item['items'] = features
+            update[prop_name] = array_item
+            return update
+        else:
+            update[prop_name] = features
+            return update
+
+    for sample in samples:
+        if sample.get('sub_embedding_group'):
+            continue
+        sample_props.update(get_prop(sample))
+    return sample_props
+
+
+def generate_variant_json(inserts):
+    """
+    Generates variant JSON, similarly to above
+    Given inserts generates var_props, columns and facets as a 3 tuple
+    """
+    var_props = OrderedDict()
+    cols = OrderedDict()
+    facs = OrderedDict()
+    mvp_list = [i for i in inserts if i.get('mvp')]
+    variants = [i for i in mvp_list if i.get('scope') != 'sample']
+
+    def get_prop(item):
+        temp = OrderedDict()
+        prop_name = item['field_name']
+        features = OrderedDict()
+        features.update({
+            "title": item['schema_title'],
+            "vcf_name": item['vcf_name'],
+            "type": item['field_type']
+        })
+        if item.get('schema_description'):
+            features['description'] = item['schema_description']
+        if item.get('links_to'):
+            features['linkTo'] = item['links_to']
+        if item.get('enum_list'):
+            features['enum'] = item['enum_list']
+        if item.get('field_priority'):
+            features['lookup'] = item['field_priority']
+
+        for a_field in ['scale', 'domain', 'method', 'seperator', 'source_name', 'source_version']:
+            if item.get(a_field):
+                features[a_field] = item[a_field]
+
+        # handle sub_embedded object
+        if item.get('sub_embedding_group'):
+            assert item['is_list'] == True
+            sub_temp = OrderedDict()
+            prop = OrderedDict()
+            prop[prop_name] = features
+            sum_ob_name = item['sub_embedding_group']
+            sub_title = sum_ob_name.replace("_", " ").title()
+            sub_temp.update({
+                "title": sub_title,
+                "type": "array",
+                "items": {
+                    "title": sub_title,
+                    "type": "object",
+                    "properties": prop
+                    }
+                })
+            temp[sum_ob_name] = sub_temp
+            return temp
+
+        # convert to array sturcutre
+        if item.get('is_list'):
+            array_item = OrderedDict()
+            array_item.update( {
+                "title": item['schema_title'],
+                "type": "array",
+                "vcf_name": item['vcf_name']
+            })
+            if item.get('schema_description'):
+                array_item['description'] = item['schema_description']
+            if item.get('field_priority'):
+                array_item['lookup'] = item['field_priority']
+            array_item['items'] = features
+            temp[prop_name] = array_item
+            return temp
+        else:
+            temp[prop_name] = features
+            return temp
+
+    def update(d, u):
+        for k, v in six.iteritems(u):
+            dv = d.get(k, {})
+            if not isinstance(dv, Mapping):
+                d[k] = v
+            elif isinstance(v, Mapping):
+                d[k] = update(dv, v)
+            else:
+                d[k] = v
+        return d
+
+    for variant in variants:
+        update(var_props, get_prop(variant))
+        if variant.get('facet_priority'):
+            facs[variant['field_name']] = {'title': variant['schema_title']}
+        if variant.get('column_priority'):
+            cols[variant['field_name']] = {'title': variant['schema_title']}
+
+    return var_props, cols, facs
+
+
 def main():
     """ Works with mp.csv, downloaded 10-25 """
     logging.basicConfig()
@@ -295,12 +492,21 @@ def main():
         logger.error('Failed to process mapping table. Exiting.\n')
         exit(1)
 
-    print(FIELDS)
     ANNOTATION_SCHEMA = build_annotation_field_schema(FIELDS)
-    print(ANNOTATION_SCHEMA)
     with open('annotation_field.json', 'w+') as out:
         json.dump(ANNOTATION_SCHEMA, out)
     logger.info('Successfully wrote new annotation_schema.json\n')
+    inserts = process_inserts(fname, FIELDS)
+    # XXX: Post inserts below
+    # from ff_utils import post_metadata
+    # for entry in inserts:
+    #     ff_utils.post_metadata(insert, 'annotation_field')
+    logger.info('Successfully created/posted annotations\n')
+    sample_props = generate_sample_json(inserts)
+    logger.info('Successfully generated sample JSON\n')
+    var_props, cols, facs = generate_variant_json(inserts)
+    logger.info('Successfully generated variant JSON\n')
+    # Write out stuff here as appropriate
 
 
 if __name__ == '__main__':
--- src/encoded/tests/test_build_annotation_schema.py
+++ src/encoded/tests/test_build_annotation_schema.py
@@ -7,7 +7,10 @@ from encoded.commands.build_annotation_schema import (
     read_mapping_table,
     add_annotation_schema_fields,
     add_annotation_schema_facets_and_columns,
-    build_annotation_field_schema
+    build_annotation_field_schema,
+    process_inserts,
+    generate_sample_json,
+    generate_variant_json
 )
 
 pytestmark = [pytest.mark.working]
@@ -18,6 +21,27 @@ EXPECTED_FIELDS = ['field_name', 'vcf_name', 'value_example', 'field_type',
                     'schema_title', 'schema_description', 'source_name',
                     'source_version', 'field_priority', 'column_priority',
                     'facet_priority', 'links_to', 'mvp']
+EXPECTED_INSERT = {'is_list': False, 'field_name': 'chrom', 'mvp': True, 'schema_title':
+               'Chromosome', 'enum_list': ['1', '2', '3', '4', '5', '6', '7', '8',
+               '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20',
+               '21', '22', 'X', 'Y', 'M'], 'institution': 'hms-dbmi', 'field_type':
+               'string', 'column_priority': 1, 'field_priority': 1, 'project':
+               '12a92962-8265-4fc0-b2f8-cf14f05db58b', 'submitted_by': 'koray_kirli@hms.harvard.edu',
+               'scope': 'variant', 'source_version': 'VCFv4.2', 'source_name': 'VCF',
+               'value_example': '1', 'vcf_name': 'CHROM'}
+MVP_EXPECTED = 317
+SAMPLE_FIELDS_EXPECTED = 12
+VARIANT_FIELDS_EXPECTED = 305
+
+@pytest.fixture
+def fields():
+    _, _, FIELDS = read_mapping_table(FNAME)
+    return FIELDS
+
+
+@pytest.fixture
+def inserts(fields):
+    return process_inserts(FNAME, fields)
 
 
 def test_read_mapping_table():
@@ -53,13 +77,12 @@ def test_add_facets_and_columns():
     assert 'source_name' in schema['columns']
 
 
-def test_build_full_schema():
+def test_build_full_schema(fields):
     """
     Tests that some fields look correct when we build the whole annotation
     field schema. Tests random things about properties that should be there
     """
-    _, _, FIELDS = read_mapping_table(FNAME)
-    schema = json.loads(build_annotation_field_schema(FIELDS))
+    schema = json.loads(build_annotation_field_schema(fields))
     assert schema['properties']['field_name']['type'] == 'string'
     assert schema['properties']['is_list']['type'] == 'boolean'
     assert schema['properties']['scale']['title'] == 'Scale'
@@ -67,3 +90,51 @@ def test_build_full_schema():
     assert sorted(schema['properties']['scope']['enum']) == sorted(['variant', 'sample', 'gene'])
     assert sorted(schema['properties']['links_to']['enum']) == sorted(['Gene', 'Disorder', 'Phenotype'])
     assert schema['properties']['value_example']['type'] == 'string'
+
+
+def test_process_inserts(inserts):
+    """ Tests that we properly process an insert """
+    assert inserts[0] == EXPECTED_INSERT
+    mvp_list = [i for i in inserts if i.get('mvp')]
+    assert len(mvp_list) == MVP_EXPECTED
+    sample = [i for i in mvp_list if i.get('scope') == 'sample']
+    assert len(sample) == SAMPLE_FIELDS_EXPECTED
+    variant = [i for i in mvp_list if i.get('scope') != 'sample']
+    assert len(variant) == VARIANT_FIELDS_EXPECTED
+
+
+def test_generate_sample_json(inserts):
+    """ Tests that sample JSON is being created correctly checking three we expect """
+    sample_props = generate_sample_json(inserts)
+    assert sample_props['qual']['title'] == 'Quality score'
+    assert sample_props['qual']['vcf_name'] == 'QUAL'
+    assert sample_props['qual']['type'] == 'number'
+    assert sample_props['pid']['title'] == 'Physical phasing ID information'
+    assert sample_props['pid']['vcf_name'] == 'PID'
+    assert sample_props['pid']['type'] == 'string'
+    assert sample_props['af']['title'] == 'Allele fraction'
+    assert sample_props['af']['source_name'] == 'VCF'
+    assert sample_props['af']['source_version'] == 'VCFv4.2'
+
+
+def test_generate_variant_json(inserts):
+    """ Tests that variant JSON along with columns and facets are produced """
+    var_props, cols, facs = generate_variant_json(inserts)
+    assert cols['chrom']['title'] == 'Chromosome'
+    assert cols['pos']['title'] == 'Position'
+    assert cols['id']['title'] == 'ID'
+    assert cols['ref']['title'] == 'Reference allele'
+    assert cols['alt']['title'] == 'Alternative allele'
+    assert facs['clinsig']['title'] == 'ClinVar significance'
+    assert var_props['cytoband']['title'] == 'Cytoband'
+    assert var_props['cytoband']['type'] == 'string'
+    assert var_props['cytoband']['scale'] == 'WINDOW'
+    assert var_props['cytoband']['domain'] == 'FUNCTION'
+    assert var_props['cytoband']['source_name'] == 'ANNOVAR'
+    assert var_props['exac_sas']['title'] == 'ExAC Allele frequency in South Asian samples'
+    assert var_props['exac_sas']['vcf_name'] == 'ExAC_SAS'
+    assert var_props['exac_sas']['type'] == 'number'
+    assert var_props['exac_sas']['scale'] == 'VARIANT'
+    assert var_props['exac_sas']['domain'] == 'POPULATION GENETICS'
+    assert var_props['exac_sas']['source_name'] == 'ExAC'
+    assert var_props['exac_sas']['source_version'] == '3'
