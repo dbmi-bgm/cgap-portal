9146191d6906f02900cbfa971d612375532e3a72
William Ronchetti <wrr33@cornell.edu>
2020-01-02 09:40:26 -0500

    remove MAX_NGRAM import

--- src/encoded/search.py
+++ src/encoded/search.py
@@ -10,7 +10,7 @@ from snovault import (
 )
 from snovault.embed import make_subrequest
 from snovault.elasticsearch import ELASTIC_SEARCH
-from snovault.elasticsearch.create_mapping import MAX_NGRAM, determine_if_is_date_field
+from snovault.elasticsearch.create_mapping import determine_if_is_date_field
 from snovault.elasticsearch.indexer_utils import get_namespaced_index
 from snovault.resource_views import collection_view_listing_db
 from snovault.util import (
