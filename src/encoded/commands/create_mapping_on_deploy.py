f52ade6fc2b7beb762f0c6f6609f1227d7c4979e
Will Ronchetti <wrr33@cornell.edu>
2020-01-22 16:16:38 -0500

    Deploy Change (#74)
    * C4-10 port FF deploy change
    
    * C4-10 port cmod cleanup from FF

--- src/encoded/commands/create_mapping_on_deploy.py
+++ src/encoded/commands/create_mapping_on_deploy.py
@@ -48,6 +48,82 @@ ITEM_INDEX_ORDER = [
     'Page'
 ]
 
+ENV_PROD = 'fourfront-cgap'
+ENV_DEV = 'fourfront-cgapdev'
+ENV_TEST = 'fourfront-cgaptest'
+ENV_WOLF = 'fourfront-cgapwolf'
+
+BEANSTALK_PROD_ENVS = [
+    ENV_PROD,
+]
+
+BEANSTALK_TEST_ENVS = [
+    ENV_DEV,
+    ENV_TEST,
+    ENV_WOLF,
+]
+
+
+def get_my_env(app):
+    """
+    Gets the env name of the currently running environment
+
+    :param app: handle to Pyramid app
+    :return: current env, one of the above
+    """
+    return app.registry.settings.get('env.name')
+
+
+def get_deployment_config(app):
+    """
+    Gets deployment configuration based on what env we are on. Right now we wipe ES on all except
+    CGAP prod
+
+    :param app: handle to Pyramid app
+    :return: dict of config options
+    """
+    deploy_cfg = {}
+    current_prod_env = ENV_PROD  # this could change depending on how we do this in the future
+    my_env = get_my_env(app)
+    deploy_cfg['ENV_NAME'] = my_env
+    if current_prod_env == my_env:
+        log.info('This looks like our production environment -- do not wipe ES')
+        deploy_cfg['WIPE_ES'] = False
+    elif my_env in BEANSTALK_PROD_ENVS:  # unused but could be used in future
+        log.info('This looks like our staging environment -- do not wipe ES')
+        deploy_cfg['WIPE_ES'] = False
+    elif my_env in BEANSTALK_TEST_ENVS:
+        log.info('This looks like a test environment -- WIPE ES')
+        deploy_cfg['WIPE_ES'] = True
+    else:
+        log.warning('This environment is not recognized: %s' % my_env)
+        log.warning('Proceeding without wiping ES')
+        deploy_cfg['WIPE_ES'] = False
+    return deploy_cfg
+
+
+def _run_create_mapping(app, args):
+    """
+    Runs create mapping with arguments based on args or deployment config based on env
+
+    :param app: handle to Pyramid app
+    :param args: args from Argparse in main
+    :return: None
+    """
+    try:
+        deploy_cfg = get_deployment_config(app)
+        if args.wipe_es:  # override deploy_cfg WIPE_ES option
+            log.info('Overriding deploy_cfg and wiping ES')
+            deploy_cfg['WIPE_ES'] = True
+        if deploy_cfg['WIPE_ES']:
+            run_create_mapping(app, check_first=False, item_order=ITEM_INDEX_ORDER)
+        else:
+            run_create_mapping(app, check_first=True, item_order=ITEM_INDEX_ORDER)
+    except Exception as e:
+        log.error('Exception encountered while gathering deployment information or running create_mapping')
+        log.error(str(e))
+        exit(1)
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -56,6 +132,7 @@ def main():
     )
     parser.add_argument('config_uri', help="path to configfile")
     parser.add_argument('--app-name', help="Pyramid app name in configfile")
+    parser.add_argument('--wipe-es', help="Specify to wipe ES", action='store_true', default=False)
 
     args = parser.parse_args()
     app = get_app(args.config_uri, args.app_name)
@@ -63,24 +140,4 @@ def main():
     set_logging(in_prod=app.registry.settings.get('production'), log_name=__name__, level=logging.DEBUG)
     # set_logging(app.registry.settings.get('elasticsearch.server'), app.registry.settings.get('production'), level=logging.DEBUG)
 
-    # check if staging
-    try:
-        data_env = whodaman()
-        env = app.registry.settings.get('env.name')
-        if 'webprod' in env:
-            if data_env != env:
-                log.info("looks like we are on staging, run create mapping without check first")
-                run_create_mapping(app, check_first=False, item_order=ITEM_INDEX_ORDER)
-                return
-        # handle mastertest ... by blowing away all data first
-        if 'mastertest' in env:
-            log.info("looks like we are on mastertest, run create mapping without check first")
-            run_create_mapping(app, check_first=False, purge_queue=True, item_order=ITEM_INDEX_ORDER)
-            return
-        log.info("looks like we are NOT on staging or mastertest so run create mapping with check first")
-    except Exception:
-        import traceback
-        log.warning("error checking whodaman: %s " % traceback.format_exc())
-        log.warning("couldn't get wodaman, so assuming NOT staging")
-    log.info("... using default create mapping case")
-    run_create_mapping(app, check_first=True, purge_queue=True, item_order=ITEM_INDEX_ORDER)
+    _run_create_mapping(app, args)
