e164836a46af90489f20fd7b9f199273f7ed7e4a
aschroed <andrew_schroeder@hms.harvard.edu>
2020-02-07 16:47:02 -0500

    and a bit more

--- src/encoded/types/evidence.py
+++ src/encoded/types/evidence.py
@@ -12,45 +12,6 @@ from .base import (
 )
 
 
-# def get_evidence_linked_items(request, rpaths, direction):
-#     """ This function is generally useful for getting the items that are linked to
-#         others through an Evidence item so this is where it lives for now.
-#         To use it in a calculated property the linked Items in subject_item or object_item
-#         must be rev linked to the Evidence item using
-#
-#         rev = {
-#             'as_subject_evidence': ('Evidence', 'subject_item'),
-#             'as_object_evidence': ('Evidence', 'object_item'),
-#         }
-#
-#         NOTE if you know that a certain Item type will ever only be either a subject or object
-#         you can omit the unneeded item from the rev dict.
-#
-#         In the calculated property you get a list to the resource paths by calling
-#         self.rev_link_atids
-#
-#         params:
-#         request - the request object
-#         paths - list of resource paths from the rev links
-#         direction - 'either subject or object'
-#     """
-#     uuids = []
-#     for evi in rpaths:
-#         ''' cannot use embedded frame so using the object frame and relying on the collection name to match
-#             the item type and always be part of the resource_path in the value - a bit hacky and might want
-#             to prefer to use another get_item_if_you_can for filtering by type but might not be performant
-#         '''
-#         dname = direction + '_item'
-#         # get frame=raw to stop evidence items from linking each other
-#         evi_item = get_item_if_you_can(request, evi, 'evidences', frame='raw')
-#         if evi_item:
-#             # this will just be a uuid
-#             ri_info = evi_item.get(dname)
-#             if ri_info:
-#                 uuids.append(ri_info)
-#     return uuids
-
-
 @abstract_collection(
     name='evidences',
     acl=ALLOW_SUBMITTER_ADD,
