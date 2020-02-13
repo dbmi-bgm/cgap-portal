42e3330eb03922d2bfcbec3d60413fadccadae77
Sarah <sarah_reiff@hms.harvard.edu>
2020-02-13 10:45:45 -0500

    added children calc prop to individual

--- src/encoded/tests/test_types_individual.py
+++ src/encoded/tests/test_types_individual.py
@@ -39,3 +39,10 @@ def test_patch_invalid_project_inst(testapp, project, institution, MIndividual):
     res = testapp.post_json('/individual', MIndividual, status=201).json['@graph'][0]
     testapp.patch_json(res['@id'], {'project': 'does not exist'}, status=422)
     testapp.patch_json(res['@id'], {'institution': 'does not exist'}, status=422)
+
+
+def test_individual_children(testapp, project, institution, MIndividual, WIndividual):
+    res_m = testapp.post_json('/individual', MIndividual, status=201).json['@graph'][0]
+    WIndividual['father'] = res_m['@id']
+    res_f = testapp.post_json('/individual', WIndividual, status=201).json['@graph'][0]
+    assert testapp.get(res_m['@id']).json.get('children') == [res_f['@id']]
---
+++ src/encoded/types/individual.py
@@ -0,0 +1,54 @@
+from snovault import (
+    calculated_property,
+    collection,
+    load_schema,
+)
+from .base import (
+    Item
+)
+
+
+@collection(
+    name='individuals',
+    unique_key='accession',
+    properties={
+        'title': 'Individuals',
+        'description': 'Listing of Individuals',
+    })
+class Individual(Item):
+    item_type = 'individual'
+    name_key = 'accession'
+    schema = load_schema('encoded:schemas/individual.json')
+    rev = {'children_f': ('Individual', 'father'),
+           'children_m': ('Individual', 'mother')}
+
+    embedded_list = [
+        'father.is_deceased',
+        'father.sex',
+        'mother.is_deceased',
+        'mother.sex'
+    ]
+
+    @calculated_property(schema={
+        "title": "Display Title",
+        "description": "A calculated title for every object in 4DN",
+        "type": "string"
+    })
+    def display_title(self, request, accession, bgm_id=None, other_id=None):
+        """Use bgm_id, other_id, or accession (in that order)"""
+        if bgm_id:
+            title = bgm_id
+        elif other_id:
+            title = '%s (%s)' % (other_id['id'], other_id['id_source'])
+        else:
+            title = accession
+        return title
+
+    @calculated_property(schema={
+        "title": "Children",
+        "description": "Children of the individual",
+        "type": "array"
+    })
+    def children(self, request):
+        return (self.rev_link_atids(request, "children_f") +
+                self.rev_link_atids(request, "children_m"))
