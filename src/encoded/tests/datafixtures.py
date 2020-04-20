6140efa850b0365fce4fcd0584aca335a65fdcae
Sarah <sarah_reiff@hms.harvard.edu>
2020-04-20 09:22:46 -0400

    moved phenotype earlier in ORDER in datafixtures

--- src/encoded/tests/datafixtures.py
+++ src/encoded/tests/datafixtures.py
@@ -3,10 +3,10 @@ import copy
 
 
 ORDER = [
-    'user', 'project', 'institution', 'file_format', 'variant_consequence', 'cohort', 'family', 'individual',
-    'sample', 'workflow', 'access_key', 'disorder', 'document', 'file_fastq',
-    'file_processed', 'file_reference', 'gene', 'sample_processing', 'case',
-    'report', 'page', 'phenotype', 'quality_metric_fastqc', 'evidence_dis_pheno',
+    'user', 'project', 'institution', 'file_format', 'variant_consequence', 'phenotype',
+    'cohort', 'family', 'individual', 'sample', 'workflow', 'access_key', 'disorder',
+    'document', 'file_fastq', 'file_processed', 'file_reference', 'gene', 'sample_processing',
+    'case', 'report', 'page', 'quality_metric_fastqc', 'evidence_dis_pheno',
     'quality_metric_bamcheck', 'quality_metric_qclist', 'quality_metric_wgs_bamqc',
     'quality_metric_vcfcheck', 'quality_metric_workflowrun', 'software', 'static_section',
     'tracking_item', 'workflow_mapping', 'workflow_run_awsem', 'workflow_run'
