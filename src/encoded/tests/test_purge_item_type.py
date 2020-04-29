5bc37d69cf6bfd8d8e6859e9f2ee3223229d4d30
William Ronchetti <wrr33@cornell.edu>
2020-04-29 13:51:43 -0400

    C4-98 hopefully fix more testing problems, add entry point

--- src/encoded/tests/test_purge_item_type.py
+++ src/encoded/tests/test_purge_item_type.py
@@ -33,13 +33,14 @@ def many_dummy_static_sections(testapp):
         static_section_template['name'] = 'search-info-header.Workflow:%s' % i
         resp = testapp.post_json('/static_section', static_section_template, status=201).json
         paths.append(resp['@graph'][0]['@id'])
+    testapp.post_json('/index', {'record': True})
     return paths
 
 
 @pytest.mark.parametrize('item_type', ['static_section'])  # maybe should test some other types...
 def test_purge_item_type_from_db(testapp, dummy_static_section, item_type):
     """ Tests purging all items of a certain item type from the DB """
-    assert purge_item_type_from_storage(testapp, [item_type])
+    assert purge_item_type_from_storage(testapp, [item_type]) is True
     testapp.post_json('/index', {'record': True})
     testapp.get('/search/?type=StaticSection', status=404)
     testapp.get('/static-sections/442c8aa0-dc6c-43d7-814a-854af460b015?datastore=database', status=404)
@@ -48,7 +49,7 @@ def test_purge_item_type_from_db(testapp, dummy_static_section, item_type):
 def test_purge_item_type_from_db_many(testapp, many_dummy_static_sections):
     """ Tests posting/deleting several static sections and checking all are gone """
     paths_to_check = many_dummy_static_sections
-    assert purge_item_type_from_storage(testapp, ['static_section'])
+    assert purge_item_type_from_storage(testapp, ['static_section']) is True
     testapp.post_json('/index', {'record': True})
     path_string = '%s?datastore=database'
     for path in paths_to_check:
@@ -60,7 +61,9 @@ def test_purge_item_type_with_links_fails(testapp, workbook):
     """ Tries to remove Individuals, which when the workbook is indexed will have links so
         deletion will fail
     """
+    testapp.post_json('/index', {'record': True})  # must index everything so individual links show up
+    time.sleep(5)  # wait for indexing to catch up
     assert not purge_item_type_from_storage(testapp, ['individual'])
-    assert purge_item_type_from_storage(testapp, ['cohort'])  # this one will work since it is not linkedTo
+    assert purge_item_type_from_storage(testapp, ['cohort']) is True  # this one will work since it is not linkedTo
     testapp.post_json('/index', {'record': True})
     testapp.get('/search/?type=Cohort', status=404)
