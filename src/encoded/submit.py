eb9a0224baf7eb4cdd109cfd87cb61f22bce10f4
Sarah <sarah_reiff@hms.harvard.edu>
2020-05-29 17:05:15 -0400

    added some comments and edits to submit.py

--- src/encoded/submit.py
+++ src/encoded/submit.py
@@ -14,12 +14,13 @@ BGM_FIELD_MAPPING = {
 }
 
 
-POST_ORDER = ['Sample', 'SampleProcessing', 'Individual', 'Family']
+POST_ORDER = ['sample', 'sample_processing', 'individual', 'family']
 
 
 SECOND_ROUND = {}
 
 
+# This is a placeholder for a submission endpoint modified from loadxl
 @view_config(route_name='submit_data', request_method='POST', permission='add')
 @debug_log
 def submit_data(context, request):
@@ -43,6 +44,10 @@ def submit_data(context, request):
 
 
 def xls_to_json(xls_data, project, institution):
+    '''
+    Converts excel file to json for submission.
+    Functional but expect future changes.
+    '''
     book = xlrd.open_workbook(xls_data)
     sheet, = book.sheets()
     row = row_generator(sheet)
@@ -56,15 +61,15 @@ def xls_to_json(xls_data, project, institution):
         row_dict = {keys[i].lower(): item for i, item in enumerate(r)}
         rows.append(row_dict)
 
-    items = {'Individual': {}, 'Family': {}, 'Sample': {}, 'SampleProcessing': {}}
+    items = {'individual': {}, 'family': {}, 'sample': {}, 'sample_processing': {}}
     specimen_ids = {}
     for row in rows:
         indiv_alias = 'test-project:individual-{}'.format(row['patient id'])
         fam_alias = 'test-project:family-{}'.format(row['family id'])
         sp_alias = 'test-project:sampleproc-{}'.format(row['specimen id'])
         # create items for Individual
-        if row['patient id'] not in items['Individual']:
-            items['Individual'][indiv_alias] = {
+        if row['patient id'] not in items['individual']:
+            items['individual'][indiv_alias] = {
                 'aliases': [indiv_alias],
                 'individual_id': row['patient id'],
                 'sex': row['sex'],
@@ -72,16 +77,16 @@ def xls_to_json(xls_data, project, institution):
                 'birth_year': row['birth year']
             }
         # create/edit items for Family
-        if fam_alias not in items['Family']:
+        if fam_alias not in items['family']:
             items['Family'][fam_alias] = {
                 'aliases': [fam_alias],
                 'family_id': row['family id'],
                 'members': [indiv_alias]
             }
         else:
-            items['Family'][fam_alias]['members'].append(indiv_alias)
+            items['family'][fam_alias]['members'].append(indiv_alias)
         if row.get('relation to proband', '').lower() in ['proband', 'mother', 'father']:
-            items['Family'][fam_alias][row['relation to proband'].lower()] = indiv_alias
+            items['family'][fam_alias][row['relation to proband'].lower()] = indiv_alias
         # create item for Sample if there is a specimen
         if row['specimen id']:
             samp_alias = 'test-project:sample-{}'.format(row['specimen id'])
@@ -90,7 +95,7 @@ def xls_to_json(xls_data, project, institution):
                 specimen_ids[row['specimen id']] += 1
             else:
                 specimen_ids[row['specimen id']] = 1
-            items['Sample'][samp_alias] = {
+            items['sample'][samp_alias] = {
                 'aliases': [samp_alias],
                 'workup_type': row['workup type'],
                 'specimen_type': row['specimen type'],
@@ -106,10 +111,10 @@ def xls_to_json(xls_data, project, institution):
                 'specimen_notes': row['specimen notes'],
                 'files': []
             }
-            items['Individual'][indiv_alias]['samples'] = [samp_alias]
+            items['individual'][indiv_alias]['samples'] = [samp_alias]
             # create SampleProcessing item for that one sample if needed
             if row['report required'].lower() in ['yes', 'y']:
-                items['SampleProcessing'][sp_alias] = {
+                items['sample_processing'][sp_alias] = {
                     'aliases': [sp_alias],
                     'analysis_type': row['workup type'],
                     'samples': [samp_alias]
@@ -118,24 +123,24 @@ def xls_to_json(xls_data, project, institution):
             print('WARNING: No specimen id present for patient {},'
                   ' sample will not be created.'.format(row['patient id']))
     # create SampleProcessing item for trio/group if needed
-    for v in items['Family'].values():
+    for v in items['family'].values():
         if 'members' in v and len(v['members']) > 1:
             # create sample_processing item
             alias = 'test-project:{}-sampleproc'.format(v['family_id'])
-            samples = [items['Individual'][indiv].get('samples', [None])[0] for indiv in v['members']]
+            samples = [items['individual'][indiv].get('samples', [None])[0] for indiv in v['members']]
             samples = [s for s in samples if s]
             if len (samples) > 1:
                 sp = {
                     'aliases': [alias],
                     'samples': samples
                 }
-                analysis_type = items['Sample'][items['Individual'][v['proband']]['samples'][0]]['workup_type']
+                analysis_type = items['sample'][items['individual'][v['proband']]['samples'][0]]['workup_type']
                 if sorted(v['members']) == sorted([v['proband'], v['mother'], v['father']]):
                     sp['analysis_type'] = analysis_type + '-Trio'
                 else:
                     sp['analysis_type'] = analysis_type + '-Group'
-                items['SampleProcessing'][alias] = sp
-    # removed unused fields, add project and institution 
+                items['sample_processing'][alias] = sp
+    # removed unused fields, add project and institution
     for val1 in items.values():
         for val2 in val1.values():
             remove_keys = [k for k, v in val2.items() if not v]
@@ -147,7 +152,15 @@ def xls_to_json(xls_data, project, institution):
     return items
 
 
-def check_against_db():
+def validate_and_post(testapp, json_data, dryrun=False):
+    '''
+    Still in progress, not necessarily functional yet. NOT YET TESTED.
+
+    Function that:
+    1. looks up each item in json
+    2. if item in db, will validate and patch any different metadata
+    3. if item not in db, will post item
+    '''
     alias_dict = {}
     links = ['samples', 'members', 'mother', 'father', 'proband']
     for itemtype in POST_ORDER:
@@ -159,13 +172,13 @@ def check_against_db():
             except Exception as e:
                 # post if not in db
                 if 'HTTPNotFound' in str(e):
-                    validation = testapp.post_json(results[itemtype][alias], itemtype + '/?checkonly=True', status=201)
-                    if validation:  # modify to check for lack of validation errors
-                        response = testapp.post_json(results[itemtype][alias], status=201)
-                        # do something to record response
-                    else:
+                    validation = testapp.post_json('/{}/?checkonly=True'.format(itemtype), results[itemtype][alias], status=201)
+                    if not validation:  # modify to check for presence of validation errors
                         # do something to report validation errors
                         pass
+                    elif not dryrun:  # post
+                        response = testapp.post_json('/' + itemtype, results[itemtype][alias], status=201)
+                        # do something to record response
             else:
                 # patch if item exists in db
                 alias_dict[alias] = result['@id']
@@ -190,10 +203,26 @@ def check_against_db():
                             val = result.get(field, [])
                             val.extend(results[itemtype][alias][field])
                             to_patch[field] = list(set(val))
+                validation = testapp.patch_json(result['@id'] + '/?checkonly=True', to_patch, status=200)
+                if not validation:  # modify to check for presence of validation errors
+                    # do something to report validation errors
+                    pass
+                elif not dryrun:  # patch
+                    response = testapp.patch_json('/' + itemtype, results[itemtype][alias], status=201)
+                    # do something to record response
+
+
+# This was just to see if i could post something using testapp in the python command line, currently works.
+def test_function():
+    app = get_app('development.ini', 'app')
+    environ = {'HTTP_ACCEPT': 'application/json', 'REMOTE_USER': 'TEST'}
+    testapp = TestApp(app, environ)
+    response = testapp.post_json('/project', {'name': 'test', 'title': 'Test'}, status=201)
+    print(response)
 
 
 def cell_value(cell, datemode):
-    """Get cell value from excel."""
+    """Get cell value from excel. [From Submit4DN]"""
     # This should be always returning text format
     ctype = cell.ctype
     value = cell.value
@@ -217,6 +246,7 @@ def cell_value(cell, datemode):
 
 
 def row_generator(sheet):
+    '''Generator that gets rows from excel sheet [From Submit4DN]'''
     datemode = sheet.book.datemode
     for index in range(sheet.nrows):
         yield [cell_value(cell, datemode) for cell in sheet.row(index)]
