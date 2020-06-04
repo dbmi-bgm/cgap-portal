551c8282e024166b2d7c7435244b44054a9df59f
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2020-06-04 07:32:02 -0400

    add support for case-specific columns

--- src/encoded/static/components/browse/columnExtensionMap.js
+++ src/encoded/static/components/browse/columnExtensionMap.js
@@ -4,8 +4,12 @@ import React from 'react';
 import _ from 'underscore';
 
 import { console, object } from '@hms-dbmi-bgm/shared-portal-components/es/components/util';
+import { itemUtil } from '@hms-dbmi-bgm/shared-portal-components/es/components/util/object';
 import { LocalizedTime, formatPublicationDate } from '@hms-dbmi-bgm/shared-portal-components/es/components/ui/LocalizedTime';
-import { basicColumnExtensionMap } from '@hms-dbmi-bgm/shared-portal-components/es/components/browse/components/table-commons';
+import { basicColumnExtensionMap,
+    DisplayTitleColumnWrapper,
+    DisplayTitleColumnDefault,
+    DisplayTitleColumnUser } from '@hms-dbmi-bgm/shared-portal-components/es/components/browse/components/table-commons';
 import { Schemas, typedefs } from './../util';
 
 // eslint-disable-next-line no-unused-vars
@@ -15,8 +19,77 @@ export const DEFAULT_WIDTH_MAP = { 'lg' : 200, 'md' : 180, 'sm' : 120, 'xs' : 12
 
 /** Theoretically we could change all these render functions to just be functional React components, maybe a later todo. */
 
+function renderAdvancedColumn(topLeft, status, main, dateTitle, date) {
+    return( 
+        <div style={{ width: "100%" }}>
+            <div className="d-flex justify-content-between">
+                <span className="col-topleft text-ellipsis-container" style={{ fontSize: "13px", fontStyle: "italic" }}>
+                    {topLeft}
+                </span>
+                <i className="item-status-indicator-dot mr-07" data-status={status}/>
+            </div>
+            <div
+                className="col-main text-ellipsis-container"
+                style={{ textTransform: "uppercase", textAlign: "center", fontSize: "20px", fontWeight: "600" }}
+            >
+                {main}
+            </div>
+            <div className="col-date text-ellipsis-container" style={{ textAlign: "center", fontSize: "12px" }}>
+                <strong>{dateTitle}</strong> 
+                <LocalizedTime timestamp={date} formatType="date-sm" />
+            </div>
+        </div>
+        );
+
+}
+
+// renderAdvancedColumn(result.display_title, result.status, result.individual_id, "Accessioned:", result.date_created);
+
+/**
+ * Should move this to CGAP at some point probably
+ */
+export const DisplayTitleColumnIndividual = React.memo(function DisplayTitleIndividualDefault({ result, link, onClick }) {
+    // `href` and `context` reliably refer to search href and context here, i.e. will be passed in from VirtualHrefController.
+    let title = itemUtil.getTitleStringFromContext(result); // Gets display_title || title || accession || ...
+
+    const tooltip = (typeof title === "string" && title.length > 20 && title) || null;
+    
+
+    console.log("result", result);
+    return <div key="title-container" className={`title-block d-flex flex-column`} data-tip={tooltip} data-delay-show={750}>
+        {renderAdvancedColumn(result.individual.display_title, result.individual.status, result.case_id, "Accessioned:", result.individual.date_created)}
+    </div>;
+});
+
+
+
+
+
 export const columnExtensionMap = {
     ...basicColumnExtensionMap,
+    // 'display_title' : {
+    //     'title' : "Title",
+    //     'widthMap' : { 'lg' : 280, 'md' : 250, 'sm' : 200 },
+    //     'minColumnWidth' : 90,
+    //     'order' : -100,
+    //     'render' : function renderDisplayTitleColumn(result, parentProps){
+    //         const { href, context, rowNumber, detailOpen, toggleDetailOpen } = parentProps;
+    //         const { '@type' : itemTypeList = ["Item"] } = result;
+    //         let renderElem;
+    //         if (itemTypeList[0] === "User") {
+    //             renderElem = <DisplayTitleColumnUser {...{ result }}/>;
+    //         } else if (itemTypeList[0] === "Individual") {
+    //             renderElem = <DisplayTitleColumnIndividual {...{ result }}/>;
+    //         } else {
+    //             renderElem = <DisplayTitleColumnDefault {...{ result }}/>;
+    //         }
+    //         return (
+    //             <DisplayTitleColumnWrapper {...{ result, href, context, rowNumber, detailOpen, toggleDetailOpen }}>
+    //                 { renderElem }
+    //             </DisplayTitleColumnWrapper>
+    //         );
+    //     }
+    // },
     // TODO: change to organization
     'lab.display_title' : {
         'title' : "Lab",
@@ -34,6 +107,38 @@ export const columnExtensionMap = {
                     { labLink }
                 </span>
             );
+        }
+    },
+    'individual': {
+        'title': "Individual",
+        'widthMap' : { 'lg' : 280, 'md' : 250, 'sm' : 200 },
+        'render' : function renderIndividualColumn(result, parentProps){
+            const { href, context, rowNumber, detailOpen, toggleDetailOpen } = parentProps;
+            const { '@type' : itemTypeList = ["Item"] } = result;
+
+            if (itemTypeList[0] === "Case") {
+                return (<DisplayTitleColumnWrapper {...{ result, href, context, rowNumber, detailOpen, toggleDetailOpen }}>
+                            <DisplayTitleColumnIndividual {...{ result }}/>
+                     </DisplayTitleColumnWrapper>);
+            }
+            return <DisplayTitleColumnIndividual {...{ result }}/>;
+        }
+    },
+    'sample_processing': {
+        'title': "This won't work right",
+        'render' : function renderIndividualColumn(result, parentProps){
+            const { href, context, rowNumber, detailOpen, toggleDetailOpen } = parentProps;
+            const { '@type' : itemTypeList = ["Item"], individual, sample_processing: { samples = []} } = result;
+
+            let selected = {};
+            console.log("individual @id", individual['@id']);
+            samples.forEach(sample => {
+                console.log("sample @id", sample['@id']);
+                if (sample.individual['@id'] === individual['@id']){
+                    selected = sample;
+                }
+            });
+            return renderAdvancedColumn(selected.accession, selected.status, selected.specimen_type, "Collected Date:", selected.specimen_collection_date);
         }
     },
     'date_published' : {
