a6fe747c13b08c3a044f539c928f867a4b92e62c
Sarah <sarah_reiff@hms.harvard.edu>
2020-06-11 13:29:39 -0400

    Major changes to submit.py
    Code broken down into smaller functions
    Functions not all finished currently
    Significant refactoring

--- src/encoded/submit.py
+++ src/encoded/submit.py
@@ -2,8 +2,13 @@ from pyramid.paster import get_app
 from pyramid.response import Response
 from pyramid.view import view_config
 from snovault.util import debug_log
-from webtest import TestApp
+# from webtest import TestApp
+from dcicutils.misc_utils import VirtualApp
+from dcicutils import ff_utils
+from webtest.app import AppError
+import ast
 import datetime
+import json
 import xlrd
 
 
@@ -32,7 +37,7 @@ def submit_data(context, request):
     post_only = request.json.get('post_only', False)
     app = get_app(config_uri, 'app')
     environ = {'HTTP_ACCEPT': 'application/json', 'REMOTE_USER': 'TEST'}
-    testapp = TestApp(app, environ)
+    virtualapp = VirtualApp(app, environ)
     # expected response
     request.response.status = 200
     result = {
@@ -64,82 +69,27 @@ def xls_to_json(xls_data, project, institution):
     items = {'individual': {}, 'family': {}, 'sample': {}, 'sample_processing': {}}
     specimen_ids = {}
     for row in rows:
-        indiv_alias = 'test-project:individual-{}'.format(row['patient id'])
-        fam_alias = 'test-project:family-{}'.format(row['family id'])
-        sp_alias = 'test-project:sampleproc-{}'.format(row['specimen id'])
+        indiv_alias = '{}:individual-{}'.format(project['name'], row['patient id'])
+        fam_alias = '{}:family-{}'.format(project['name'], row['family id'])
+        sp_alias = '{}:sampleproc-{}'.format(project['name'], row['specimen id'])
         # create items for Individual
-        if row['patient id'] not in items['individual']:
-            items['individual'][indiv_alias] = {
-                'aliases': [indiv_alias],
-                'individual_id': row['patient id'],
-                'sex': row['sex'],
-                'age': row['age'],
-                'birth_year': row['birth year']
-            }
+        items = fetch_individual_metadata(row, items, indiv_alias)
         # create/edit items for Family
-        if fam_alias not in items['family']:
-            items['Family'][fam_alias] = {
-                'aliases': [fam_alias],
-                'family_id': row['family id'],
-                'members': [indiv_alias]
-            }
-        else:
-            items['family'][fam_alias]['members'].append(indiv_alias)
-        if row.get('relation to proband', '').lower() in ['proband', 'mother', 'father']:
-            items['family'][fam_alias][row['relation to proband'].lower()] = indiv_alias
+        items = fetch_family_metadata(row, items, indiv_alias, fam_alias)
         # create item for Sample if there is a specimen
         if row['specimen id']:
-            samp_alias = 'test-project:sample-{}'.format(row['specimen id'])
+            samp_alias = '{}:sample-{}'.format(project['name'], row['specimen id'])
             if row['specimen id'] in specimen_ids:
                 samp_alias = samp_alias + '-' + specimen_ids[row['specimen id']]
                 specimen_ids[row['specimen id']] += 1
             else:
                 specimen_ids[row['specimen id']] = 1
-            items['sample'][samp_alias] = {
-                'aliases': [samp_alias],
-                'workup_type': row['workup type'],
-                'specimen_type': row['specimen type'],
-                'specimen_collection_date': row['date collected'],
-                'specimen_collection_location': row['location collected'],
-                'specimen_accession': row['specimen id'],
-                'date_transported': row['date transported'],
-                'transported_by': row['transport method'],
-                'sent_by': row['sent by'],
-                'date_received': row["date rec'd at ref lab"],
-                'specimen_accepted': row['specimen accepted by ref lab'],
-                'dna_concentration': row['dna concentration'],
-                'specimen_notes': row['specimen notes'],
-                'files': []
-            }
-            items['individual'][indiv_alias]['samples'] = [samp_alias]
-            # create SampleProcessing item for that one sample if needed
-            if row['report required'].lower() in ['yes', 'y']:
-                items['sample_processing'][sp_alias] = {
-                    'aliases': [sp_alias],
-                    'analysis_type': row['workup type'],
-                    'samples': [samp_alias]
-                }
+            items = fetch_sample_metadata(row, items, indiv_alias, samp_alias, sp_alias)
         else:
             print('WARNING: No specimen id present for patient {},'
                   ' sample will not be created.'.format(row['patient id']))
     # create SampleProcessing item for trio/group if needed
-    for v in items['family'].values():
-        if 'members' in v and len(v['members']) > 1:
-            # create sample_processing item
-            alias = 'test-project:{}-sampleproc'.format(v['family_id'])
-            samples = [items['individual'][indiv].get('samples', [None])[0] for indiv in v['members']]
-            samples = [s for s in samples if s]
-            if len (samples) > 1:
-                sp = {
-                    'aliases': [alias],
-                    'samples': samples
-                }
-                analysis_type = items['sample'][items['individual'][v['proband']]['samples'][0]]['workup_type']
-                if sorted(v['members']) == sorted([v['proband'], v['mother'], v['father']]):
-                    sp['analysis_type'] = analysis_type + '-Trio'
-                else:
-                    sp['analysis_type'] = analysis_type + '-Group'
-                items['sample_processing'][alias] = sp
+    items = create_sample_processing_groups(items, sp_alias)
     # removed unused fields, add project and institution
     for val1 in items.values():
         for val2 in val1.values():
@@ -152,7 +102,157 @@ def xls_to_json(xls_data, project, institution):
     return items
 
 
-def validate_and_post(testapp, json_data, dryrun=False):
+def fetch_individual_metadata(row, items, indiv_alias):
+    new_items = items.copy()
+    info = {
+        'aliases': [indiv_alias],
+        'individual_id': row['patient id'],
+        'sex': row.get('sex'),
+        'age': row.get('age'),
+        'birth_year': row.get('birth year')
+    }
+    if indiv_alias not in new_items['individual']:
+        new_items['individual'][indiv_alias] = {k: v for k, v in info.items() if v}
+    else:
+        for key in info:
+            if key not in new_items['individual'][indiv_alias]:
+                new_items['individual'][indiv_alias][key] = info[key]
+    return new_items
+
+
+def fetch_family_metadata(row, items, indiv_alias, fam_alias):
+    new_items = items.copy()
+    info = {
+        'aliases': [fam_alias],
+        'family_id': row['family id'],
+        'members': [indiv_alias]
+    }
+    if row.get('relation to proband', '').lower() in ['proband', 'mother', 'father']:
+        info[row['relation to proband'].lower()] = indiv_alias
+    if fam_alias not in new_items['family']:
+        new_items['family'][fam_alias] = info
+    else:
+        if indiv_alias not in new_items['family'][fam_alias]['members']:
+            new_items['family'][fam_alias]['members'].append(indiv_alias)
+        if row.get('relation to proband', '').lower() not in new_items['family'][fam_alias]:
+            new_items['family'][fam_alias][row['relation to proband'].lower()] = indiv_alias
+    return new_items
+
+
+def fetch_sample_metadata(row, items, indiv_alias, samp_alias, sp_alias):
+    new_items = items.copy()
+    info = {
+        'aliases': [samp_alias],
+        'workup_type': row.get('workup type'),
+        'specimen_type': row.get('specimen type'),
+        'specimen_collection_date': row.get('date collected'),
+        'specimen_collection_location': row.get('location collected'),
+        'specimen_accession': row['specimen id'],
+        'date_transported': row.get('date transported'),
+        'transported_by': row.get('transport method'),
+        'sent_by': row.get('sent by'),
+        'date_received': row.get("date rec'd at ref lab"),
+        'specimen_accepted': row.get('specimen accepted by ref lab'),
+        'dna_concentration': row.get('dna concentration'),
+        'specimen_notes': row.get('specimen notes')
+    }
+    new_items['sample'][samp_alias] = {k: v for k, v in info.items() if v}
+    if indiv_alias in new_items['individual']:
+        new_items['individual'][indiv_alias]['samples'] = [samp_alias]
+    # create SampleProcessing item for that one sample if needed
+    if row['report required'].lower() in ['yes', 'y']:
+        new_items['sample_processing'][sp_alias] = {
+            'aliases': [sp_alias],
+            'analysis_type': row['workup type'],
+            'samples': [samp_alias]
+        }
+    return new_items
+
+
+def create_sample_processing_groups(items, sp_alias):
+    new_items = items.copy()
+    for v in new_items['family'].values():
+        if 'members' in v and len(v['members']) > 1:
+            # create sample_processing item
+            samples = [items['individual'][indiv].get('samples', [None])[0] for indiv in v['members']]
+            samples = [s for s in samples if s]
+            if len (samples) > 1:
+                sp = {
+                    'aliases': [sp_alias],
+                    'samples': samples
+                }
+                analysis_type = items['sample'][items['individual'][v['proband']]['samples'][0]]['workup_type']
+                if all([relation in v for relation in ['proband', 'mother', 'father']]) and sorted(
+                    v['members']) == sorted([v['proband'], v['mother'], v['father']]
+                ):
+                    sp['analysis_type'] = analysis_type + '-Trio'
+                else:
+                    sp['analysis_type'] = analysis_type + '-Group'
+                new_items['sample_processing'][sp_alias] = sp
+    return new_items
+
+# NOT YET TESTED
+def compare_with_db(alias, virtualapp):
+    try:  # check if already in db
+        # result = virtualapp.get(alias + '/?frame=object')
+        # result = virtualapp.get('/search/?type=Item&aliases={}'.format(alias))
+        result = virtualapp.get('/search/?type=Item&age=33')
+        print(result)
+    except Exception as e:  # if not in db
+        print(e)
+        if 'HTTPNotFound' in str(e):
+            return None
+    else:
+        return result.json
+
+
+def validate_item(virtualapp, item, method, itemtype, atid=None):
+    if method == 'post':
+        #import pdb; pdb.set_trace()
+        try:
+            validation = virtualapp.post_json('/{}/?checkonly=True'.format(itemtype), item)
+        except AppError as e:
+            print('exception')
+            return parse_exception(e)
+        else:
+            return
+    elif method == 'patch':
+        try:
+            validation = virtualapp.patch_json(atid + '?checkonly=True', item, status=200)
+        except Exception as e:
+            return parse_exception(e)
+        else:
+            return
+    else:
+        raise ValueError("Unrecognized method -- must be 'post' or 'patch'")
+
+
+def parse_exception(e):
+    """ff_utils functions raise an exception when the expected code is not returned.
+    This response is a pre-formatted text, and this function will get the resonse json
+    out of it. [Adapted from Submit4DN]"""
+    try:
+        # try parsing the exception
+        text = e.args[0]
+        resp_text = text[text.index('{'):-1]
+        resp_text = json.loads(resp_text.replace('\\', ''))
+        resp_list = [error['description'] for error in resp_text['errors']]
+        return resp_list
+    # if not re-raise
+    except:  # pragma: no cover
+        raise e
+
+
+def patch_item_data():
+    pass
+
+
+def post_item_data():
+    pass
+
+
+# NOT FINISHED
+def validate_and_post(virtualapp, json_data, dryrun=False):
     '''
     Still in progress, not necessarily functional yet. NOT YET TESTED.
 
@@ -163,22 +263,19 @@ def validate_and_post(testapp, json_data, dryrun=False):
     '''
     alias_dict = {}
     links = ['samples', 'members', 'mother', 'father', 'proband']
+    errors = []
+    json_data_final = {'post': {}, 'patch': {}}
     for itemtype in POST_ORDER:
-        profile = testapp.get('/profiles/{}.json'.format(itemtype))
+        profile = virtualapp.get('/profiles/{}.json'.format(itemtype))
         for alias in results[itemtype]:
-            try:
-                # check if already in db
-                result = testapp.get(alias + '/?frame=object')
-            except Exception as e:
-                # post if not in db
-                if 'HTTPNotFound' in str(e):
-                    validation = testapp.post_json('/{}/?checkonly=True'.format(itemtype), results[itemtype][alias], status=201)
-                    if not validation:  # modify to check for presence of validation errors
-                        # do something to report validation errors
-                        pass
-                    elif not dryrun:  # post
-                        response = testapp.post_json('/' + itemtype, results[itemtype][alias], status=201)
-                        # do something to record response
+            result = compare_with_db(alias)
+            if not result:
+                error = validate_item(results[itemtype][alias], 'post', itemtype)
+                if error:  # modify to check for presence of validation errors
+                    # do something to report validation errors
+                    errors.append(error)
+                else:
+                    json_data_final['post'].setdefault(itemtype, default=[]).append(results[itemtype][alias])
             else:
                 # patch if item exists in db
                 alias_dict[alias] = result['@id']
@@ -203,22 +300,47 @@ def validate_and_post(testapp, json_data, dryrun=False):
                             val = result.get(field, [])
                             val.extend(results[itemtype][alias][field])
                             to_patch[field] = list(set(val))
-                validation = testapp.patch_json(result['@id'] + '/?checkonly=True', to_patch, status=200)
-                if not validation:  # modify to check for presence of validation errors
+                error = validate_item(to_patch, 'post', itemtype, atid=result['@id'])
+                if error:  # modify to check for presence of validation errors
                     # do something to report validation errors
-                    pass
-                elif not dryrun:  # patch
-                    response = testapp.patch_json('/' + itemtype, results[itemtype][alias], status=201)
+                    errors.append(error)
+                else:  # patch
+                    json_data_final['patch'][result['@id']] = to_patch
                     # do something to record response
+    if errors:
+        return errors
+    output = []
+    item_names = {'individual': 'individual_id', 'family': 'family_id', 'sample': 'specimen_id'}
+    if json_data_final['post']:
+        for k, v in json_data_final['post'].items():
+            # also create Case and Report items for each SampleProcessing item created
+            for item in v:
+                for field in links:
+                    if field in item:
+                        json_data_final['patch'][item['aliases'][0]] = item[field]
+                        del item[field]
+                try:
+                    response = virtualapp.post_json('/' + k, item, status=201)
+                    aliasdict[item['aliases'][0]] = response.json['@graph'][0]['@id']
+                    if response.json['status'] == 'success' and k in item_names:
+                        output.append('Success - {} {} posted'.format(k, item[item_names[k]]))
+                except Exception:
+                    pass
+    for k, v in json_data_final['patch'].items():
+        atid = k if k.startswith('/') else aliasdict[k]
+        try:
+            response = testapp.patch_json(atid, v, status=200)
+        except Exception:
+            pass
 
 
 # This was just to see if i could post something using testapp in the python command line, currently works.
-def test_function():
-    app = get_app('development.ini', 'app')
-    environ = {'HTTP_ACCEPT': 'application/json', 'REMOTE_USER': 'TEST'}
-    testapp = TestApp(app, environ)
-    response = testapp.post_json('/project', {'name': 'test', 'title': 'Test'}, status=201)
-    print(response)
+# def test_function():
+#     app = get_app('development.ini', 'app')
+#     environ = {'HTTP_ACCEPT': 'application/json', 'REMOTE_USER': 'TEST'}
+#     testapp = TestApp(app, environ)
+#     response = testapp.post_json('/project', {'name': 'test', 'title': 'Test'}, status=201)
+#     print(response)
 
 
 def cell_value(cell, datemode):
