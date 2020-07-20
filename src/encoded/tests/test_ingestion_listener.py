f1aea12deb61530492c74c7d255cc9752411e093
William Ronchetti <wrr33@cornell.edu>
2020-07-20 09:39:18 -0400

    fix small test comment

--- src/encoded/tests/test_ingestion_listener.py
+++ src/encoded/tests/test_ingestion_listener.py
@@ -151,7 +151,8 @@ def test_ingestion_listener_run(testapp, mocked_vcf_file, gene_workbook, setup_a
         return current_time < (start_time + end_delta)
 
     # XXX: This is a really hard thing to test, but take my word for it that this is doing "something" -Will
-    #      If you do not get ValueError here, it means
+    #      If you do not get ValueError here, it means the VCF wasn't processed in the run method or a different
+    #      error occurred.
     with mock.patch('encoded.ingestion_listener.IngestionListener.should_remain_online',
                     new=mocked_should_remain_online):
         with pytest.raises(ValueError):
