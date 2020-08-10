f495974bb57c175a92b013123c3146f39072a727
aschroed <andrew_schroeder@hms.harvard.edu>
2020-08-10 17:24:36 -0400

    additional code specific and shared for g3d

--- src/encoded/commands/parse_monarch_g2d.py
+++ src/encoded/commands/parse_monarch_g2d.py
@@ -32,62 +32,60 @@ from ..commands.parse_hpoa import (
     get_items_from_db_keyed_by_field,
     get_input_gen,
     get_header_info_and_field_name_line,
-    get_fields_from_line
+    get_fields_from_line,
+    compare_existing_to_newly_generated,
+
 )
 from ..commands.load_items import load_items
 
 ''' URL for fetching the human gene to disorder annotation file from monarch gene_disease.9606.tsv
 '''
 MONARCH_G2D_URL = 'https://archive.monarchinitiative.org/latest/tsv/gene_associations/gene_disease.9606.tsv'
-
-''' Fields in a Gene Item that are can be used to map rows in the Monarch gene to disorder file
+DATASOURCE = 'Monarch'
+''' Fields in the annotation file or gene items that can be used for mapping file rows to Gene Items
+    from_gene has prefixes used in annotation file for those IDs.
 '''
 GENEID_FIELDS = {
-    'gene_symbol': None,
-    'hgnc_id': 'HGNC',
-    'entrez_id': 'NCBIGene',
+    'from_file': ['subject', 'subject_label'],
+    'from_gene': [('hgnc_id', 'HGNC'), ('entrez_id', 'NCBIGene'), ('gene_symbol', None)]
 }
-
-
 ''' Dictionary for field mapping between gene_disease file from monarch and Gene2Disorder Item
 '''
 FIELD_MAPPING = {
-    'subject': 'using_gene_id',
-    'DiseaseName': 'unused field',
-    'Qualifier': 'is_not',
-    'HPO_ID': 'phenotype',
-    'Reference': 'attribution_id',
-    'Evidence': 'evidence_code',
-    'Onset': 'onset',
-    'Frequency': ['frequency_term', 'frequency_value'],
-    'Sex': 'affected_sex',
-    'Modifier': 'modifier',
-    'Aspect': 'aspect',
-    'Biocuration': 'curation_history'
+    'subject': {'field_name': 'using_gene_id'},
+    'relation_label': {'field_name': 'relationship_name'},
+    'evidence_label': {
+        'field_name': 'evidence_class',
+        'is_array': True,
+        'separator': '|'
+    },
+    'source': {
+        'field_name': 'attributed_pubs',
+        'is_array': True,
+        'separator': '|'
+    },
+    'is_defined_by': {
+        'field_name': 'original_source',
+        'is_array': True,
+        'separator': '|',
+        'parse': True
+    }
 }
 
-''' Field that needs to be skipped or only used for logging missing info
-'''
-DIS_NAME_FIELD_FROM_INPUT = 'DiseaseName'
 
-''' Relationship to use in the Evidence Item for making evidence links between ITEMS
-'''
-RELATION = 'associated with'
-
-
-def get_fields_for_item_added_by_file():
-    ''' returns a list of all the property names of the item that are generated
-        from the file - omits calc prop and other user editable fields like clinic_notes
-        fields will be used to compare dbterms to terms from file
-    '''
-    item_fields = ['subject_item', 'object_item', 'relationship_name']
-    to_add = [v for k, v in FIELD_MAPPING.items() if k not in ['Frequency', 'DiseaseName', 'HPO_ID']]
-    item_fields.extend(to_add)
-    item_fields.extend(FIELD_MAPPING.get('Frequency'))
-    return item_fields
-
-
-ITEM_FIELDS = get_fields_for_item_added_by_file()
+# def get_fields_for_item_added_by_file():
+#     ''' returns a list of all the property names of the item that are generated
+#         from the file - omits calc prop and other user editable fields like clinic_notes
+#         fields will be used to compare dbterms to terms from file
+#     '''
+#     item_fields = ['subject_item', 'object_item', 'relationship_name']
+#     to_add = [v for k, v in FIELD_MAPPING.items() if k not in ['Frequency', 'DiseaseName', 'HPO_ID']]
+#     item_fields.extend(to_add)
+#     item_fields.extend(FIELD_MAPPING.get('Frequency'))
+#     return item_fields
+#
+#
+# ITEM_FIELDS = get_fields_for_item_added_by_file()
 
 
 def get_logger(lname, logfile):
@@ -136,9 +134,10 @@ def get_gene2_altid_map(genes):
         the correct gene item to use in link
         NOTE: using hardcoded fields in a defined list
     '''
+    gfield2pre = {v[0]: v[1] for v in GENEID_FIELDS.get('from_gene', [])}
     id2geneuid = {}
     for guid, g in genes.items():
-        for f, pre in GENEID_FIELDS.items():
+        for f, pre in gfield2pre.items():
             value = g.get(f)
             if value:
                 if pre:
@@ -153,17 +152,16 @@ def line2list(line):
     return [d.strip() for d in line.split('\t')]
 
 
-def find_disorder_uid_using_file_id(data, xref2disorder):
-    """ gets the database ID used in annotation file and maps to existing disorder uuid
+def find_gene_uid_from_file_fields(data, ids2geneuid):
+    """ searches IDs (from a couple of different fields using preferred ids)
+        used in annotation file and maps to existing gene uuid
+        will use the first found
     """
-    using_id = data.get('DatabaseID')
-    if not using_id:
-        return
-    if using_id.startswith('ORPHA'):
-        map_id = using_id.replace('ORPHA', 'Orphanet')
-    else:
-        map_id = using_id
-    return xref2disorder.get(map_id)
+    for field in GENEID_FIELDS.get('from_file', []):
+        id2chk = data.get(field)
+        geneid = ids2geneuid.get(id2chk)
+        if geneid:
+            return geneid
 
 
 def check_hpo_id_and_note_problems(fname, hpoid, hpoid2uuid, problems):
@@ -184,67 +182,54 @@ def check_hpo_id_and_note_problems(fname, hpoid, hpoid2uuid, problems):
     return None
 
 
-def create_evi_annotation(data, hpoid2uuid, problems):
-    """ Looks at all the fields in the annotation line and
-        transforms to EvidenceDisPheno format
+def _parse_vals(vals):
+    pvals = []
+    for v in vals:
+        _, src = v.rsplit('/', 1)
+        src = src.replace('#', '')
+        pvals.append(src)
+    return pvals
+
+
+def create_gene2disorder_evi_annotation(data, problems, dt_time):
+    """ Looks at all the fields in the data line and
+        transforms to GeneDisorderEvidence format
+        fields we care about
+        * datasource - constant Monarch
+        * datasource_version - date run
+        * subject_item - gene uuid obtained by a function from subject or subject_label
+        * object_item - disorder uuid obtained by a function from object (mondo id)
+        * subject -> using_gene_id
+        * relation_label -> relationship_name
+        * evidence_label (| separated values) -> evidence_class items
+        * source (| separated PMIDS) -> attibuted_pubs items
+        * is_defined_by (| separated url form) -> original_source items
     """
-    HP_REGEX = re.compile('^HP:[0-9]{7}')
-    pheno_annot = {}
+    g2d_annot = {'datasource': DATASOURCE, 'datasource_version': dt_time}
     for f, v in data.items():
-        if not v or (f == DIS_NAME_FIELD_FROM_INPUT):
+        vals = []
+        finfo = {}
+        if not v:  # skip empty fields
             continue
         if f in ['subject_item', 'object_item']:
-            cgf = f
-        elif f == 'Frequency':
-            if v.startswith('HP:'):
-                cgf = FIELD_MAPPING[f][0]
+            g2d_annot[f] = v
+        elif f in FIELD_MAPPING:
+            finfo = FIELD_MAPPING.get(f)
+            if finfo.get('is_array'):
+                vals = [val.strip() for val in v.split(finfo.get('separator'))]
             else:
-                cgf = FIELD_MAPPING[f][1]
-        else:
-            cgf = FIELD_MAPPING[f]
+                vals = [v.strip()]
 
-        if f == 'Qualifier':
-            v = True
-        elif f == 'Sex':
-            v = v[0:1].upper()
+            if finfo.get('parse'):
+                # for now we only have special parsing with the url field so this
+                # is not a general case
+                vals = _parse_vals(vals)
+        if vals and finfo:
+            if not finfo.get('is_array'):
+                vals = vals[0]
+            g2d_annot[finfo.get('field_name')] = vals
 
-        if isinstance(v, str) and HP_REGEX.match(v):
-            hpuid = check_hpo_id_and_note_problems(f, v, hpoid2uuid, problems)
-            if not hpuid:
-                continue
-            else:
-                v = hpuid
-        pheno_annot[cgf] = v
-    return pheno_annot
-
-
-def compare_existing_to_newly_generated(logger, connection, evidence_items, itype):
-    """ gets all the existing evidence items from database and compares to all the newly
-        generated ones from annotations and if found removes from list
-    """
-    sq = 'search/?type={}&status!=obsolete'.format(itype)
-    logger.info("COMPARING FILE ITEMS WITH CURRENT DB CONTENT")
-    logger.info("searching: {}".format(datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")))
-    dbitems = search_metadata(sq, connection, is_generator=True, page_limit=500)
-    existing = 0
-    uids2obsolete = []
-    logger.info("comparing: {}".format(datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")))
-    for db_evi in dbitems:
-        # import pdb; pdb.set_trace()
-        tochk = convert2raw(db_evi)
-        if tochk in evidence_items:
-            existing += 1
-            evidence_items.remove(tochk)
-        else:
-            uids2obsolete.append(db_evi.get('uuid'))
-    logger.info("result: {}".format(datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ")))
-    return evidence_items, existing, uids2obsolete
-
-
-def convert2raw(item):
-    # need to remove properties not generated from file eg. calc props and others
-    stripped_item = {k: v for k, v in item.items() if k in ITEM_FIELDS}
-    return get_raw_form(stripped_item)
+    return g2d_annot
 
 
 def log_problems(logger, problems):
@@ -322,6 +307,9 @@ def main():  # pragma: no cover
     ids2genes = get_gene2_altid_map(genes)
     # this bit just converts to simple 1:1 mapping Dictionary
     ids2genes = {k: v[0] for k, v in ids2genes.items()}
+    # lookup table key mondo id - value disorder uuid
+    mondo2disuid = {k: v.get('uuid') for k, v in disorders.items()}
+
     # bit to check which id maps to more than one gene - currently there are
     # 16 symbol to 2 uuid - genes should be merged but for now just take the first
     # one to make associations
@@ -345,7 +333,9 @@ def main():  # pragma: no cover
     # in this case no header and first line is field names
     field_line = get_header_info_and_field_name_line(lines, None, logger)
     fields = get_fields_from_line(field_line, logger)  # currently don't have a mapping
-    import pdb; pdb.set_trace()
+    if not fields:
+        logger.warn("Can't get field names - bailing!")
+        sys.exit(1)
 
     for line in lines:
         if line.startswith("#"):
@@ -353,30 +343,27 @@ def main():  # pragma: no cover
         data_list = line2list(line)
         data = dict(zip(fields, data_list))
 
-        # find the  disorder_uuid to refer to subject_item
-        disorder_id = find_disorder_uid_using_file_id(data, xref2disorder)
-        if not disorder_id:
-            problems.setdefault('no_map', []).append(data)
+        # find the gene uuid
+        gene_uid = find_gene_uid_from_file_fields(data, ids2genes)
+        if not gene_uid:
+            problems.setdefault('no_gene_in_cgap', []).append(data)
             continue
-        data['subject_item'] = disorder_id
+        data['subject_item'] = gene_uid
 
-        # and the HPO_ID to refer to object_item
-        hpo_id = data.get('HPO_ID')
-        phenotype_id = check_hpo_id_and_note_problems('HPO_ID', hpo_id, hpoid2uuid, problems)
-        if not phenotype_id:
-            # missing phenotype
+        # and the disorder uuid - based on MONDO id in object
+        disorder_uid = mondo2disuid.get(data.get('object'))
+        if not disorder_uid:
+            problems.setdefault('no_disorder_in_cgap', []).append(data)
             continue
-        data['object_item'] = phenotype_id
-        del data['HPO_ID']
+        data['object_item'] = disorder_uid
 
-        pheno_annot = create_evi_annotation(data, hpoid2uuid, problems)
+        g2d_evi = create_gene2disorder_evi_annotation(data, problems, start)
 
-        if pheno_annot:
-            pheno_annot['relationship_name'] = RELATION
-            if pheno_annot in evidence_items:
+        if g2d_evi:
+            if g2d_evi in evidence_items:
                 problems.setdefault('redundant_annot', []).append(pheno_annot)  # (data, dis2pheno[ppos]))
                 continue
-            evidence_items.append(pheno_annot)
+            evidence_items.append(g2d_evi)
 
     logger.info("after parsing annotation file we have {} evidence items".format(len(evidence_items)))
 
