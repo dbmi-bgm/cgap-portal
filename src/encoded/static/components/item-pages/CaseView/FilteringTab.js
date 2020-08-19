6f28490ad8ee87435ce0cb26ae01df0a51be75ec
Alex Balashov <alexander_balashov@hms.harvard.edu>
2020-08-19 16:47:16 -0400

    Minor comment edit

--- src/encoded/static/components/item-pages/CaseView/FilteringTab.js
+++ src/encoded/static/components/item-pages/CaseView/FilteringTab.js
@@ -137,14 +137,9 @@ export function FilteringTabSubtitle(props){
             "@id" : caseInstitutionID
         }
     } = caseItem;
-    const {
-        "@id" : activeFilterSetID,
-        display_title: activeFilterSetTitle,
-        //filter_blocks = []
-    } = active_filterset || {};
 
     const [ isLoading, setIsLoading ] = useState(false);
-    // From `state.lastFilterSetSaved` we use only `date_created` so doesn't matter if frame=object vs frame=page for it.
+    // From `state.lastFilterSetSaved` we use only non linkTo properties from it so doesn't matter if frame=object vs frame=page for it.
     // `undefined` means not ever set or removed previously vs `null` means explicitly nothing set in current session.
     const [ lastFilterSetSaved, setLastFilterSetSaved ] = useState(active_filterset || undefined);
 
