3099469c7641c19774ece43e73c8cc11c97fd7b5
William Ronchetti <wrr33@cornell.edu>
2020-08-20 16:08:17 -0400

    C4-255 continue porting/adjusting buraks code

--- src/encoded/inheritance_mode.py
+++ src/encoded/inheritance_mode.py
@@ -1,6 +1,24 @@
+class InheritanceModeError(Exception):
+    pass
+
+
 class InheritanceMode:
 
-    EMPTY = '.'
+    EMPTY = '.'  # XXX: is this really what this is?
+
+    CHROMOSOMES = [
+        '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14',
+        '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y', 'M'
+    ]
+
+    MALE = 'M'
+    FEMALE = 'F'
+    SEXES = [MALE, FEMALE]  # XXX: any additional?
+
+    MOTHER = 'mother'
+    FATHER = 'father'
+    SELF = 'self'
+    TRIO = [MOTHER, FATHER, SELF]
 
     # Genotype labels
     GENOTYPE_LABEL_DOT = "Missing"
@@ -37,23 +55,37 @@ class InheritanceMode:
     INHMODE_LABEL_NONE_BOTH_PARENTS = "Low relevance, present in both parent(s)"
     INHMODE_LABEL_NONE_OTHER = "Low relevance, other"
 
+    @staticmethod
+    def is_sex_chromosome(chrom):
+        return chrom == 'X' or chrom == 'Y'
+
+    @staticmethod
+    def alleles_match(a1, a2, is_zero=False):
+        """ Returns True if a1==a1 and additionally if a1==a2==0 if is_zero is True """
+        if is_zero:
+            return a1 == 0 and a2 == 0
+        return a1 == a2
+
     @classmethod
     def is_multiallelic_site(cls, genotypes):
         """ Returns true of any genotype in the list of given genotypes has allele number 2 or greater
 
+        :precondition: genotypes is list in format in example where all elements conform
         :param genotypes: a list of genotypes to analyze ex: ["0/0", "1/1"]
-        :returns: True if multiallelic site, False otherwise
+        :returns: True if multi-allelic site, False otherwise
         """
+        if genotypes is None:
+            raise InheritanceModeError('Passed None to is_multiallelic_site')
         for gt in genotypes:
             allele1, allele2 = gt.split('/')
             if allele1 == cls.EMPTY:
                 continue
-            elif int(allele1) > 1 or int(allele2) > 1:
+            elif int(allele1) > 1 or int(allele2) > 1:  # could fail here
                 return True
         return False
 
     @classmethod
-    def select_genotype_label(cls, gt, sex, chrom):
+    def compute_genotype_label(cls, *, gt, sex, chrom):  # XXX: force keyword args for clarity
         """ Given the genotype, sex and chromosome determine the genotype label
 
         :param gt: single genotype to analyze ex: "0/0"
@@ -62,36 +94,150 @@ class InheritanceMode:
         :returns: genotype label
         """
         allele1, allele2 = gt.split('/')
+        if sex not in cls.SEXES:
+            raise InheritanceModeError('Bad sex given to compute_genotype_label: %s' % sex)
+        if chrom not in cls.CHROMOSOMES:
+            raise InheritanceModeError('Bad chromosome given to compute_genotype_label: %s' % chrom)
 
         # XXX: document what this is doing, refactor into descriptive helper method
         if allele1 == cls.EMPTY:
-            if sex == 'F' and chrom == 'Y':
+            if sex == cls.FEMALE and chrom == cls.MALE:
                 return cls.GENOTYPE_LABEL_FEMALE_CHRY
             return cls.GENOTYPE_LABEL_DOT
 
         # cast to int since we no longer need strings ?
-        allele1, allele2 = int(allele1), int(allele2)
+        try:
+            allele1, allele2 = int(allele1), int(allele2)
+        except ValueError as e:
+            raise InheritanceModeError('Bad genotype given to compute_genotype_label: %s' % e)
 
         # XXX: document what this is doing, refactor into descriptive helper method
-        if sex == 'F' and chrom == 'Y':
-            if allele1 == 0 and allele2 == 0:
+        if sex == cls.FEMALE and chrom == 'Y':
+            if cls.alleles_match(allele1, allele2, is_zero=True):
                 return cls.GENOTYPE_LABEL_FEMALE_CHRY
             return cls.GENOTYPE_LABEL_SEX_INCONSISTENT
 
         # XXX: document what this is doing, refactor into descriptive helper method
-        if sex == "male" and chrom in ['X', 'Y']:
-            if allele1 != allele2:
+        if sex == cls.MALE and cls.is_sex_chromosome(chrom):
+            if not cls.alleles_match(allele1, allele2):
                 return cls.GENOTYPE_LABEL_SEX_INCONSISTENT
             if allele1 == 0:
                 return cls.GENOTYPE_LABEL_0
             return cls.GENOTYPE_LABEL_M
 
         # XXX: document what this is doing, refactor into descriptive helper method
-        if allele1 == 0 and allele2 == 0:
+        if cls.alleles_match(allele1, allele2, is_zero=True):
             return cls.GENOTYPE_LABEL_00
-        if allele1 == 0:
+        elif allele1 == 0:
             return cls.GENOTYPE_LABEL_0M
-        if allele1 == allele2:
+        elif allele1 == allele2:
             return cls.GENOTYPE_LABEL_MM
 
         return cls.GENOTYPE_LABEL_MN
+
+    @classmethod
+    def compute_family_genotype_labels(cls, genotypes, sexes, chrom):
+        """ Computes family genotype labels for all keys present in genotypes. Assumes consistent
+            structure amongst genotypes and sexes.
+
+        :param genotypes: dictionary of role -> genotype mappings
+        :param sexes: dictionary of role -> sex mappings
+        :param chrom: relevant chromosome
+        :returns: genotype_labels, dictionary of role -> list of genotype labels
+        """
+        genotype_labels = {}
+        for role, genotype in genotypes.items():
+            sex = sexes[role]
+            genotype_labels[role] = [cls.compute_genotype_label(gt=genotype, sex=sex, chrom=chrom)]
+
+        # handle multi-allelic site
+        if cls.is_multiallelic_site(genotypes.values()):
+            for role, labels in genotype_labels.items():
+                if cls.GENOTYPE_LABEL_MN_KEYWORD not in labels:
+                    labels.append(cls.GENOTYPE_LABEL_MN_ADDON)
+        return genotype_labels
+
+    @staticmethod
+    def check_if_label_exists(label_to_find, labels_to_search):
+        """ Helper for below method that checks if a given label exists in any sub lists """
+        for labels in labels_to_search.values():
+            if label_to_find in labels:
+                return True
+        return False
+
+    @staticmethod
+    def mother_father_ref_ref(mother_gt, father_gt):
+        """ Returns true if both mother and father are ref/ref (0/0) """
+        return mother_gt == '0/0' and father_gt == '0/0'
+
+    @classmethod
+    def compute_inheritance_mode_trio(cls, *, genotypes, genotype_labels, sexes, chrom, novoPP):
+        """ Computes inheritence modes for the trio of 'self', 'mother', 'father'.
+
+        :param genotypes: dictionary of role -> genotype mappings
+        :param genotype_labels: dictionary of role -> genotype_label mappings
+        :param sexes: dictionary of role -> sex mappings
+        :param chrom: relevant chromosome
+        :param novoPP: novoCaller post-posterior probability (likely de novo), takes precedence
+        :returns: list of inheritance modes
+        """
+        # validate precondition
+        for d in [genotypes, genotype_labels, sexes]:
+            for role in cls.TRIO:
+                assert role in d
+
+        if (cls.check_if_label_exists(cls.GENOTYPE_LABEL_DOT, genotype_labels) or
+                cls.is_multiallelic_site(genotypes.values()) or
+                cls.check_if_label_exists(cls.GENOTYPE_LABEL_SEX_INCONSISTENT, genotype_labels)):
+            return []
+
+        # XXX: determine de novo (extract into helper?)
+        if novoPP > 0.9:
+            return [cls.INHMODE_LABEL_DE_NOVO_STRONG]
+        if novoPP > 0.1:
+            return [cls.INHMODE_LABEL_DE_NOVO_MEDIUM]
+        if (cls.mother_father_ref_ref(genotypes[cls.MOTHER], genotypes[cls.FATHER]) and
+                genotypes[cls.SELF] == '0/1' and chrom == 'autosome'):  # XXX: since when can chrom have value 'autosome'?
+            return [cls.INHMODE_LABEL_DE_NOVO_WEAK]
+        if (cls.mother_father_ref_ref(genotypes[cls.MOTHER], genotypes[cls.FATHER])
+                and ((genotypes[cls.SELF] == '0/1' and sexes[cls.SELF] == cls.FEMALE and chrom == cls.MALE)
+                     or (genotypes[cls.SELF] == '1/1' and sexes[cls.SELF] == cls.MALE and chrom != 'autosome'))):
+            if novoPP == 0:
+                return [cls.INHMODE_LABEL_DE_NOVO_WEAK]
+            if novoPP == -1:
+                return [cls.INHMODE_LABEL_DE_NOVO_CHRXY]
+            raise ValueError("novoPP is different from 0 or -1 on sex chromosome: %s" % novoPP)
+
+        # XXX: after the above, we should have some comments talking about what each of these
+        # are doing (and potentially extract them out).
+        if (genotypes[cls.MOTHER] == "0/0"
+                and genotype_labels[cls.FATHER] == cls.GENOTYPE_LABEL_0M
+                and genotypes[cls.SELF] == "0/1"):
+            return [cls.INHMODE_DOMINANT_FATHER]
+
+        if (genotypes[cls.MOTHER] == "0/1" and genotypes[cls.FATHER] == "0/0"
+                and genotypes[cls.SELF] == "0/1"):
+            return [cls.INHMODE_DOMINANT_MOTHER]
+
+        if (genotypes[cls.MOTHER] == "0/1" and genotypes[cls.FATHER] == "0/1"
+                and genotypes[cls.SELF] == "1/1"):
+            return [cls.INHMODE_LABEL_RECESSIVE]
+
+        if (genotypes[cls.MOTHER] == "0/1" and genotypes[cls.FATHER] == "0/0"
+                and genotypes[cls.SELF] == "1/1" and sexes[cls.SELF] == cls.MALE and chrom == 'X'):
+            return [cls.INHMODE_LABEL_X_LINKED_RECESSIVE_MOTHER, cls.INHMODE_LABEL_X_LINKED_DOMINANT_MOTHER]
+
+        if (genotypes[cls.MOTHER] == "0/0" and genotype_labels[cls.FATHER] == cls.GENOTYPE_LABEL_M and
+                chrom == 'X' and genotype_labels[cls.SELF] in [cls.GENOTYPE_LABEL_M, cls.GENOTYPE_LABEL_0M]):
+            return [cls.INHMODE_LABEL_X_LINKED_DOMINANT_FATHER]
+
+        if (genotype_labels[cls.FATHER] == cls.GENOTYPE_LABEL_M and
+                chrom == 'Y' and genotype_labels[cls.SELF] == cls.GENOTYPE_LABEL_M):
+            return [cls.INHMODE_LABEL_Y_LINKED]
+
+        if (((genotypes[cls.MOTHER] == "0/1" and genotypes[cls.FATHER] == "0/0") or
+             (genotypes[cls.MOTHER] == "0/0" and genotypes[cls.FATHER] == "0/1"))
+                and genotypes[cls.SELF] == "1/1"):
+            return [cls.INHMODE_LABEL_LOH]
+
+        return []
--- src/encoded/tests/test_inheritance_mode.py
+++ src/encoded/tests/test_inheritance_mode.py
@@ -1,5 +1,11 @@
 import pytest
-from ..inheritance_mode import InheritanceMode
+from ..inheritance_mode import InheritanceMode, InheritanceModeError
+
+
+def test_is_multiallelic_site_raises_error():
+    """ Tests cases that should raise error. """
+    with pytest.raises(InheritanceModeError):
+        InheritanceMode.is_multiallelic_site(None)
 
 
 @pytest.mark.parametrize('gts', [
@@ -7,10 +13,11 @@ from ..inheritance_mode import InheritanceMode
     ['0/0'],
     ['0/1'],
     ['1/1'],
-    ['1/0']
+    ['1/0'],
+    ['0/0', './.', '1/0']
 ])
-def test_inheritance_mode_is_multiallelic_site_is_false(gts):
-    """ Tests basic truths about the multiallelic site function """
+def test_is_multiallelic_site_is_false(gts):
+    """ Tests cases that should return False """
     assert InheritanceMode.is_multiallelic_site(gts) is False
 
 
@@ -22,6 +29,113 @@ def test_inheritance_mode_is_multiallelic_site_is_false(gts):
     ['4/17'],
     ['0/0', '2/2']
 ])
-def test_inheritance_mode_is_multiallelic_site_is_true(gts):
-    """ Tests basic truths about the multiallelic site function """
+def test_is_multiallelic_site_is_true(gts):
+    """ Test cases that should return True """
     assert InheritanceMode.is_multiallelic_site(gts) is True
+
+
+@pytest.mark.parametrize('gt, sex, chrom', [
+    ('4/D', 'M', 'X'),
+    ('1/1', 'F', 'D'),
+    ('./.', 'R', '4')
+])
+def test_compute_genotype_label_raises_error(gt, sex, chrom):
+    """ Tests error cases """
+    with pytest.raises(InheritanceModeError):
+        InheritanceMode.compute_genotype_label(gt=gt, sex=sex, chrom=chrom)
+
+
+@pytest.mark.parametrize('gt, sex, chrom, expected', [
+    ('./.', 'F', 'Y', InheritanceMode.GENOTYPE_LABEL_FEMALE_CHRY),
+    ('./.', 'F', '3', InheritanceMode.GENOTYPE_LABEL_DOT),
+    ('1/1', 'F', 'Y', InheritanceMode.GENOTYPE_LABEL_SEX_INCONSISTENT),
+    ('0/0', 'F', 'Y', InheritanceMode.GENOTYPE_LABEL_FEMALE_CHRY),
+    ('1/0', 'F', 'Y', InheritanceMode.GENOTYPE_LABEL_SEX_INCONSISTENT),
+    ('0/1', 'F', 'Y', InheritanceMode.GENOTYPE_LABEL_SEX_INCONSISTENT),
+    ('0/1', 'M', 'X', InheritanceMode.GENOTYPE_LABEL_SEX_INCONSISTENT),
+    ('0/1', 'M', 'Y', InheritanceMode.GENOTYPE_LABEL_SEX_INCONSISTENT),
+    ('0/0', 'M', 'Y', InheritanceMode.GENOTYPE_LABEL_0),
+    ('0/0', 'M', 'X', InheritanceMode.GENOTYPE_LABEL_0),
+    ('1/1', 'M', 'Y', InheritanceMode.GENOTYPE_LABEL_M),
+    ('1/1', 'M', 'X', InheritanceMode.GENOTYPE_LABEL_M),
+    ('0/0', 'M', '1', InheritanceMode.GENOTYPE_LABEL_00),
+    ('0/1', 'M', '1', InheritanceMode.GENOTYPE_LABEL_0M),
+    ('1/1', 'M', '1', InheritanceMode.GENOTYPE_LABEL_MM),
+    ('2/1', 'M', '1', InheritanceMode.GENOTYPE_LABEL_MN),
+    ('0/0', 'M', '5', InheritanceMode.GENOTYPE_LABEL_00),
+    ('0/1', 'M', '3', InheritanceMode.GENOTYPE_LABEL_0M),
+    ('1/1', 'M', '21', InheritanceMode.GENOTYPE_LABEL_MM),
+    ('2/1', 'M', '9', InheritanceMode.GENOTYPE_LABEL_MN),
+])
+def test_compute_genotype_label(gt, sex, chrom, expected):
+    assert InheritanceMode.compute_genotype_label(gt=gt, sex=sex, chrom=chrom) == expected
+
+
+@pytest.mark.parametrize('gts, sexes, chrom, expected_labels', [
+    (  # test case 1
+        {
+            'self': '1/1',
+            'mother': '1/0',
+            'father': '0/1'
+        },
+        {
+            'self': 'M',
+            'mother': 'F',
+            'father': 'M'
+        },
+        '5',
+        {
+            'self': ['Homozygus alternate'],
+            'mother': ['Heterozygous alt/alt -  multiallelic'],
+            'father': ['Heterozygous']
+        }
+    ),
+    (  # test case 2
+        {
+            'self': '1/1',
+            'mother': '1/1',
+            'father': '0/1'
+        },
+        {
+            'self': 'F',
+            'mother': 'F',
+            'father': 'M'
+        },
+        'X',
+        {
+            'self': ['Homozygus alternate'],
+            'mother': ['Homozygus alternate'],
+            'father': ['False']
+        }
+    ),
+    (  # test case 3
+        {
+            'self': '0/1',
+            'mother': '1/1',
+            'father': '0/0'
+        },
+        {
+            'self': 'F',
+            'mother': 'F',
+            'father': 'M'
+        },
+        'X',
+        {
+            'self': ['Heterozygous'],
+            'mother': ['Homozygus alternate'],
+            'father': ['Hemizygous reference']
+        }
+    )
+])
+def test_compute_family_genotype_labels(gts, sexes, chrom, expected_labels):
+    """ Tests a few family genotype labels - there are a lot of ways this can go, so look
+        to automate testing somehow from a collection of examples. """
+    actual_labels = InheritanceMode.compute_family_genotype_labels(gts, sexes, chrom)
+    for key, value in actual_labels.items():
+        assert expected_labels[key] == value
+    for key, value in expected_labels.items():
+        assert actual_labels[key] == value
+
+
+def test_compute_inheritance_mode_trio():
+    pass  # write me!
