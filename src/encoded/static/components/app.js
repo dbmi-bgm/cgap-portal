052f0107d54172a9d8a4ed43c638885160489dcf
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2020-11-19 09:24:38 -0500

    fix cannot read property 'apply' of null bug

--- src/encoded/static/components/app.js
+++ src/encoded/static/components/app.js
@@ -170,6 +170,7 @@ export default class App extends React.PureComponent {
 
         // Save navigate fxn and other req'd stuffs to GLOBAL navigate obj.
         // So that we may call it from anywhere if necessary without passing through props.
+        navigate.initializeFromApp(this);
         navigate.setNavigateFunction(this.navigate);
         navigate.registerCallbackFunction(Alerts.updateCurrentAlertsTitleMap.bind(this, null));
 
