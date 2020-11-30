ad3f8d54fcedce7347fef5750c7743e66d3a0bff
Sarah <sarah_reiff@hms.harvard.edu>
2020-11-30 17:18:08 -0500

    edits to note schema/class

--- src/encoded/types/note.py
+++ src/encoded/types/note.py
@@ -35,11 +35,10 @@ class NoteStandard(Note):
 
     item_type = 'note_standard'
     schema = load_schema('encoded:schemas/note_standard.json')
-    embedded_list = Note.embedded_list
 
     @calculated_property(schema={
         "title": "Display Title",
-        "description": "Individual's Identifier",
+        "description": "Note's display title",
         "type": "string"
     })
     def display_title(self, date_created):
@@ -62,11 +61,10 @@ class NoteInterpretation(Note):
 
     item_type = 'note_interpretation'
     schema = load_schema('encoded:schemas/note_interpretation.json')
-    embedded_list = Note.embedded_list
 
     @calculated_property(schema={
         "title": "Display Title",
-        "description": "Individual's Identifier",
+        "description": "Note's display title",
         "type": "string"
     })
     def display_title(self, date_created):
