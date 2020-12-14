12b23f3fc1f36a39bceb5b4a5424acb7dabd3e42
Sarah <sarah_reiff@hms.harvard.edu>
2020-12-14 11:59:14 -0500

    docstrings added for note tests

--- src/encoded/tests/test_types_note.py
+++ src/encoded/tests/test_types_note.py
@@ -30,51 +30,66 @@ def new_standard_note():
     }
 
 
+def post_note(testapp, item, note_type='note_interpretation', expected_status=201):
+    """ helper function for posting notes in tests """
+    return testapp.post_json('/' + note_type, item, status=expected_status)
+
+
 def test_note_interpretation_note_link(workbook, testapp):
+    """ test previous_note linkTo works on workbook insert notes """
     resp1 = testapp.get(f'/notes-interpretation/{note1_uuid}/', status=200).json
     resp2 = testapp.get(f'/notes-interpretation/{note2_uuid}/', status=200).json
     assert resp1['previous_note']['@id'] == resp2['@id']
 
 def test_add_note_interpretation_success(workbook, testapp, new_interpretation):
-    resp = testapp.post_json('/note_interpretation', new_interpretation, status=201)
+    """ test NoteInterpretation item posts successfully """
+    resp = post_note(testapp, new_interpretation)
 
 def test_add_note_interpretation_fail(workbook, testapp, new_interpretation):
+    """ test NoteInterpretation item fails to post when schema isn't followed """
     new_interpretation['classification'] = 'Likely Pathogenic'  # wrong case for enum
-    resp = testapp.post_json('/note_interpretation', new_interpretation, status=422)
+    resp = post_note(testapp, new_interpretation, expected_status=422)
 
 def test_patch_note_interpretation_success(workbook, testapp, new_interpretation):
-    resp = testapp.post_json('/note_interpretation', new_interpretation, status=201).json['@graph'][0]
+    """ test NoteIntepretation item is patched successfully when associated_items SOEA is added """
+    resp = post_note(testapp, new_interpretation).json['@graph'][0]
     patch_info = {
         'associated_items': [{"item_type": "Variant", "item_identifier": variant_with_note}]
     }
     resp = testapp.patch_json('/' + resp['@id'], patch_info, status=200)
 
 def test_patch_note_interpretation_fail(workbook, testapp, new_interpretation):
-    resp = testapp.post_json('/note_interpretation', new_interpretation, status=201).json['@graph'][0]
+    """ test NoteInterpretation item fails to patch with incorrectly formatted prop """
+    resp = post_note(testapp, new_interpretation).json['@graph'][0]
     patch_info = {
         'associated_items': {"item_type": "Variant", "item_identifier": variant_with_note}  # wrong type
     }
     resp = testapp.patch_json('/' + resp['@id'], patch_info, status=422)
 
 def test_add_standard_note_success(workbook, testapp, new_standard_note):
-    resp = testapp.post_json('/note_standard', new_standard_note, status=201)
+    """ test NoteStandard item posts successfully """
+    resp = post_note(testapp, new_standard_note, note_type='note_standard')
 
 def test_add_standard_note_fail(workbook, testapp, new_standard_note):
+    """ test NoteStandard item post fails when extra prop added """
     new_standard_note['classification'] = 'benign'  # not a standard note property
-    resp = testapp.post_json('/note_standard', new_standard_note, status=422)
+    resp = post_note(testapp, new_standard_note, note_type='note_standard', expected_status=422)
 
 def test_patch_standard_note_success(workbook, testapp, new_standard_note):
-    resp = testapp.post_json('/note_standard', new_standard_note, status=201).json['@graph'][0]
+    """ test NoteStandard item patches successfully """
+    resp = post_note(testapp, new_standard_note, note_type='note_standard').json['@graph'][0]
     patch_info = {"note_text": "Some different text"}
     resp = testapp.patch_json('/' + resp['@id'], patch_info, status=200)
 
 def test_patch_standard_note_fail(workbook, testapp, new_standard_note):
-    resp = testapp.post_json('/note_standard', new_standard_note, status=201).json['@graph'][0]
+    """ test NoteStandard item patch fails with extra property """
+    resp = post_note(testapp, new_standard_note, note_type='note_standard').json['@graph'][0]
     patch_info = {"conclusion": "For these reasons we made a conclusion."}  # not a std note property
     resp = testapp.patch_json('/' + resp['@id'], patch_info, status=422)
 
 def test_link_standard_note_to_report(workbook, testapp, new_standard_note, test_report):
-    note = testapp.post_json('/note_standard', new_standard_note, status=201).json['@graph'][0]
+    """ test NoteStandard linkTo on Report item works """
+    note = post_note(testapp, new_standard_note, note_type='note_standard').json['@graph'][0]
     patch = {'extra_notes': [note['@id']]}
     resp = testapp.patch_json('/' + test_report['@id'], patch, status=200).json['@graph'][0]
     assert resp['extra_notes'] == [note['@id']]
