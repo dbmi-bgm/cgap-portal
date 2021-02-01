54caf02a7ed0b27d82daf83c958b8eda0fedbe5c
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-02-01 13:50:58 -0500

    fix misspelling 1: a-> e in vertebrate

--- src/encoded/static/components/item-pages/VariantSampleView/VariantTabBody.js
+++ src/encoded/static/components/item-pages/VariantSampleView/VariantTabBody.js
@@ -370,7 +370,7 @@ function PredictorsSection({ context, getTipForField, currentTranscriptIdx }){
                         </tr>
                         <tr>
                             <td className="text-left">
-                                <label className="mb-0" data-tip={getTipForField("conservation_phylop100")}>PhyloP (100 Vertabrates)</label>
+                                <label className="mb-0" data-tip={getTipForField("conservation_phylop100")}>PhyloP (100 Vertebrates)</label>
                             </td>
                             <td className="text-left">{ conservation_phylop100 }</td>
                         </tr>
