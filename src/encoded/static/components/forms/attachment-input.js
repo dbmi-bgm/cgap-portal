daa750b335929da1ac02acb2b77c0a5a570e2bd6
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-02-05 11:11:35 -0500

    Quick n dirty XHR implementation multipart/form-data

--- src/encoded/static/components/forms/attachment-input.js
+++ src/encoded/static/components/forms/attachment-input.js
@@ -20,7 +20,9 @@ export class AttachmentInputController extends React.PureComponent {
         this.handleChange = this.handleChange.bind(this);
         // this.onFormSubmit = this.onFormSubmit.bind(this);
         this.state = {
-            loading: false
+            loading: false,
+            success: null,
+
         };
     }
 
@@ -29,76 +31,45 @@ export class AttachmentInputController extends React.PureComponent {
         file.filename = file.name;
 
         const formData = new FormData();
-        formData._boundary = "---------ArbitraryBoundary38839293dfsdfiso";
-        console.log("file.get('_boundary')", formData.get("_boundary"));
         formData.append("datafile", file);
-        console.log("file.get('datafile')", formData.get('datafile'));
 
-        console.log("context", this.props.context);
         this.setState({ loading: true }, ()=>{
 
-            // const attachment_props = {};
-            // const { datafile = {} } = attachment_props;
             const { context: { uuid }, href } = this.props;
 
-            // datafile.type = file.type;
-            // datafile.filename = file.name;
-            // if (file.size) {
-            //     datafile.size = file.size;
-            // }
-            // const fileReader = new window.FileReader();
-            // fileReader.readAsText(file);
-            // fileReader.onloadend = (e) => {
-            //     console.log("e.target.result", e.target.result);
-            //     if (e.target.result) {
-            //          attachment_props.href = e.target.result;
             const postURL = '/ingestion-submissions/' + uuid + '/submit_for_ingestion';
-            console.log(`Attempting Ingestion. \n\nPatching to: ${postURL}`);
-            ajax.promise(
-                postURL,
-                'POST',
-                { "Content-Type" : `multipart/form-data; boundary-${formData._boundary}` },
-                formData,
-                false,
-                true
-                // JSON.stringify(attachment_props)
-            ).then((data) => {
-                // TODO test if anything else wrong with response and throw if so.
-                if (!data || data.status === "error"){
-                    throw data;
+            console.log(`Attempting Ingestion. \n\nPosting to: ${postURL}`);
+
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", postURL, true);
+
+            xhr.onreadystatechange = function() { // Call a function when the state changes.
+                if (xhr.readyState !== 4) return;
+                if (xhr.readyState === xhr.DONE && xhr.status === 200) {
+                    // Request finished. Do processing here.
+                    console.log("response:", xhr.response);
+                    this.setState({ loading: false, success: true });
+                } else {
+                    this.setState({ loading: false, success: false }, function(){
+                        Alerts.queue(AttachmentInputController.ErrorObject);
+                    });
+                    console.error("Submission Ingestion Error: ", this.response);
                 }
-                console.log(" got dataaaa", data);
-                return data;
-            }).then((data)=>{
-                this.setState({ loading: false });
-                console.log("got some data", data);
-                return data;
-            }).catch((data)=>{
-                this.setState({ loading: false }, function(){
-                    Alerts.queue(AttachmentInputController.ErrorObject);
-                });
-                console.error("Submission Ingestion Error: ", data);
-            });
-            //      } else {
-            //          console.error("Submission Ingestion Error: Loading file contents into FileReader failed.");
-            //          this.setState({ loading: false }, function(){
-            //              Alerts.queue(AttachmentInputController.ErrorObject);
-            //          });
-            //          return;
-            //      }
-            // };
+            }.bind(this);
+
+            xhr.send(formData);
         });
     }
 
-    // onFormSubmit() {
-    //     e.preventDefault();
-    // }
+    onFormSubmit() {
+        e.preventDefault();
+    }
 
     render(){
         const { children, ...passProps } = this.props;
-        const { loading: loadingPostSubmissionResult } = this.state;
+        const { loading: loadingFileResult, success: postFileSuccess } = this.state;
         return (
-            React.Children.map(children, (c) => React.cloneElement(c, { ...passProps, loadingPostSubmissionResult, onFileInputChange: this.handleChange }))
+            React.Children.map(children, (c) => React.cloneElement(c, { ...passProps, loadingFileResult, postFileSuccess, onFileInputChange: this.handleChange }))
         );
     }
 }
