1b9a6df7b8660f6b3792150149e2dc9125188027
SooLee <duplexa@gmail.com>
2021-02-23 11:10:40 -0500

    randomize file name in bamsnap test

--- src/encoded/tests/test_types_variant_sample.py
+++ src/encoded/tests/test_types_variant_sample.py
@@ -12,7 +12,7 @@ def test_variant_sample():
         'variant': 'f6aef055-4c88-4a3e-a306-d37a71535d8b',
         'AD': '1,3',
         'CALL_INFO': 'my_test_sample',
-        'file': 'dummy-file-name2',
+        'file': 'dummy-file-name',
         'project': 'hms-dbmi',
         'institution': 'hms-dbmi'
     }
@@ -21,10 +21,11 @@ def test_variant_sample():
 @pytest.mark.integrated  # uses s3
 def test_bam_snapshot_download(workbook, es_testapp, test_variant_sample):
     """ Tests that we can correctly download an IGV image from the wfoutput bucket. """
+    test_variant_sample['file'] += str(random.choice(range(0,100)))
     res = es_testapp.post_json(VARIANT_SAMPLE_URL, test_variant_sample, status=[201, 409]).json
     uuid = res['@graph'][0]['uuid']
     bam_snapshot_location = res['@graph'][0]['bam_snapshot']
-    assert bam_snapshot_location == 'dummy-file-name2/bamsnap/chr1_12125898.png'
+    assert bam_snapshot_location == test_variant_sample['file'] + '/bamsnap/chr1_12125898.png'
     download = es_testapp.get('/' + uuid + '/@@download').location
     resp = requests.get(download)
     assert 'hello world' in resp.content.decode('utf-8')
