7f432ac0dd5a90e895d6060f1f120353bd64e621
SooLee <duplexa@gmail.com>
2021-02-23 12:07:32 -0500

    no randomize

--- src/encoded/tests/test_types_variant_sample.py
+++ src/encoded/tests/test_types_variant_sample.py
@@ -22,7 +22,8 @@ def test_variant_sample():
 @pytest.mark.integrated  # uses s3
 def test_bam_snapshot_download(workbook, es_testapp, test_variant_sample):
     """ Tests that we can correctly download an IGV image from the wfoutput bucket. """
-    test_variant_sample['file'] += str(random.choice(range(0,100)))
+    #test_variant_sample['file'] += str(random.choice(range(0,100)))
+    test_variant_sample['file'] += '2'
     res = es_testapp.post_json(VARIANT_SAMPLE_URL, test_variant_sample, status=[201, 409]).json
     uuid = res['@graph'][0]['uuid']
     bam_snapshot_location = res['@graph'][0]['bam_snapshot']
