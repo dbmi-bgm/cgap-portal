5751335ffd8f68e0a5421c8bbfdce85162d8c46e
William Ronchetti <wrr33@cornell.edu>
2021-02-23 11:57:53 -0500

    push working configuration, nginx still not working though

--- deploy/docker/local/entrypoint.sh
+++ deploy/docker/local/entrypoint.sh
@@ -3,8 +3,14 @@
 # Enter venv
 export PATH="/opt/venv/bin:$PATH"
 
-# load local data
-poetry run load-data development.ini --app-name app
+# Clear db/es since this is the local entry point
+poetry run clear-db-es-contents development.ini --app-name app --env $CGAP_ENV_NAME
+
+# Create mapping
+poetry run create-mapping-on-deploy development.ini --app-name app
+
+# Load Data (based on development.ini, for now just master-inserts)
+poetry run load-data development.ini --app-name app --prod
 
 # Start application
 make deploy2
