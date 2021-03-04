a3269f871af4c9803d1c9a9d63c031a37b5b2326
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2021-03-04 18:10:28 -0500

    Create barebones gene list submission architecture

--- src/encoded/ingestion/processors.py
+++ src/encoded/ingestion/processors.py
@@ -5,6 +5,7 @@ from dcicutils.misc_utils import ignored
 from ..ingestion.common import get_parameter
 from ..util import debuglog, s3_local_file
 from ..submit import submit_metadata_bundle
+from ..submit_genelist import submit_genelist
 from .exceptions import UndefinedIngestionProcessorType
 from ..types.ingestion import SubmissionFolio
 
@@ -44,6 +45,51 @@ def handle_data_bundle(submission: SubmissionFolio):
                            " The ingestion_type 'data_bundle' was renamed to 'metadata_bundle'"
                            " prior to the initial release. Your submission program probably needs to be updated."
                            % submission.ingestion_type)
+
+
+@ingestion_processor('genelist')
+def handle_genelist(submission: SubmissionFolio):
+
+    with submission.processing_context():
+        s3_client = submission.s3_client
+        submission_id = submission.submission_id
+        institution = get_parameter(submission.parameters, 'institution')
+        project = get_parameter(submission.parameters, 'project')
+        validate_only = get_parameter(
+                submission.parameters,
+                'validate_only',
+                as_type=bool,
+                default=False
+        )
+        genelist_results = submit_genelist(
+                s3_client=s3_client,
+                bucket=submission.bucket,
+                key=submission.object_name,
+                project=project,
+                institution=institution,
+                vapp=submission.vapp,
+                validate_only=validate_only
+        )
+        debuglog(
+                submission_id,
+                "genelist_result:",
+                json.dumps(genelist_results, indent=2)
+        )
+
+        with submission.s3_output(key_name='validation_report') as fp:
+            submission.show_report_lines(
+                    genelist_results.get('validation_output', []),
+                    fp
+            )
+            submission.note_additional_datum(
+                    'validation_output',
+                    from_dict=genelist_results
+            )
+
+        submission.process_standard_bundle_results(genelist_results)
+
+        if not genelist_results.get('success'):
+            submission.fail()
 
 
 @ingestion_processor('metadata_bundle')
---
+++ src/encoded/submit_genelist.py
@@ -0,0 +1,285 @@
+import itertools
+import json
+from base64 import b64encode
+
+from .util import s3_local_file
+
+
+def submit_genelist(
+        *,
+        s3_client,
+        bucket,
+        key,
+        project,
+        institution,
+        vapp,
+        validate_only=False
+):
+    """
+    Handles processing of a submitted gene list.
+
+    Args:
+        s3_client: a boto3 s3 client object
+        bucket: the name of the s3 bucket that contains the data to be 
+            processed
+        key: the name of a key within the given bucket that contains the data 
+            to be processed
+        project: a project identifier
+        institution: an institution identifier
+        vapp: a vapp object
+        validate_only: a bool. If True, only do validation, not posting; 
+            otherwise (if False), do posting, too.
+    """
+
+    with s3_local_file(s3_client, bucket=bucket, key=key) as filename:
+        results = {
+                'success': False,
+                'validation_output': [],
+                'result': {},
+                'post_output': [],
+                'upload_info': []
+        }
+        if filename.endswith('.txt'):
+            genelist = GeneListSubmission(filename, project, institution, vapp) 
+            if genelist.post_output:
+                results['success'] = True
+                results['validation_output'] = genelist.validation_output
+                results['result'] = ''
+                results['post_output'] = genelist.post_output
+                return results
+            else:
+                results['validation_output'] = genelist.errors
+                return results
+        else:
+            msg = 'Gene list must be a .txt file.'
+            results['validation_output'].append(msg)
+            return results
+
+
+class GeneListSubmission:
+    """
+    Class to handle processing of submitted gene list.
+    """
+
+    def __init__(self, filename, project, institution, vapp):
+        self.filename = filename
+        self.project = project
+        self.instutition = institution
+        self.vapp = vapp
+        self.genes = self.parse_genelist()
+        self.gene_ids = self.match_genes()
+        self.jsons_to_post = self.create_jsons_to_post()
+        self.validation_output = self.validate_postings()
+        self.post_output = self.post_items()
+        self.errors = []
+
+    def parse_genelist(self):
+        """
+        Parses gene list file and removes any duplicates.
+
+        Returns one of:
+            - a unique gene list
+            - None if gene list is empty
+        """
+
+        genelist_raw = self.filename.read()
+        for delimiter in [' ', '\t', '\n', ':', ';']:
+            if delimiter in genelist_raw:
+                genelist_raw = genelist_raw.replace(delimiter, ',')
+        genelist = genelist_raw.split(',')
+        while '' in genelist:
+            genelist.remove('')
+        if len(genelist) != len(set(genelist)):
+            unique_genes = []
+            for gene in genelist:
+                if gene not in unique_genes:
+                    unique_genes.append(gene)
+            genelist = unique_genes
+        if not genelist:
+            self.errors.append(
+                    'The gene list appears to be empty.'
+            )
+            return None
+        return genelist
+
+    def match_genes(self):
+        """
+        Attempts to match every gene to unique gene item in CGAP.
+
+        Matches by gene symbol, alias symbol, previous symbol, and genereviews
+        symbol, in that order. If multiple genes in the given gene list direct
+        to the same gene item, only one instance is included, so the gene list
+        produced may be shorter than the one submitted.
+
+        Returns one of:
+            - list of gene uuids in alphabetical order
+            - None if no genes were passed in or >= 1 gene could not be matched
+        """
+
+        if not self.genes:
+            return None
+        gene_ids = {}
+        unmatched_genes = {}
+        for gene in self.genes:
+            response = self.vapp.get(
+                    'search/?type=Gene&gene_symbol=' + gene + '&field=@id',
+            )
+            if len(response) == 1:
+                if response[0]['@id'] in gene_ids:
+                    continue
+                else:
+                    gene_ids[gene] = response[0]['@id']
+            else:
+                unmatched_genes[gene] = len(response)
+        if unmatched_genes:
+            genes_without_options = []
+            genes_with_options = []
+            for gene in unmatched_genes.copy():
+                response = self.vapp.get('search/?type=Gene&q=' + gene)
+                if len(response) >= 1:
+                    search_order = [
+                            'alias_symbol',
+                            'prev_symbol',
+                            'genereviews'
+                    ]
+                    for search_term, response_item in \
+                            itertools.product(search_order, response):
+                        if (
+                            search_term in response_item.keys()
+                            and gene in response_item[search_term]
+                        ):
+                            if response_item['@id'] in \
+                                    list(gene_ids.values()):
+                                break   
+                            else:
+                                gene_ids[gene] = response_item['@id']
+                                unmatched_genes.pop(gene)
+                    if gene in unmatched_genes:
+                        options = []
+                        for possible_match in response:
+                            options.append(possible_match['gene_symbol'])
+                        genes_with_options.append({gene: options})
+                else: 
+                    genes_without_options.append(gene)
+        if unmatched_genes:
+            if genes_without_options:
+                self.errors.append(
+                        'The gene(s) %s could not be found in our database.'
+                        % ', '.join(genes_without_options)
+                )
+            if genes_with_options:
+                for gene in genes_with_options:
+                    self.errors.append(
+                            'No perfect match found for gene %s. '
+                            'Consider replacing with one of the following: %s'
+                            % (gene, ', '.join(genes_with_options[gene]))
+            return None
+        gene_ids = {
+            dict_key: gene_ids[dict_key]
+            for dict_key in sorted(gene_ids.keys())
+        }
+        return list(gene_ids.values())
+
+    def create_jsons_to_post(self):
+        """
+        Creates gene list and document jsons for posting. 
+
+        Returns one of:
+            - List of jsons [document, gene list]
+            - None if no input
+        """
+
+        if not self.gene_ids:
+            return None
+        with open(self.filename, 'rb') as stream:
+            attach = {
+                'download': self.filename.split('/')[-1],
+                'type': 'text/plain',
+                'href': (
+                    'data:%s;base64,%s'
+                    % ('text/plain', b64encode(stream.read()).decode('ascii'))
+                )
+            }
+        document_post_body = json.dumps(
+            {
+                'institution': self.institution,
+                'project': self.project,
+                'status': 'shared',
+                'attachment': attach
+            }
+        )
+        genelist_post_body = json.dumps(
+            {
+                # 'title': title + " (%s)" % str(len(gene_uuids))
+                # 'source_file': ?
+                'institution': self.institution,
+                'project': self.project,
+                'genes': self.gene_ids,
+                'status': 'shared',
+            }
+        )
+        return [document_post_body, genelist_post_body]
+
+    def validate_posting(self):
+        """
+        Attempts to validate document and gene list jsons provided by
+        jsons_to_post.
+
+        May need to be updated when handling overwriting of previous files as
+        is only set to post at the moment. 
+
+        Returns one of:
+            - 'success' if both documents validated
+            - None if no jsons were created or validation failed
+        """
+
+        if not self.jsons_to_post:
+            return None
+        document_json = self.jsons_to_post[0]
+        genelist_json = self.jsons_to_post[1]
+        try:
+            self.vapp.post_json('/Document/?check_only=true', document_json)
+        except Exception:
+            self.errors.append('Document validation failed')
+            return None
+        try:
+            self.vapp.post_json('/GeneList/?check_only=true', genelist_json)
+        except Exception:
+            self.errors.append('Gene list validation failed.')
+            return None
+        return 'success'
+
+    def post_items(self):
+        """
+        Attempts to post document and gene list jsons to CGAP.
+
+        Returns one of:
+            - 'success'
+            - None if no jsons were created or posting failed
+        """
+
+        if not self.jsons_to_post:
+            return None
+        document_json = self.jsons_to_post[0]
+        genelist_json = self.jsons_to_post[1]
+        try:
+            document_post = self.vapp.post_json('/Document/', document_json)
+        except Exception:
+            self.errors.append('Posting document failed.')
+            return None
+        # Add in document_post uuid to genelist_json source file so can be
+        # posted. 
+        try:
+            self.vapp.post_json('/GeneList/', genelist_json)
+        except Exception:
+            self.errors.append('Posting gene list failed.'
+            return None
+        return 'success'
+
+# Features to add/work on:
+#     * Create error class to handle all errors? Seems overboard here.
+#     * Handle overwriting existing document and gene list
+#     * Handle duplication within gene list (both duplicates in raw gene list and
+#         mulitple genes of different name that refer to same gene in database)
+#     * How to handle title input (either in input document vs via UI?)
+#     * Need to add document posted uuid to genelist post body before posting
