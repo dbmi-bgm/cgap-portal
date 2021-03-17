96832182da328f568f88898b8ded1cdd9d3bcd22
Kent Pitman <kent_pitman@hms.harvard.edu>
2021-03-17 07:46:52 -0400

    Rename deploy/tests.py to deploy/tests/test_deploy_beanstalk.py, and fix it to work.

--- deploy/tests/test_deploy_beanstalk.py
+++ deploy/tests/test_deploy_beanstalk.py
@@ -1,29 +1,29 @@
+from unittest import mock
 from pytest import fixture
-from deploy_beanstalk import previous_git_commit, parse
+from ..deploy_beanstalk import previous_git_commit, parse
 
 
-@fixture()
-def git_log_output():
-    return '''commit bc061fc755015162741eec71f1a71ea6c3fdb786
+GIT_LOG_OUTPUT = '''commit bc061fc755015162741eec71f1a71ea6c3fdb786
 Author: j1z0 <jeremy_johnson@hms.harvard.edu>
 Date:   Thu Sep 22 22:23:54 2016 -0400
 
     we need .aws for both master and production
 
-    '''
+'''
 
 
-def test_parse_git_commit(git_log_outpu):
+def test_parse_git_commit():
     author = "j1z0 <jeremy_johnson@hms.harvard.edu>"
     msg = "we need .aws for both master and production"
     expected = "%s - %s" % (author, msg)
-    actual = parse(previous_git_commit())
+    actual = parse(GIT_LOG_OUTPUT)
 
     print("expected result: ", expected)
     print("actual result: ", actual)
     assert expected == actual
 
+
 if __name__ == "__main__":
     print(".")
-    test_parse_git_commit(git_log_output())
+    test_parse_git_commit()
     print("all good!")
