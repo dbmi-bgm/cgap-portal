cbad1db1315ea3b1b26da69b73acfa2af58c2b8e
Alex Balashov <alexander_balashov@hms.harvard.edu>
2021-03-29 17:58:08 -0400

    Adjust 'handleBeforeUnload'
    If isSubmissions is text, use it as message.
    (maybe todo: rename isSubmissions)

--- src/encoded/static/components/app.js
+++ src/encoded/static/components/app.js
@@ -1043,9 +1043,9 @@ export default class App extends React.PureComponent {
     handleBeforeUnload(e){
         const { isSubmitting } = this.state;
         if (isSubmitting){
-            const dialogText = "Leaving will cause all unsubmitted work to be lost. Are you sure you want to proceed?";
-            e.returnValue = dialogText;
-            return dialogText;
+            const confirmMessage = typeof isSubmitting === "string" ? isSubmitting : "Leaving will cause all unsubmitted work to be lost. Are you sure you want to proceed?"; "Leaving will cause all unsubmitted work to be lost. Are you sure you want to proceed?";
+            e.returnValue = confirmMessage;
+            return confirmMessage;
         }
     }
 
