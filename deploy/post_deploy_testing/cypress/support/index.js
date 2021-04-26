3a003e466b3d41905db8db2c2ae0b6590d7f876b
Alex Balashov <alexander_balashov@hms.harvard.edu>
2021-04-26 14:34:02 -0400

    Cypress tests setup (initial)

--- deploy/post_deploy_testing/cypress/support/index.js
+++ deploy/post_deploy_testing/cypress/support/index.js
@@ -18,3 +18,10 @@ import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+
+/* ---- */
+
+export const cypressVisitHeaders = {
+    "Accept" : "text/html, application/xhtml+xml, application/xml;q=0.9, image/avif, image/webp, image/apng, */*;q=0.8, application/signed-exchange;v=b3;q=0.9"
+};
