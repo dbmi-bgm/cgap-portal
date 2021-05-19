b41433849f0afd759682c2b5a6877dec2af2ec3e
William Ronchetti <wrr33@cornell.edu>
2021-05-19 09:28:58 -0400

    provision variant version 2 upgader

--- src/encoded/upgrade/variant.py
+++ src/encoded/upgrade/variant.py
@@ -5,7 +5,7 @@ from snovault import (
 
 @upgrade_step('variant', '1', '2')
 def variant_update_1_2(value, system):
-    """ Upgrades variant from schema version 1 to 2, deleting csq_rs_dbsnp151 """
+    """ Upgrades variant from schema version 1 to 2, deleting the 4 relevant fields. """
 
     # If we are on schema version >1, no update needed
     if int(value.get('schema_version', '1')) >= 2:
@@ -13,3 +13,12 @@ def variant_update_1_2(value, system):
 
     if 'csq_rs_dbsnp151' in value:
         del value['csq_rs_dbsnp151']
+
+    if 'csq_hg19_chr' in value:
+        del value['csq_hg19_chr']
+
+    if 'csq_hg19_pos' in value:
+        del value['csq_hg19_pos']
+
+    if 'csq_clinvar_clnhgvs' in value:
+        del value['csq_clinvar_clnhgvs']
