b22e4eaaf4e2f41fd5add49bd85ae4fc608b28fa
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2021-06-01 10:57:53 -0400

    Organize tests into class

--- src/encoded/tests/test_custom_embed.py
+++ src/encoded/tests/test_custom_embed.py
@@ -8,6 +8,23 @@ EMBED_URL = "/embed"
 KEYS_NOT_INCLUDED = ["@context", "actions", "aggregated-items", "validation-errors"]
 
 
+@pytest.fixture
+def variant_sample_list(
+    testapp, variant_sample, variant_sample_2, genelist, project, institution
+):
+    vs_1 = testapp.post_json("/variant-samples", variant_sample).json["@graph"][0]
+    vs_2 = testapp.post_json("/variant-samples", variant_sample_2).json["@graph"][0]
+    vs_list = {
+        "project": project["@id"],
+        "institution": institution["@id"],
+        "variant_samples": [
+            {"variant_sample_item": vs_1["@id"]},
+            {"variant_sample_item": vs_2["@id"]},
+        ],
+    }
+    return testapp.post_json("/variant-sample-lists", vs_list).json["@graph"][0]
+
+
 def _embed_with_url_params(testapp, embed_string, status="*"):
     """POST to embed endpoint with url parameters."""
     creation_post_data = {}
@@ -63,122 +80,100 @@ def _contains_atid(item):
     return result
 
 
-@pytest.fixture
-def variant_sample_list(
-    testapp, variant_sample, variant_sample_2, genelist, project, institution
-):
-    vs_1 = testapp.post_json("/variant-samples", variant_sample).json["@graph"][0]
-    vs_2 = testapp.post_json("/variant-samples", variant_sample_2).json["@graph"][0]
-    vs_list = {
-        "project": project["@id"],
-        "institution": institution["@id"],
-        "variant_samples": [
-            {"variant_sample_item": vs_1["@id"]},
-            {"variant_sample_item": vs_2["@id"]},
-        ],
-    }
-    return testapp.post_json("/variant-sample-lists", vs_list).json["@graph"][0]
+class TestCustomEmbed:
+    def test_post_methods(self, testapp, variant_sample_list):
+        """
+        Ensure equal results by posting to endpoint with item ID in URL
+        or in json body.
+        """
+        vsl_uuid = variant_sample_list["uuid"]
+        vsl_embed_url = _embed_with_url_params(testapp, EMBED_URL + "?id=" + vsl_uuid)
+        json_to_post = {"ids": [vsl_uuid]}
+        vsl_embed_json = _embed_with_json_params(testapp, json_to_post)
+        assert vsl_embed_url == vsl_embed_json
 
-
-def test_post_methods(testapp, variant_sample_list):
-    """
-    Ensure equal results by posting to endpoint with item ID in URL
-    or in json body.
-    """
-    vsl_uuid = variant_sample_list["uuid"]
-    vsl_embed_url = _embed_with_url_params(testapp, EMBED_URL + "?id=" + vsl_uuid)
-    json_to_post = {"ids": [vsl_uuid]}
-    vsl_embed_json = _embed_with_json_params(testapp, json_to_post)
-    assert vsl_embed_url == vsl_embed_json
-
-
-def test_depth_of_zero(testapp, variant_sample_list):
-    """Test request with depth=0 returns @@object view of item."""
-    vsl_uuid = variant_sample_list["uuid"]
-    url_params = EMBED_URL + "?id=" + vsl_uuid + "&depth=0"
-    vsl_embed = _embed_with_url_params(testapp, url_params)
-    for key, value in variant_sample_list.items():
-        if key not in KEYS_NOT_INCLUDED:
-            assert value == vsl_embed[key]
-
-
-def test_desired_keys(testapp, variant_sample_list):
-    """Test embedding only requested 'desired' keys."""
-    vsl_uuid = variant_sample_list["uuid"]
-    vsl_atid = variant_sample_list["@id"]
-    vsl_true = testapp.get(vsl_atid).json
-    desired = ["projects"]
-    desired_addon = "&desired=".join(desired)
-    url_params = EMBED_URL + "?id=" + vsl_uuid + "&depth=1&desired=" + desired_addon
-    vsl_embed_url = _embed_with_url_params(testapp, url_params)
-    json_to_post = {"ids": [vsl_uuid], "depth": 1, "desired": desired}
-    vsl_embed_json = _embed_with_json_params(testapp, json_to_post)
-    desired_keys = _convert_atid_to_key(desired)
-    assert vsl_embed_url == vsl_embed_json
-    for item in desired_keys:
-        for key, value in vsl_true[item].items():
+    def test_depth_of_zero(self, testapp, variant_sample_list):
+        """Test request with depth=0 returns @@object view of item."""
+        vsl_uuid = variant_sample_list["uuid"]
+        url_params = EMBED_URL + "?id=" + vsl_uuid + "&depth=0"
+        vsl_embed = _embed_with_url_params(testapp, url_params)
+        for key, value in variant_sample_list.items():
             if key not in KEYS_NOT_INCLUDED:
-                assert value == vsl_embed_url[item][key]
-    for key, value in variant_sample_list.items():
-        if key not in desired_keys:
-            assert value == vsl_embed_url[key]
+                assert value == vsl_embed[key]
 
+    def test_desired_keys(self, testapp, variant_sample_list):
+        """Test embedding only requested 'desired' keys."""
+        vsl_uuid = variant_sample_list["uuid"]
+        vsl_atid = variant_sample_list["@id"]
+        vsl_true = testapp.get(vsl_atid).json
+        desired = ["projects"]
+        desired_addon = "&desired=".join(desired)
+        url_params = EMBED_URL + "?id=" + vsl_uuid + "&depth=1&desired=" + desired_addon
+        vsl_embed_url = _embed_with_url_params(testapp, url_params)
+        json_to_post = {"ids": [vsl_uuid], "depth": 1, "desired": desired}
+        vsl_embed_json = _embed_with_json_params(testapp, json_to_post)
+        desired_keys = _convert_atid_to_key(desired)
+        assert vsl_embed_url == vsl_embed_json
+        for item in desired_keys:
+            for key, value in vsl_true[item].items():
+                if key not in KEYS_NOT_INCLUDED:
+                    assert value == vsl_embed_url[item][key]
+        for key, value in variant_sample_list.items():
+            if key not in desired_keys:
+                assert value == vsl_embed_url[key]
 
-def test_ignored_keys(testapp, variant_sample_list):
-    """Test embedding all keys except requested 'ignored' keys."""
-    vsl_uuid = variant_sample_list["uuid"]
-    ignored = ["projects"]
-    ignored_keys = _convert_atid_to_key(ignored)
-    ignored_addon = "&ignored=".join(ignored)
-    url_params = EMBED_URL + "?id=" + vsl_uuid + "&depth=1&ignored=" + ignored_addon
-    vsl_embed_url = _embed_with_url_params(testapp, url_params)
-    json_to_post = {"ids": [vsl_uuid], "depth": 1, "ignored": ignored}
-    vsl_embed_json = _embed_with_json_params(testapp, json_to_post)
-    assert vsl_embed_url == vsl_embed_json
-    for key, value in variant_sample_list.items():
-        if key in ignored_keys:
-            assert value == vsl_embed_url[key]
-        elif key == "@id":
-            continue
-        elif _contains_atid(value):
-            assert vsl_embed_url[key] != value
+    def test_ignored_keys(self, testapp, variant_sample_list):
+        """Test embedding all keys except requested 'ignored' keys."""
+        vsl_uuid = variant_sample_list["uuid"]
+        ignored = ["projects"]
+        ignored_keys = _convert_atid_to_key(ignored)
+        ignored_addon = "&ignored=".join(ignored)
+        url_params = EMBED_URL + "?id=" + vsl_uuid + "&depth=1&ignored=" + ignored_addon
+        vsl_embed_url = _embed_with_url_params(testapp, url_params)
+        json_to_post = {"ids": [vsl_uuid], "depth": 1, "ignored": ignored}
+        vsl_embed_json = _embed_with_json_params(testapp, json_to_post)
+        assert vsl_embed_url == vsl_embed_json
+        for key, value in variant_sample_list.items():
+            if key in ignored_keys:
+                assert value == vsl_embed_url[key]
+            elif key == "@id":
+                continue
+            elif _contains_atid(value):
+                assert vsl_embed_url[key] != value
 
+    def test_minimal_embeds(self, testapp, variant_sample_list):
+        """Test default minimal embedding settings work as intended."""
+        vsl_uuid = variant_sample_list["uuid"]
+        url_params = EMBED_URL + "?id=" + vsl_uuid
+        vsl_embed_url = _embed_with_url_params(testapp, url_params)
+        minimal_embeds = _convert_atid_to_key(MINIMAL_EMBEDS)
+        for key in minimal_embeds:
+            if key in vsl_embed_url:
+                assert "@id" in vsl_embed_url[key]
+                assert "title" in vsl_embed_url[key]
 
-def test_minimal_embeds(testapp, variant_sample_list):
-    """Test default minimal embedding settings work as intended."""
-    vsl_uuid = variant_sample_list["uuid"]
-    url_params = EMBED_URL + "?id=" + vsl_uuid
-    vsl_embed_url = _embed_with_url_params(testapp, url_params)
-    minimal_embeds = _convert_atid_to_key(MINIMAL_EMBEDS)
-    for key in minimal_embeds:
-        if key in vsl_embed_url:
-            assert "@id" in vsl_embed_url[key]
-            assert "title" in vsl_embed_url[key]
+    def test_non_project_user(self, bgm_user_testapp, variant_sample_list):
+        """Test user from different project cannot call for embed."""
+        vsl_uuid = variant_sample_list["uuid"]
+        url_params = EMBED_URL + "?id=" + vsl_uuid
+        vsl_embed_url = _embed_with_url_params(bgm_user_testapp, url_params, status=403)
+        assert vsl_embed_url["status"] == "error"
+        assert vsl_embed_url["title"] == "Forbidden"
 
-
-def test_non_project_user(bgm_user_testapp, variant_sample_list):
-    """Test user from different project cannot call for embed."""
-    vsl_uuid = variant_sample_list["uuid"]
-    url_params = EMBED_URL + "?id=" + vsl_uuid
-    vsl_embed_url = _embed_with_url_params(bgm_user_testapp, url_params, status=403)
-    assert vsl_embed_url["status"] == "error"
-    assert vsl_embed_url["title"] == "Forbidden"
-
-
-def test_too_many_items(testapp, variant_sample_list):
-    """Test POST with >5 item IDs results in bad request error."""
-    ids = []
-    ids.append(variant_sample_list["uuid"])
-    ids.append(variant_sample_list["project"])
-    ids.append(variant_sample_list["institution"])
-    variant_sample_atids = [
-        item["variant_sample_item"] for item in variant_sample_list["variant_samples"]
-    ]
-    ids += variant_sample_atids
-    variants_atid = [
-        variant["@id"] for variant in testapp.get("/variants/").json["@graph"]
-    ]
-    ids += variants_atid
-    json_to_post = {"ids": ids, "depth": 1}
-    embed_json = _embed_with_json_params(testapp, json_to_post, status=400)
-    assert embed_json["status"] == "error"
+    def test_too_many_items(self, testapp, variant_sample_list):
+        """Test POST with >5 item IDs results in bad request error."""
+        ids = []
+        ids.append(variant_sample_list["uuid"])
+        ids.append(variant_sample_list["project"])
+        ids.append(variant_sample_list["institution"])
+        variant_sample_atids = [
+            item["variant_sample_item"] for item in variant_sample_list["variant_samples"]
+        ]
+        ids += variant_sample_atids
+        variants_atid = [
+            variant["@id"] for variant in testapp.get("/variants/").json["@graph"]
+        ]
+        ids += variants_atid
+        json_to_post = {"ids": ids, "depth": 1}
+        embed_json = _embed_with_json_params(testapp, json_to_post, status=400)
+        assert embed_json["status"] == "error"
