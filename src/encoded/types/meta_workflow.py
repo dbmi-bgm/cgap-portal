3118e314dc2aeaa8c287962ed885391623ef612c
SooLee <duplexa@gmail.com>
2021-06-03 15:27:31 -0400

    metawf, metawfr added to conftest_settings

---
+++ src/encoded/types/meta_workflow.py
@@ -0,0 +1,39 @@
+"""The type file for the collection MetaWorkflow and MetaWorkflowRun.
+  
+moving this out of __init.py__ and into it's own file as
+add logic for autopopulating 'name' upon update or create
+"""
+from snovault import (
+    # calculated_property,
+    collection,
+    load_schema,
+)
+from .base import (
+    Item,
+)
+
+@collection(
+    name='meta-workflows',
+    unique_key='accession',
+    properties={
+        'title': 'MetaWorkflows',
+        'description': 'Listing of MetaWorkflows',
+    })
+class MetaWorkflow(Item):
+    item_type = 'meta_workflow'
+    name_key = 'accession'
+    schema = load_schema('encoded:schemas/meta_workflow.json')
+
+
+@collection(
+    name='meta-workflow-runs',
+    unique_key='accession',
+    properties={
+        'title': 'MetaWorkflowRuns',
+        'description': 'Listing of MetaWorkflowRuns',
+    })
+class MetaWorkflowRun(Item):
+    item_type = 'meta_workflow_run'
+    name_key = 'accession'
+    schema = load_schema('encoded:schemas/meta_workflow_run.json')
+~                                                                  
