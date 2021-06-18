198e4ec8ca319ceb4308202825a1aaf058e240ca
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-06-17 23:41:39 -0400

    fix for wrong tab initialization (again)

--- src/encoded/static/components/item-pages/CaseView/InterpretationTab.js
+++ src/encoded/static/components/item-pages/CaseView/InterpretationTab.js
@@ -125,13 +125,13 @@ function VariantSampleSelection({ selection, index, caseAccession, schemas }){
                         <label className="mb-04 text-small" data-tip={geneTranscriptColDescription}>
                             { geneTranscriptColTitle || "Gene, Transcript" }
                         </label>
-                        { geneTranscriptRenderFunc(variant_sample_item, { align: 'left', link: vsID + '?showInterpretation=True&annotationTab=0&interpretationTab=1' + (caseAccession ? '&caseSource=' + caseAccession : '') }) }
+                        { geneTranscriptRenderFunc(variant_sample_item, { align: 'left', link: vsID + '?showInterpretation=True&annotationTab=0&interpretationTab=0' + (caseAccession ? '&caseSource=' + caseAccession : '') }) }
                     </div>
                     <div className="col col-sm-4 col-lg-2 py-2">
                         <label className="mb-04 text-small" data-tip={variantColDescription}>
                             { variantColTitle || "Variant" }
                         </label>
-                        { variantRenderFunc(variant_sample_item, { align: 'left', link: vsID + '?showInterpretation=True&annotationTab=1' + (caseAccession ? '&caseSource=' + caseAccession : '') }) }
+                        { variantRenderFunc(variant_sample_item, { align: 'left', link: vsID + '?showInterpretation=True&annotationTab=1&interpretationTab=1' + (caseAccession ? '&caseSource=' + caseAccession : '') }) }
                     </div>
                     <div className="col col-sm-4 col-lg-3 py-2">
                         <label className="mb-04 text-small" data-tip={genotypeLabelColDescription}>
