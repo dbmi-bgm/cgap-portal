4061596f774740df95aae0cc7404fe1a82ad0628
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-06-17 23:53:46 -0400

    Comment for OverviewTabView

--- src/encoded/static/components/item-pages/VariantSampleView/index.js
+++ src/encoded/static/components/item-pages/VariantSampleView/index.js
@@ -22,6 +22,14 @@ export default class VariantSampleView extends DefaultItemView {
 
 }
 
+/**
+ * OverviewTabView pulls in the most recent version of the VariantSample via the embed api; this "new context"
+ * is passed into VariantSampleOverview and is used to determine whether or not to render InterpretationSpace.
+ * This way, InterpretationSpace is always initialized with the most recent version of context.
+ *
+ * Note: Currently, this "newContext" is not used for annotation space at all, but since Variants and Genes are
+ * updated infrequently; this information is far less likely to be "stale" than the notes needed for Interpretation.
+ */
 class OverviewTabView extends React.Component {
 
     static getTabObject(props) {
