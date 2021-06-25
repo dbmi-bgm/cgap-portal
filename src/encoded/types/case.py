15b07751119ae051bfbd4c5e37632c8ef9403cd8
Sarah <sarah_reiff@hms.harvard.edu>
2021-06-25 14:53:06 -0400

    ingestion_ids now a mixin, added to family

--- src/encoded/types/case.py
+++ src/encoded/types/case.py
@@ -181,7 +181,7 @@ def _build_case_embedded_list():
         "individual.samples.files.quality_metric.status",
 
         # Family linkTo
-        "individual.families.uuid",
+        "individual.families.ingestion_ids",
 
         # Sample linkTo
         "sample.accession",
