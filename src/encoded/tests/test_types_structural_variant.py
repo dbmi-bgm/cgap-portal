5af4860949803709d90ae61683157dd11db2be5b
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2021-08-12 17:16:04 -0400

    Add test for size display calcprop

---
+++ src/encoded/tests/test_types_structural_variant.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_size_display(testapp, structural_variant):
+    """Test size_display for rounding and units."""
+    # structural_variant fixture comes with START=1000
+    test_ends = [1234, 12345, 1234567, 123456789, 12345678912]
+    expected_results = ["235 bp", "11.3 Kb", "1.2 Mb", "123.5 Mb", "12.3 Gb"]
+    variant_atid = structural_variant["@id"]
+    for end, expected_result in zip(test_ends, expected_results):
+        patch_body = {"END": end}
+        testapp.patch_json(variant_atid, patch_body, status=200)
+        result = testapp.get(variant_atid, status=200).json
+        assert result["size_display"] == expected_result
