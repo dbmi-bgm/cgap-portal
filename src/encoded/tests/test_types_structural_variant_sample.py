a8adcd3c0a71bd048129493c0152ab9aec5a5df1
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2021-08-13 08:46:02 -0400

    Fix test spelling and add post status

--- src/encoded/tests/test_types_structural_variant_sample.py
+++ src/encoded/tests/test_types_structural_variant_sample.py
@@ -13,7 +13,7 @@ def x_structural_variant(testapp, project, institution):
         "END": 2000,
         "SV_TYPE": "DUP",
     }
-    return testapp.post_json(SV_URL, item).json["@graph"][0]
+    return testapp.post_json(SV_URL, item, status=201).json["@graph"][0]
 
 @pytest.fixture
 def x_structural_variant_sample(testapp, project, institution, x_structural_variant):
@@ -25,9 +25,9 @@ def x_structural_variant_sample(testapp, project, institution, x_structural_vari
         "file": "some_vcf_file",
         "inheritance_modes": ["Dominant (maternal)"],
     }
-    return testapp.post_json(SV_SAMPLE_URL, item).json["@graph"][0]
+    return testapp.post_json(SV_SAMPLE_URL, item, status=201).json["@graph"][0]
 
-def test_sttructural_variant_sample_inheritance(x_structural_variant_sample):
+def test_structural_variant_sample_inheritance(x_structural_variant_sample):
     """Test proband-only inheritance mode calc prop."""
     assert x_structural_variant_sample["proband_only_inheritance_modes"] == ["X-linked"]
  
