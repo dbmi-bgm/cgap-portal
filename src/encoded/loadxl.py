b4640290518ec492e444689ea523b7eacba40f47
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2021-08-13 12:12:37 -0400

    Fix docstring and add higlass-view-config inserts

--- src/encoded/loadxl.py
+++ src/encoded/loadxl.py
@@ -569,11 +569,12 @@ def load_prod_data(app, overwrite=False):
     """
     return load_data(app, indir='master-inserts', overwrite=overwrite)
 
+
 def load_deploy_data(app, overwrite=False):
     """
-    Load deploy-inserts
+    Load deploy-inserts and master-inserts.
 
     Returns:
         None if successful, otherwise Exception encountered
     """
-    reutrn load_data(app, indir="deploy-inserts", overwrite=overwrite)
+    return load_data(app, indir="deploy-inserts", overwrite=overwrite)
