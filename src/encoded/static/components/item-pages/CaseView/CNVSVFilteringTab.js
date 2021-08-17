a9778e16714b894d6ebf3a670fcf482b86c29186
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-08-17 11:54:25 -0400

    Don't open annotation space in new tab by default

--- src/encoded/static/components/item-pages/CaseView/CNVSVFilteringTab.js
+++ src/encoded/static/components/item-pages/CaseView/CNVSVFilteringTab.js
@@ -79,7 +79,7 @@ function CaseViewEmbeddedStructuralVariantSearchTable(props) {
 
                     // annotationID structured like <type>_chr...etc; need just the part after underscore
                     const splitAnnotationID = (annotation_id || display_title).split("_");
-                    return <a href={atID} target="_blank" rel="noreferrer">{splitAnnotationID[1]}</a>;
+                    return <a href={atID}>{splitAnnotationID[1]}</a>;
                 }
             },
             // 'bam_snapshot': { // Note: not going to be added until a few versions from now; this may need updates specific to SVs when finally implemented
