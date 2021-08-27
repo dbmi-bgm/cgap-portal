5f6c8cde38c000057a0f4e65abc609829f086960
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-08-27 10:54:57 -0400

    More updates

--- src/encoded/static/components/item-pages/StructuralVariantSampleView/SvSampleTabBody.js
+++ src/encoded/static/components/item-pages/StructuralVariantSampleView/SvSampleTabBody.js
@@ -6,6 +6,17 @@ import React from 'react';
 export class SvSampleTabBody extends React.Component {
 
     render() {
-        return ("test sample tab body");
+        return (
+            <div className="variant-tab-body card-body">
+                <div className="row flex-column flex-lg-row">
+                    <div className="inner-card-section col pb-2 pb-lg-0">
+                        <div className="info-header-title">
+                            <h4>Sample</h4>
+                        </div>
+                        <div className="info-body"></div>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
