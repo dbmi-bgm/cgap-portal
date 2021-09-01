7a7d461352a2eccfdb61afa1b101dd1d70f55b89
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-09-01 14:53:09 -0400

    Scaffolding for detail pane transcript switching

--- src/encoded/static/components/item-pages/StructuralVariantSampleView/SvDetailPanes.js
+++ src/encoded/static/components/item-pages/StructuralVariantSampleView/SvDetailPanes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { object, schemaTransforms } from '@hms-dbmi-bgm/shared-portal-components/es/components/util';
@@ -8,8 +8,39 @@ import { responsiveGridState } from './../../util/layout';
 
 import { ExternalDatabasesSection, GeneOverview, ConstraintScoresSection } from '../VariantSampleView/AnnotationSections';
 
+/** Reuse this method for SNVs if it remains the same */
+function getInitialTranscriptIndex(transcript) {
+    // Set initial index to most severe or canonical transcript.
+    let initialIndex = transcript.findIndex(function({ csq_most_severe }){
+        return !!(csq_most_severe);
+    });
+
+    if (initialIndex === -1){
+        initialIndex = transcript.findIndex(function({ csq_canonical }){
+            return !!(csq_canonical);
+        });
+    }
+
+    if (initialIndex === -1){
+        initialIndex = 0;
+    }
+    return parseInt(initialIndex);
+}
+
 export function SvGeneDetailPane(props) {
-    const { paddingWidthMap, paddingWidth, containerWidth, windowWidth, result, minimumWidth, propsFromTable, schemas } = props;
+    const { paddingWidthMap, paddingWidth, containerWidth, windowWidth, result, minimumWidth, propsFromTable, schemas, context } = props;
+
+    const { structural_variant: { transcript = [] } = {} } = context || {};
+    const { ensgid: resultEnsgid = null } = result;
+
+    // Filter out transcripts that do not match the current result's gene
+    const resultTranscripts = transcript.filter(function({ csq_gene }){
+        const { ensgid } = csq_gene;
+        return (ensgid === resultEnsgid);
+    });
+
+    // Initialize state based on transcripts that match current result
+    const [currentTranscriptIdx, setTranscriptIdx] = useState(getInitialTranscriptIndex(resultTranscripts));
 
     let usePadWidth = paddingWidth || 0;
     if (paddingWidthMap){
@@ -47,40 +78,9 @@ export function SvGeneDetailPane(props) {
                             <h4>Consequence of SNV</h4>
                         </div>
                         <div className="info-body">
-                            <div className="row mb-03">
-                                <div className="col-12 col-xl-3">
-                                    <label htmlFor="" className="mb-0" data-tip={null}>
-                                        Consequence:
-                                    </label>
-                                </div>
-                                <div className="col-12 col-xl-9" id="">
-                                    (Need Fieldname)
-                                </div>
-                            </div>
-                            <div className="row mb-03">
-                                <div className="col-12 col-xl-3">
-                                    <label htmlFor="" className="mb-0" data-tip={null}>
-                                        Breakpoint 1:
-                                    </label>
-                                </div>
-                                <div className="col-12 col-xl-9" id="">
-                                    (Need Fieldname)
-                                </div>
-                            </div>
-                            <div className="row mb-03">
-                                <div className="col-12 col-xl-3">
-                                    <label htmlFor="" className="mb-0" data-tip={null}>
-                                        Breakpoint 2:
-                                    </label>
-                                </div>
-                                <div className="col-12 col-xl-9" id="">
-                                    (Need Fieldname)
-                                </div>
-                            </div>
+                            <ConsequenceOfSNVSection currentTranscript={resultTranscripts[currentTranscriptIdx]}/>
                         </div>
                     </div>
-
-
                 </div>
                 <div className="col-12 col-md-6 d-flex flex-column">
 
@@ -104,5 +104,55 @@ export function SvGeneDetailPane(props) {
                 </div>
             </div>
         </div>
+    );
+}
+
+function ConsequenceOfSNVSection({ currentTranscript }) {
+    if (!currentTranscript){
+        return (
+            <div className="row mb-03">
+                <div className="col-12 text-center">
+                    <em>No transcripts available</em>
+                </div>
+            </div>);
+    }
+
+    // const { } = currentTranscript;
+    console.log("currentTranscript", currentTranscript);
+    // TODO: Add Consequence and Breakpoints once they are added
+
+    return (
+        <React.Fragment>
+            <div className="row mb-03">
+                <div className="col-12 col-xl-3">
+                    <label htmlFor="" className="mb-0" data-tip={null}>
+                        Consequence:
+                    </label>
+                </div>
+                <div className="col-12 col-xl-9" id="">
+                    (Need Fieldname)
+                </div>
+            </div>
+            <div className="row mb-03">
+                <div className="col-12 col-xl-3">
+                    <label htmlFor="" className="mb-0" data-tip={null}>
+                        Breakpoint 1:
+                    </label>
+                </div>
+                <div className="col-12 col-xl-9" id="">
+                    (Need Fieldname)
+                </div>
+            </div>
+            <div className="row mb-03">
+                <div className="col-12 col-xl-3">
+                    <label htmlFor="" className="mb-0" data-tip={null}>
+                        Breakpoint 2:
+                    </label>
+                </div>
+                <div className="col-12 col-xl-9" id="">
+                    (Need Fieldname)
+                </div>
+            </div>
+        </React.Fragment>
     );
 }
