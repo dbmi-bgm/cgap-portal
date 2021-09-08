6200907fc857e9dda3db33fbdba8e36f723c630c
Sarah <sarah_reiff@hms.harvard.edu>
2021-09-08 09:49:19 -0400

    fixed foursight url with correct prod link

--- src/encoded/root.py
+++ src/encoded/root.py
@@ -138,7 +138,7 @@ def health_check(config):
         # change when we get a CGAP-specific Foursight
         if env_name and env_name.startswith('fourfront-'):
             fs_env = env_name[len('fourfront-'):]
-            foursight_url = 'https://4miof0jxmh.execute-api.us-east-1.amazonaws.com/api/view/' + fs_env
+            foursight_url = 'https://u9feld4va7.execute-api.us-east-1.amazonaws.com/api/view/' + fs_env
         else:
             foursight_url = None
 
