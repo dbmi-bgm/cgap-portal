1ec4eff045eb55d40621ed466999bfc44025b5bd
Alex Balashov <alexander_balashov@hms.harvard.edu>
2021-09-17 11:58:17 -0400

    Unit Tests

--- src/encoded/tests/test_search_export_tsv.py
+++ src/encoded/tests/test_search_export_tsv.py
@@ -24,6 +24,46 @@ pytestmark = [pytest.mark.working, pytest.mark.schema, pytest.mark.search, pytes
 # design your tests accordingly
 
 
+def check_spreadsheet_rows(result_rows, colname_to_index):
+
+    # Check presence of columns in the doc
+    assert result_rows[0][1] == "Chrom (hg38)"
+    assert result_rows[0][2] == "Pos (hg38)"
+    assert result_rows[0][16] == "Canonical transcript ID"
+    assert result_rows[0][17] == "Canonical transcript location"
+    assert result_rows[0][25] == "Variant Quality"
+
+    # Add all test cases to here
+    expected_data = [
+        {
+            "Chrom (hg38)": "1",
+            "Pos (hg38)": "2030666",
+            "Chrom (hg19)": "1",
+            "Pos (hg19)": "1962105",
+            "Canonical transcript ID": "ENST00000378585",
+            "Canonical transcript location": "Exon 9/9 (3′ UTR)",
+            "Variant Quality": "688.12",
+            "Genotype Quality": "99",
+            "gnomADv3 popmax population": "East Asian",
+            "LOEUF": "0.245"
+        },
+        {
+            "Chrom (hg38)": "16",
+            "Pos (hg38)": "23630011",
+            "Canonical transcript ID": "ENST00000261584",
+            "Canonical transcript location": "Exon 5/13",
+            "Variant Quality": "261.94",
+            "Genotype Quality": "24",
+            "gnomADv3 popmax population": "",
+            "LOEUF": "1.006"
+        }
+    ]
+
+    curr_row_idx = 2 # Result row at index=1 is column descriptions.
+    for ed in expected_data:
+        for name, value in ed.items():
+            assert result_rows[curr_row_idx][colname_to_index[name]] == value
+        curr_row_idx += 1
 
 
 def test_filtering_tab(workbook, html_es_testapp):
@@ -66,18 +106,25 @@ def test_filtering_tab(workbook, html_es_testapp):
 
     # All values are of type string when parsed below.
     result_rows = [ row.rstrip(' \r').split('\t') for row in res.body.decode('utf-8').split('\n') ]
+    colname_to_index = { col_name: col_idx for col_idx, col_name in enumerate(result_rows[0]) }
 
-    assert result_rows[0][1] == "Chrom (hg38)"
-    assert result_rows[0][1] == "Pos (hg38)"
-    assert result_rows[0][16] == "Canonical transcript ID"
-    assert result_rows[0][17] == "Canonical transcript location"
-    assert result_rows[0][25] == "Variant Quality"
+    check_spreadsheet_rows(result_rows, colname_to_index)
+    
 
-    # Result row at index=1 is descriptions.
 
-    assert result_rows[2][1] == "1"
-    assert result_rows[2][1] == "2030666"
-    assert result_rows[2][16] == "ENST00000378585"
-    assert result_rows[2][17] == "Exon 9/9 (3′ UTR)"
-    assert result_rows[2][25] == "688.12"
+
+def test_interpretation_tab(workbook, html_es_testapp):
+
+    res = html_es_testapp.get(
+        '/variant-sample-lists/292250e7-5cb7-4543-85b2-80cd318287b2/@@spreadsheet/suggested-filename-to-use.csv',
+    )
+
+    # Should infer from suggested filename ending in .tsv
+    assert 'text/csv' in res.content_type
+
+    # All values are of type string when parsed below.
+    result_rows = [ row.rstrip(' \r').split('\t') for row in res.body.decode('utf-8').split('\n') ]
+    colname_to_index = { col_name: col_idx for col_idx, col_name in enumerate(result_rows[0]) }
+
+    check_spreadsheet_rows(result_rows, colname_to_index)
 
