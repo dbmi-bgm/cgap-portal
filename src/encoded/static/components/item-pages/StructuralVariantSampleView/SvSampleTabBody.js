0de9bf184cc519e11902488b11dc280d288bef43
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-10-04 12:10:11 -0400

    styling fixes + add tips for sv breakpoint table

--- src/encoded/static/components/item-pages/StructuralVariantSampleView/SvSampleTabBody.js
+++ src/encoded/static/components/item-pages/StructuralVariantSampleView/SvSampleTabBody.js
@@ -1,10 +1,27 @@
 'use strict';
 
 import React from 'react';
+import _ from 'underscore';
+import { schemaTransforms } from '@hms-dbmi-bgm/shared-portal-components/es/components/util';
 
 
 export function SvSampleTabBody(props){
-    const { context = {} } = props;
+    const { context = {}, schemas } = props;
+
+    function getTipForField(field, itemType = "StructuralVariantSample", nestedField = ""){
+        if (!schemas) return null;
+        const schemaProperty = schemaTransforms.getSchemaProperty(field, schemas, itemType);
+
+        if (!nestedField) {
+            return (schemaProperty || {}).description || null;
+        }
+
+        const pathToDescription = nestedField.split(".");
+        pathToDescription.push("description");
+
+        return _.get(schemaProperty || {}, pathToDescription, null);
+    }
+
     return (
         <div className="sample-tab-body card-body">
             <div className="row flex-column flex-lg-row">
@@ -13,7 +30,7 @@ export function SvSampleTabBody(props){
                         <h4>Breakpoint Confidence Intervals</h4>
                     </div>
                     <div className="info-body">
-                        <SvQualityTable {...{ context }} />
+                        <SvQualityTable {...{ context, getTipForField }} />
                     </div>
                 </div>
             </div>
@@ -23,7 +40,7 @@ export function SvSampleTabBody(props){
                         <h4>Genotype</h4>
                     </div>
                     <div className="info-body">
-                        <GenotypeQualityTable {...{ context }} />
+                        <GenotypeQualityTable {...{ context, getTipForField }} />
                     </div>
                 </div>
             </div>
@@ -39,6 +56,7 @@ function SvQualityTable(props) {
             confidence_interval_end = [],
             imprecise = null
         } = {},
+        getTipForField
     } = props;
     const fallbackElem = <em> - </em>;
 
@@ -47,35 +65,41 @@ function SvQualityTable(props) {
 
     const impreciseDisplay = imprecise === false ? "Imprecise" : imprecise === true ? "Precise" : fallbackElem;
     return (
-        <table className="w-100">
-            <thead>
-                <tr>
-                    <th className="text-left w-50">Quality</th>
-                    <th className="text-left">Value</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td className="text-600 text-left">Precise/Imprecise</td>
-                    <td className="text-left">{impreciseDisplay}</td>
-                </tr>
-                <tr>
-                    <td className="text-600 text-left">Confidence interval around left breakpoint</td>
-                    <td className="text-left">{startExists ? confidence_interval_start.join(", "): fallbackElem}</td>
-                </tr>
-                <tr>
-                    <td className="text-600 text-left">Confidence interval around right breakpoint</td>
-                    <td className="text-left">{endExists ? confidence_interval_end.join(", "): fallbackElem}</td>
-                </tr>
-            </tbody>
-        </table>
+        <div className="table-responsive">
+            <table className="w-100">
+                <thead>
+                    <tr>
+                        <th className="text-left" style={{ width: "325px" }}>Quality</th>
+                        <th className="text-left">Value</th>
+                        <th className="text-left">Definition</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className="text-600 text-left">Precise/Imprecise</td>
+                        <td className="text-left">{impreciseDisplay}</td>
+                        <td className="text-left">{ getTipForField("imprecise") }</td>
+                    </tr>
+                    <tr>
+                        <td className="text-600 text-left">Confidence interval around left breakpoint</td>
+                        <td className="text-left">{startExists ? confidence_interval_start.join(", "): fallbackElem}</td>
+                        <td className="text-left">{ getTipForField("confidence_interval_start", "StructuralVariantSample", "items" ) }</td>
+                    </tr>
+                    <tr>
+                        <td className="text-600 text-left">Confidence interval around right breakpoint</td>
+                        <td className="text-left">{endExists ? confidence_interval_end.join(", "): fallbackElem}</td>
+                        <td className="text-left">{ getTipForField("confidence_interval_end", "StructuralVariantSample", "items") }</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     );
 }
 
 
 // TODO: Update with Genotype Quality & Likelihoods when fields are available
 function GenotypeQualityTable(props) {
-    const { context: { samplegeno = [] } = {} } = props;
+    const { context: { samplegeno = [] } = {}, getTipForField } = props;
     const fallbackElem = <em> - </em>;
 
     const rows = samplegeno.map((sg) => {
@@ -98,18 +122,20 @@ function GenotypeQualityTable(props) {
         );
     });
     return (
-        <table className="w-100">
-            <thead>
-                <tr>
-                    <th className="text-left">Relation</th>
-                    <th className="text-left">ID</th>
-                    <th className="text-left">Genotype</th>
-                    <th className="text-left">Genotype Quality</th>
-                    <th className="text-left">Genotype Likelihoods</th>
-                </tr>
-            </thead>
-            <tbody>
-                { rows }
-            </tbody>
-        </table>);
+        <div className="table-responsive">
+            <table className="w-100">
+                <thead>
+                    <tr>
+                        <th className="text-left">Relation</th>
+                        <th className="text-left">ID</th>
+                        <th className="text-left">Genotype</th>
+                        <th className="text-left">Genotype Quality</th>
+                        <th className="text-left">Genotype Likelihoods</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    { rows }
+                </tbody>
+            </table>
+        </div>);
 }
