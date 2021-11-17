1d8b158ac56baa11625a30b125b071b301f2d88e
Alex Balashov <alexander_balashov@hms.harvard.edu>
2021-11-17 18:51:40 -0500

    User Dashboard (Homepage) Search

--- src/encoded/static/components/static-pages/HomePage/UserDashboard.js
+++ src/encoded/static/components/static-pages/HomePage/UserDashboard.js
@@ -1,7 +1,9 @@
 'use strict';
 
-import React, { useMemo } from 'react';
+import React, { useMemo, useCallback, useState, useRef } from 'react';
 import _ from 'underscore';
+import url from 'url';
+import queryString from 'query-string';
 
 import Dropdown from 'react-bootstrap/esm/Dropdown';
 import DropdownButton from 'react-bootstrap/esm/DropdownButton';
@@ -90,6 +92,7 @@ function AboveCasesTableOptions(props){
 
     return (
         <React.Fragment>
+
             <div className="container-wide py-0 bg-white">
                 <div className="tab-section-title flex-wrap">
 
@@ -120,7 +123,8 @@ function AboveCasesTableOptions(props){
             </div>
 
             <hr className="tab-section-title-horiz-divider"/>
-            <div className="container-wide toggle-reports row align-items-center py-2">
+
+            <div className="container-wide toggle-reports row align-items-center py-3">
                 <div className="col-12 col-sm-auto">
                     <ProjectFilterCheckbox isContextLoading={isContextLoading || !context} onChange={onToggleOnlyShowCasesWithReports} checked={onlyShowCasesWithReports}>
                         Show Only Cases with Reports
@@ -130,6 +134,12 @@ function AboveCasesTableOptions(props){
                     <ProjectFilterCheckbox isContextLoading={isContextLoading || !context} onChange={onToggleOnlyShowProbandCases} checked={onlyShowProbandCases}>
                         Show Only Proband Cases
                     </ProjectFilterCheckbox>
+                </div>
+                <div className="d-none d-md-block col-md">
+                    <hr/>
+                </div>
+                <div className="col-12 col-md-auto">
+                    <SearchBar {...{ isContextLoading, context, navigate }} />
                 </div>
             </div>
         </React.Fragment>
@@ -163,7 +173,7 @@ class ProjectFilterCheckbox extends React.PureComponent {
     render(){
         const { isContextLoading, checked, children } = this.props;
         const { isChanging } = this.state;
-        const labelCls = "mb-0 px-2 py-1" + (isChanging ? " is-changing position-relative" : "");
+        const labelCls = "mb-0 py-1" + (isChanging ? " is-changing" : "");
         return (
             <Checkbox disabled={isContextLoading} onChange={this.onChange} checked={checked} labelClassName={labelCls}>
                 <span className="text-small">
@@ -180,19 +190,26 @@ function ProjectSelectDropdown(props){
         context: searchContext,
         navigate: virtualNavigate,
         onFilter,
-        isContextLoading = false
+        isContextLoading = false,
+        className
     } = props;
     const {
         facets: ctxFacets = [],
         filters: ctxFilters,
         "@id": ctxHref
     } = searchContext || {};
-    const projectFacet = _.findWhere(ctxFacets, { "field" : "project.display_title" });
-    const projectFilter = _.findWhere(ctxFilters, { "field" : "project.display_title" }) || null;
+
+    const { projectFacet, projectFilter } = useMemo(function(){
+        return {
+            projectFacet: _.findWhere(ctxFacets, { "field" : "project.display_title" }) || null,
+            projectFilter: _.findWhere(ctxFilters, { "field" : "project.display_title" }) || null
+        };
+    }, [ searchContext ]);
+
     const { term: projectFilterTerm = null } = projectFilter || {};
     const { terms: facetTerms = [] } = projectFacet || {};
 
-    function onTermSelect(evtKey, e){
+    const onTermSelect = useCallback(function(evtKey, e){
         e.preventDefault();
         if (!evtKey) {
             if (projectFilter) {
@@ -213,34 +230,94 @@ function ProjectSelectDropdown(props){
             const updatedSearchHref = searchFilters.filtersToHref(updatedFilters, ctxHref, null, false, null);
             virtualNavigate(updatedSearchHref);
         }
-    }
+    }, [ onFilter, projectFilter, projectFacet ]);
 
-    let options = null;
-    if (!isContextLoading) {
-        options = facetTerms.sort(function({ key: a, doc_count: aDC }, { key: b, doc_count: bDC }){
-            if (a === "CGAP Core") return -1;
-            if (b === "CGAP Core") return 1;
-            return bDC - aDC;
-        }).map(function(projectTermObj){
-            const { key: projectTerm, doc_count } = projectTermObj;
-            const active = projectTerm === projectFilterTerm;
-            return (
-                <DropdownItem key={projectTerm} eventKey={projectTerm} active={active}>
-                    { Term.toName("project.display_title", projectTerm) }
-                    <small className="ml-07">({ doc_count })</small>
-                </DropdownItem>
-            );
-        });
-    }
+    const renderedOptions = useMemo(function(){
+        let options = null;
+        if (!isContextLoading) {
+            options = facetTerms.sort(function({ key: a, doc_count: aDC }, { key: b, doc_count: bDC }){
+                if (a === "CGAP Core") return -1;
+                if (b === "CGAP Core") return 1;
+                return bDC - aDC;
+            }).map(function(projectTermObj){
+                const { key: projectTerm, doc_count } = projectTermObj;
+                const active = projectTerm === projectFilterTerm;
+                return (
+                    <DropdownItem key={projectTerm} eventKey={projectTerm} active={active}>
+                        { Term.toName("project.display_title", projectTerm) }
+                        <small className="ml-07">({ doc_count })</small>
+                    </DropdownItem>
+                );
+            });
+        }
+        return options;
+    }, [ isContextLoading, facetTerms ]);
 
     return (
         <DropdownButton disabled={isContextLoading || facetTerms.length === 0}
-            title={ projectFilterTerm || "All Projects" } onSelect={onTermSelect} variant="outline-dark">
+            title={projectFilterTerm || "All Projects"} onSelect={onTermSelect}
+            variant="outline-dark" className={className}>
             <DropdownItem eventKey={0} active={!projectFilterTerm}>
                 <span className="text-600">All Projects</span>
             </DropdownItem>
-            { options }
+            { renderedOptions }
         </DropdownButton>
     );
 
+}
+
+function SearchBar (props) {
+    const {
+        context: searchContext,
+        isContextLoading,
+        navigate: virtualNavigate
+    } = props;
+    // This should be present & accurate on search response as long as is not compound filterset
+    // search with 2+ filterblocks used.
+    const { "@id": currentSearchHref } = searchContext || {};
+
+    const disabled = isContextLoading || !currentSearchHref;
+
+    const currentSearchParts = useMemo(function(){
+        if (!currentSearchHref) {
+            return null;
+        }
+        return url.parse(currentSearchHref, true);
+    }, [ searchContext ]);
+
+    const { query: currentSearchQuery } = currentSearchParts || {};
+    const { q: currentSearchTextQuery = "" } = currentSearchQuery || {};
+
+    // const [ value, setValue ] = useState(currentSearchTextQuery);
+
+    // const onChange = useCallback(function(e){
+    //     const { target: { value: evtValue } } = e;
+    //     setValue(evtValue);
+    // });
+
+    const searchInputRef = useRef(null);
+
+    const onSubmit = useCallback(function(e){
+        e.stopPropagation();
+        e.preventDefault();
+        const nextQuery = { ...currentSearchQuery };
+        const nextValue = searchInputRef.current.value;
+        if (nextValue) {
+            nextQuery.q = nextValue;
+        } else {
+            delete nextQuery.q;
+        }
+        const nextSearchParts = {
+            ...currentSearchParts,
+            "search": "?" + queryString.stringify(nextQuery)
+        };
+        virtualNavigate(url.format(nextSearchParts));
+    }, [ virtualNavigate, currentSearchParts ]);
+
+    return (
+        <form onSubmit={onSubmit} className="mb-0">
+            <input type="text" className="form-control" placeholder="Search..."
+                {...{ disabled }} defaultValue={currentSearchTextQuery} ref={searchInputRef} />
+        </form>
+    );
 }
