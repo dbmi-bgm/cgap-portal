769d7b9a2ab5dafd3395abec1c15fbfedb7610c5
Alex Balashov <alexander_balashov@hms.harvard.edu>
2022-01-11 14:01:46 -0500

    Add'l Comment

--- src/encoded/tests/test_types_note.py
+++ src/encoded/tests/test_types_note.py
@@ -161,6 +161,7 @@ def test_patch_standard_note_fail(workbook, es_testapp, new_standard_note):
     patch_info = {"conclusion": "For these reasons we made a conclusion."}  # not a std note property
     es_testapp.patch_json('/' + resp['@id'], patch_info, status=422)
 
+# Will be possibly re-enabled if data model is updated back to having this field as a linkTo Note.
 # def test_link_standard_note_to_report(workbook, es_testapp, new_standard_note, test_report):
 #     """ test NoteStandard linkTo on Report item works """
 #     note = post_note(es_testapp, new_standard_note, note_type='note_standard').json['@graph'][0]
