4dc935e1209279592f4c63b9d2f284d3766466e2
William Ronchetti <wrr33@cornell.edu>
2022-01-12 16:35:16 -0500

    change how port is passed

--- deploy/docker/production/entrypoint_portal.sh
+++ deploy/docker/production/entrypoint_portal.sh
@@ -11,11 +11,11 @@ service nginx start
 
 # Start application
 echo "Starting server 1"
-pserve production.ini --port 6543 &
+pserve production.ini http_port=6543 &
 echo "Starting server 2"
-pserve production.ini --port 6544 &
+pserve production.ini http_port=6544 &
 echo "Starting server 3"
-pserve production.ini --port 6545 &
+pserve production.ini http_port=6545 &
 echo "Starting server 4"
-pserve production.ini --port 6546
+pserve production.ini http_port=6546
 
