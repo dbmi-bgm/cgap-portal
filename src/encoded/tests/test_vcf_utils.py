3464f8b7a35e0f51f49c288d0b68d755cd596481
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2022-01-18 12:55:40 -0500

    Update VCF ingestion tests

--- src/encoded/tests/test_vcf_utils.py
+++ src/encoded/tests/test_vcf_utils.py
@@ -11,13 +11,45 @@ from .variant_fixtures import (  # noqa
 
 
 pytestmark = [pytest.mark.working, pytest.mark.ingestion]
-TEST_VCF = './src/encoded/annotations/GAPFIBVPFEP5_v0.5.4.reformat.altcounts.vcf.subset'
+TEST_VCF = resolve_file_path(
+    "annotations/GAPFII76KW2T_v0.5.5.reformat.altcounts.vcf.subset"
+)
 EXPECTED_ANNOTATION_FIELDS = ['comHet', 'CSQ']
-VARIANT_SCHEMA = './src/encoded/schemas/variant.json'
-VARIANT_SAMPLE_SCHEMA = './src/encoded/schemas/variant_sample.json'
-TEST_SV_VCF = resolve_file_path("annotations/GAPFIV4NLMMT_v0.0.3.vcf.subset")
+VARIANT_SCHEMA = resolve_file_path("schemas/variant.json")
+VARIANT_SAMPLE_SCHEMA = resolve_file_path("schemas/variant_sample.json")
+TEST_SV_VCF = resolve_file_path("annotations/GAPFIQHD6QAN_v0.0.4.vcf.subset")
 SV_SCHEMA = resolve_file_path("schemas/structural_variant.json")
 SV_SAMPLE_SCHEMA = resolve_file_path("schemas/structural_variant_sample.json")
+VEP_IDENTIFIER = 'transcript'
+GENE_IDENTIFIER = 'genes'
+HG19_IDENTIFIER = 'hg19'
+
+
+def get_top_level_field(item, field):
+    if field not in item:
+        return None
+    return item[field]
+
+
+def get_sub_annotation_field(item, idx, identifier, field):
+    annotations = get_top_level_field(item, identifier)
+    if annotations is None:
+        return None
+    if idx > len(annotations):
+        raise ValueError('Index out of range on annotations %s, idx: %s' % (identifier, idx))
+    return annotations[idx].get(field, None)
+
+
+def get_transcript_field(item, idx, field):
+    return get_sub_annotation_field(item, idx, VEP_IDENTIFIER, field)
+
+
+def get_genes_field(item, idx, field):
+    return get_sub_annotation_field(item, idx, GENE_IDENTIFIER, field)
+
+
+def get_hg19_field(item, idx, field):
+    return get_sub_annotation_field(item, idx, HG19_IDENTIFIER, field)
 
 
 @pytest.fixture
@@ -27,33 +59,6 @@ def test_vcf():
 
 
 class TestIngestVCF:
-
-    VEP_IDENTIFIER = 'transcript'
-    GENE_IDENTIFIER = 'genes'
-    HG19_IDENTIFIER = 'hg19'
-
-    @staticmethod
-    def get_top_level_field(item, field):
-        if field not in item:
-            return None
-        return item[field]
-
-    def _get_sub_annotation_field(self, item, idx, identifier, field):
-        annotations = self.get_top_level_field(item, identifier)
-        if annotations is None:
-            return None
-        if idx > len(annotations):
-            raise ValueError('Index out of range on annotations %s, idx: %s' % (identifier, idx))
-        return annotations[idx].get(field, None)
-
-    def get_transcript_field(self, item, idx, field):
-        return self._get_sub_annotation_field(item, idx, self.VEP_IDENTIFIER, field)
-
-    def get_genes_field(self, item, idx, field):
-        return self._get_sub_annotation_field(item, idx, self.GENE_IDENTIFIER, field)
-
-    def get_hg19_field(self, item, idx, field):
-        return self._get_sub_annotation_field(item, idx, self.HG19_IDENTIFIER, field)
 
     def test_parser_meta(self, test_vcf):
         """ Checks that we can correctly read which fields are annotation fields
@@ -75,17 +80,17 @@ class TestIngestVCF:
         result = test_vcf.create_variant_from_record(record)
 
         # check top level fields
-        assert self.get_top_level_field(result, 'CHROM') == '8'
-        assert self.get_top_level_field(result, 'csq_spliceai_pred_ds_al') == 0.0  # default
-        assert self.get_top_level_field(result, 'csq_spliceai_pred_dp_dg') == 38
-        assert self.get_top_level_field(result, 'csq_spliceai_pred_dp_ag') == -47
+        assert get_top_level_field(result, 'CHROM') == '8'
+        assert get_top_level_field(result, 'csq_spliceai_pred_ds_al') == 0.0  # default
+        assert get_top_level_field(result, 'csq_spliceai_pred_dp_dg') == 38
+        assert get_top_level_field(result, 'csq_spliceai_pred_dp_ag') == -47
 
         # check sub-embedded object fields
         assert len(result['transcript']) == 1
-        assert self.get_transcript_field(result, 0, 'csq_consequence') == ['splice_region_variant']
-        assert self.get_transcript_field(result, 0, 'csq_gene') == 'ENSG00000261236'
-        assert self.get_transcript_field(result, 0, 'csq_feature') == 'ENST00000569669'
-        assert self.get_transcript_field(result, 0, 'csq_strand') is False
+        assert get_transcript_field(result, 0, 'csq_consequence') == ['splice_region_variant']
+        assert get_transcript_field(result, 0, 'csq_gene') == 'ENSG00000261236'
+        assert get_transcript_field(result, 0, 'csq_feature') == 'ENST00000569669'
+        assert get_transcript_field(result, 0, 'csq_strand') is False
 
     def test_build_multiple_variants(self, test_vcf):
         """
@@ -98,35 +103,43 @@ class TestIngestVCF:
         result = test_vcf.create_variant_from_record(record)
 
         # check top level fields
-        assert self.get_top_level_field(result, 'csq_cadd_phred') == 2.692
-        assert self.get_top_level_field(result, 'csq_spliceai_pred_symbol') == 'SAMD11'
-        assert self.get_top_level_field(result, 'csq_gnomadg_ac') == 340
-        assert self.get_top_level_field(result, 'csq_spliceai_pred_dp_ag') == 8
+        assert get_top_level_field(result, 'csq_cadd_phred') == 2.692
+        assert get_top_level_field(result, 'csq_spliceai_pred_symbol') == 'SAMD11'
+        assert get_top_level_field(result, 'csq_gnomadg_ac') == 340
+        assert get_top_level_field(result, 'csq_spliceai_pred_dp_ag') == 8
+        assert get_top_level_field(result, "hgvsg") == (
+            "NC_000001.11:g.943547_943549del"
+        )
+        assert get_top_level_field(result, "hgvsg_hg19") == (
+            "NC_000001.10:g.878927_878929del"
+        )
+        assert get_top_level_field(result, "hg19_chr") == "1"
+        assert get_top_level_field(result, "hg19_pos") == 878926
 
         # check transcript
         assert len(result['transcript'].keys()) == 11
-        assert self.get_transcript_field(result, 0, 'csq_consequence') == ['intron_variant']
-        assert self.get_transcript_field(result, 0, 'csq_feature') == 'ENST00000341065'
-        assert self.get_transcript_field(result, 0, "csq_strand") is True
+        assert get_transcript_field(result, 0, 'csq_consequence') == ['intron_variant']
+        assert get_transcript_field(result, 0, 'csq_feature') == 'ENST00000341065'
+        assert get_transcript_field(result, 0, "csq_strand") is True
 
         # check genes
-        assert self.get_genes_field(result, 0, 'genes_most_severe_gene') == 'ENSG00000187634'
-        assert self.get_genes_field(result, 0, 'genes_most_severe_transcript') == 'ENST00000342066'
+        assert get_genes_field(result, 0, 'genes_most_severe_gene') == 'ENSG00000187634'
+        assert get_genes_field(result, 0, 'genes_most_severe_transcript') == 'ENST00000342066'
 
         # check record 3 (only a few things)
         record = test_vcf.read_next_record()
         result = test_vcf.create_variant_from_record(record)
-        assert len(result[self.VEP_IDENTIFIER].keys()) == 1
-        assert self.get_top_level_field(result, 'spliceaiMaxds') == 0
-        assert self.get_top_level_field(result, 'variantClass') == 'SNV'
-        assert self.get_top_level_field(result, 'csq_spliceai_pred_symbol') == 'NOC2L'
-        assert self.get_transcript_field(result, 0, 'csq_consequence') == ['missense_variant']
-        assert self.get_transcript_field(result, 0, 'csq_feature') == 'ENST00000327044'
-        assert self.get_transcript_field(result, 0, 'csq_trembl') is None
-        assert self.get_transcript_field(result, 0, 'csq_domains') == ['Pfam:PF03715', 'PANTHER:PTHR12687',
+        assert len(result[VEP_IDENTIFIER].keys()) == 1
+        assert get_top_level_field(result, 'spliceaiMaxds') == 0
+        assert get_top_level_field(result, 'variantClass') == 'SNV'
+        assert get_top_level_field(result, 'csq_spliceai_pred_symbol') == 'NOC2L'
+        assert get_transcript_field(result, 0, 'csq_consequence') == ['missense_variant']
+        assert get_transcript_field(result, 0, 'csq_feature') == 'ENST00000327044'
+        assert get_transcript_field(result, 0, 'csq_trembl') is None
+        assert get_transcript_field(result, 0, 'csq_domains') == ['Pfam:PF03715', 'PANTHER:PTHR12687',
                                                                        'PANTHER:PTHR12687:SF10', 'Superfamily:SSF48371']
-        assert self.get_genes_field(result, 0, 'genes_most_severe_gene') == 'ENSG00000188976'
-        assert self.get_genes_field(result, 0, 'genes_most_severe_hgvsc') == 'ENST00000327044.7:c.1528A>C'
+        assert get_genes_field(result, 0, 'genes_most_severe_gene') == 'ENSG00000188976'
+        assert get_genes_field(result, 0, 'genes_most_severe_hgvsc') == 'ENST00000327044.7:c.1528A>C'
 
         # check record 4 (new gnomade2 fields)
         record = test_vcf.read_next_record()
@@ -140,19 +153,19 @@ class TestIngestVCF:
         record = test_vcf.read_next_record()
         result = test_vcf.create_sample_variant_from_record(record)
         for sample in result:
-            assert self.get_top_level_field(sample, 'GT') != '0/0'  # this VCF has one of these that should be dropped
+            assert get_top_level_field(sample, 'GT') != '0/0'  # this VCF has one of these that should be dropped
         assert result[0]['FS'] == 0.0
         assert result[0]['GT'] == '0/1'
 
         record = test_vcf.read_next_record()
         result = test_vcf.create_sample_variant_from_record(record)[0]  # inspect first
-        assert self.get_top_level_field(result, 'DP') == 12
-        assert self.get_top_level_field(result, 'GT') == '0/1'
-        assert self.get_top_level_field(result, 'GQ') == 63
-        assert self.get_top_level_field(result, 'PL') == '63,0,226'
+        assert get_top_level_field(result, 'DP') == 12
+        assert get_top_level_field(result, 'GT') == '0/1'
+        assert get_top_level_field(result, 'GQ') == 63
+        assert get_top_level_field(result, 'PL') == '63,0,226'
         record = test_vcf.read_next_record()
         result = test_vcf.create_sample_variant_from_record(record)[0]
-        assert self.get_top_level_field(result, 'DP') == 37
+        assert get_top_level_field(result, 'DP') == 37
         assert len(result['samplegeno']) == 3  # just check field presence
         assert result['samplegeno'][0]['samplegeno_numgt'] == '0/0'
         assert result['samplegeno'][0]['samplegeno_ad'] == '36/0'
@@ -212,9 +225,7 @@ def test_sv_vcf():
     return parser
 
 
-class TestIngestStructuralVariantVCF(TestIngestVCF):
-
-    VEP_IDENTIFIER = "transcript"  # In case of future divergence from SNV tests
+class TestIngestStructuralVariantVCF():
 
     def test_build_variants(self, test_sv_vcf):
         """
@@ -226,53 +237,63 @@ class TestIngestStructuralVariantVCF(TestIngestVCF):
         record = test_sv_vcf.read_next_record()
         result = test_sv_vcf.create_variant_from_record(record)
 
-        assert self.get_top_level_field(result, "CHROM") == "1"
-        assert self.get_top_level_field(result, "START") == 31908111
-        assert self.get_top_level_field(result, "END") == 31908161
-        assert self.get_top_level_field(result, "SV_TYPE") == "DEL"
-        assert self.get_top_level_field(result, "cytoband_start") == "p35.2"
-        assert self.get_top_level_field(result, "cytoband_end") == "p35.2"
-        assert self.get_top_level_field(result, "unrelated_count") == 1
-        assert len(result["transcript"]) == 9
-        assert self.get_transcript_field(result, 0, "csq_consequence") == [
+        assert get_top_level_field(result, "CHROM") == "1"
+        assert get_top_level_field(result, "START") == 31908111
+        assert get_top_level_field(result, "END") == 31908161
+        assert get_top_level_field(result, "SV_TYPE") == "DEL"
+        assert get_top_level_field(result, "cytoband_start") == "p35.2"
+        assert get_top_level_field(result, "cytoband_end") == "p35.2"
+        assert get_top_level_field(result, "unrelated_count") == 1
+        assert get_top_level_field(result, "hg19_chr") == "1"
+        assert get_top_level_field(result, "hg19_pos") == 32373712
+        assert get_top_level_field(result, "hg19_end") == 32373762
+        assert len(result["transcript"]) == 6
+        assert get_transcript_field(result, 0, "csq_consequence") == [
             "downstream_gene_variant"
         ]
-        assert self.get_transcript_field(result, 0, "csq_gene") == "ENSG00000184007"
-        assert self.get_transcript_field(result, 0, "csq_feature") == "ENST00000457805"
-        assert self.get_transcript_field(result, 0, "csq_biotype") == "protein_coding"
-        assert self.get_transcript_field(result, 0, "csq_distance") == "373"
-        assert self.get_transcript_field(result, 0, "csq_strand") is False
-        assert not self.get_transcript_field(result, 0, "csq_canonical")
-        assert self.get_transcript_field(result, 6, "csq_exon") == "5/5"
-        assert not self.get_transcript_field(result, 6, "csq_intron")
-        assert self.get_transcript_field(result, 6, "csq_cdna_position") == "1613-1662"
-        assert self.get_transcript_field(result, 7, "csq_canonical") is True
+        assert get_transcript_field(result, 0, "csq_gene") == "ENSG00000184007"
+        assert get_transcript_field(result, 0, "csq_feature") == "ENST00000457805"
+        assert get_transcript_field(result, 0, "csq_biotype") == "protein_coding"
+        assert get_transcript_field(result, 0, "csq_distance") == "373"
+        assert get_transcript_field(result, 0, "csq_strand") is False
+        assert not get_transcript_field(result, 0, "csq_canonical")
+        assert get_transcript_field(result, 3, "csq_exon") == "5/5"
+        assert not get_transcript_field(result, 3, "csq_intron")
+        assert get_transcript_field(result, 3, "csq_cdna_position") == "1613-1662"
+        assert get_transcript_field(result, 4, "csq_canonical") is True
+        assert get_transcript_field(result, 4, "csq_most_severe") is True
+        assert get_transcript_field(
+            result, 4, "csq_variant_5_prime_location"
+        ) == "3_UTR"
+        assert get_transcript_field(
+            result, 4, "csq_variant_3_prime_location"
+        ) == "3_UTR_or_Downstream"
         variant_keys = result.keys()
         for key in variant_keys:
             assert "gnomadg" not in key
 
-        # record 2 - gnomAD-SV values + Transcript intron
+        # record 2 - gnomAD-SV values + Transcript strand
         record = test_sv_vcf.read_next_record()
         result = test_sv_vcf.create_variant_from_record(record)
 
-        assert self.get_top_level_field(result, "gnomadg_ac") == 8
-        assert self.get_top_level_field(result, "gnomadg_af-afr") == 0.000757
-        assert self.get_top_level_field(result, "gnomadg_an-eur") == 7608
-        assert self.get_transcript_field(result, 5, "csq_intron") == "1/1"
-        assert self.get_transcript_field(result, 0, "csq_strand") is True
+        assert get_top_level_field(result, "gnomadg_ac") == 8
+        assert get_top_level_field(result, "gnomadg_af-afr") == 0.000757
+        assert get_top_level_field(result, "gnomadg_an-eur") == 7608
+        assert get_transcript_field(result, 0, "csq_strand") is True
 
         # record 3 - Additional Transcript fields
         record = test_sv_vcf.read_next_record()
         result = test_sv_vcf.create_variant_from_record(record)
         
-        assert self.get_transcript_field(result, 0, "csq_cds_position") == "3594-4583"
-        assert self.get_transcript_field(
+        assert get_transcript_field(result, 0, "csq_cds_position") == "3594-4583"
+        assert get_transcript_field(
             result, 0, "csq_protein_position"
         ) == "1198-1528"
-        assert self.get_transcript_field(result, 0, "csq_codons").startswith("agTCTCCC")
-        assert self.get_transcript_field(result, 0, "csq_amino_acids").startswith(
+        assert get_transcript_field(result, 0, "csq_codons").startswith("agTCTCCC")
+        assert get_transcript_field(result, 0, "csq_amino_acids").startswith(
             "SLPSMQGDLK"
         )
+        assert get_transcript_field(result, 1, "csq_intron") == "1/2"
 
     def test_build_variant_samples(self, test_sv_vcf):
         """
@@ -285,15 +306,15 @@ class TestIngestStructuralVariantVCF(TestIngestVCF):
         result = test_sv_vcf.create_sample_variant_from_record(record)
         
         assert len(result) == 1
-        assert self.get_top_level_field(result[0], "GT") == "0/1"
-        assert self.get_top_level_field(result[0], "CALL_INFO") == "NA12878_sample"
-        assert self.get_top_level_field(result[0], "GQ") == 43
-        assert self.get_top_level_field(result[0], "PL") == "158,0,40"
-        assert not self.get_top_level_field(result[0], "imprecise")
-        assert self.get_top_level_field(
+        assert get_top_level_field(result[0], "GT") == "0/1"
+        assert get_top_level_field(result[0], "CALL_INFO") == "NA12878_sample"
+        assert get_top_level_field(result[0], "GQ") == 43
+        assert get_top_level_field(result[0], "PL") == "158,0,40"
+        assert not get_top_level_field(result[0], "imprecise")
+        assert get_top_level_field(
             result[0], "confidence_interval_start"
         ) == [0, 1]
-        sample_geno = self.get_top_level_field(result[0], "samplegeno")
+        sample_geno = get_top_level_field(result[0], "samplegeno")
         assert len(sample_geno) == 3
         assert sample_geno[0]["samplegeno_numgt"] == "0/0"
         assert sample_geno[2]["samplegeno_sampleid"] == "NA12877_sample"
@@ -303,7 +324,7 @@ class TestIngestStructuralVariantVCF(TestIngestVCF):
         result = test_sv_vcf.create_sample_variant_from_record(record)
 
         assert len(result) == 2
-        sample_geno = self.get_top_level_field(result[1], "samplegeno")
+        sample_geno = get_top_level_field(result[1], "samplegeno")
         assert len(sample_geno) == 3
         assert sample_geno[1]["samplegeno_sampleid"] == "NA12878_sample"
         assert sample_geno[1]["samplegeno_quality"] == 48
@@ -316,10 +337,10 @@ class TestIngestStructuralVariantVCF(TestIngestVCF):
         result = test_sv_vcf.create_sample_variant_from_record(record)
 
         for idx in range(len(result)):
-            assert self.get_top_level_field(result[idx], "imprecise") is True
-            assert self.get_top_level_field(
+            assert get_top_level_field(result[idx], "imprecise") is True
+            assert get_top_level_field(
                 result[idx], "confidence_interval_start"
             ) == [-236, 237]
-            assert self.get_top_level_field(
+            assert get_top_level_field(
                 result[idx], "confidence_interval_end"
             ) == [-136, 136]
