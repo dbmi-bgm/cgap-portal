e81c3228ea85cec5260db0b4246089de7fc03a72
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2022-01-31 13:29:16 -0500

    Clean up

--- src/encoded/static/components/item-pages/components/HiGlass/EmbeddedCohortBrowser.js
+++ src/encoded/static/components/item-pages/components/HiGlass/EmbeddedCohortBrowser.js
@@ -2,60 +2,59 @@
 
 import React, { useRef, useEffect } from "react";
 import _ from "underscore";
-import { ajax } from '@hms-dbmi-bgm/shared-portal-components/es/components/util';
-
+import { ajax } from "@hms-dbmi-bgm/shared-portal-components/es/components/util";
 import { HiGlassAjaxLoadContainer } from "./HiGlassAjaxLoadContainer";
 
-
-export class GeneSearchBar extends React.PureComponent {
+class GeneSearchBar extends React.PureComponent {
   constructor(props) {
-      super(props);
-      const { higlassContainer } = props;
-      this.higlassContainer = higlassContainer;
-      
-      this.state = {
-          results : [],
-          currentTextValue : props.value || "",
-          loading: false, // starts out by loading base RequestURL
-          error: null,
-          info: null,
-      };
+    super(props);
+    const { higlassContainer } = props;
+    this.higlassContainer = higlassContainer;
 
-      this.currentRequest = null;
-      this.onLoadData = _.debounce(this.onLoadData.bind(this), 500, false);
-      this.handleResultClick = this.handleResultClick.bind(this);
+    this.state = {
+      results: [],
+      currentTextValue: props.value || "",
+      loading: false, // starts out by loading base RequestURL
+      error: null,
+      info: null,
+    };
+
+    this.currentRequest = null;
+    this.onLoadData = _.debounce(this.onLoadData.bind(this), 500, false);
+    this.handleResultClick = this.handleResultClick.bind(this);
   }
 
-  componentDidUpdate(pastProps, pastState){
-      
-  }
+  componentDidUpdate(pastProps, pastState) {}
 
   updateCurrentSearchTerm(evt) {
     const val = evt.target.value;
-    this.setState({
-      currentTextValue: val
-    }, () => {
-      this.onLoadData();
-    });
-    
+    this.setState(
+      {
+        currentTextValue: val,
+      },
+      () => {
+        this.onLoadData();
+      }
+    );
   }
 
-  constructFetchURL(){
+  constructFetchURL() {
     const { currentTextValue } = this.state;
-    const url = "https://cgap-higlass.com/api/v1/suggest/?d=gene_annotation_hg38&ac=" + encodeURIComponent(currentTextValue);
+    const url =
+      "https://cgap-higlass.com/api/v1/suggest/?d=gene_annotation_hg38&ac=" +
+      encodeURIComponent(currentTextValue);
     return url;
   }
 
-  onLoadData(){
+  onLoadData() {
     this.setState({ loading: true }, () => {
-
       if (this.currentRequest) {
         // if there's already a request running, abort it
         this.currentRequest.abort && this.currentRequest.abort();
       }
 
       // Don't load anything, if there is only one letter
-      if(this.state.currentTextValue.length <= 1){
+      if (this.state.currentTextValue.length <= 1) {
         this.setState({
           currentTextValue: this.state.currentTextValue,
           loading: false,
@@ -65,39 +64,60 @@ export class GeneSearchBar extends React.PureComponent {
         });
         return;
       }
-  
-      const requestInThisScope = this.currentRequest = ajax.load(this.constructFetchURL(), (response) => {
+
+      const requestInThisScope = (this.currentRequest = ajax.load(
+        this.constructFetchURL(),
+        (response) => {
           if (requestInThisScope !== this.currentRequest) {
-              return false; // some other request has been fired; cancel this one
+            return false; // some other request has been fired; cancel this one
           }
           this.currentRequest = null;
-          console.log(response)
-  
+
           if (!response) {
-              this.setState({ loading: false, results: [], error: "Could not get a response from server. Check network and try again.", info: null });
-              return;
+            this.setState({
+              loading: false,
+              results: [],
+              error: "Could not get a response from server. Check network and try again.",
+              info: null,
+            });
+            return;
           }
 
           if (Object.keys(response).length === 0) {
-            this.setState({ loading: false, results: [], error: null, info: "We could not find any matching genes." });
+            this.setState({
+              loading: false,
+              results: [],
+              error: null,
+              info: "We could not find any matching genes.",
+            });
             return;
           }
-          if(this.state.loading){
-            this.setState({ loading: false, results: response, error: null , info: null});
+          if (this.state.loading) {
+            this.setState({
+              loading: false,
+              results: response,
+              error: null,
+              info: null,
+            });
           }
-          
-      }, "GET", (response, xhr) => {
-        const { results = [], error = null } = response;
-        this.setState({ loading: false, results, error: error || "Something went wrong while searching for matching genes.", info: null });
+        },
+        "GET",
+        (response, xhr) => {
+          const { results = [], error = null } = response;
+          this.setState({
+            loading: false,
+            results,
+            error: error || "Something went wrong while searching for matching genes.",
+            info: null,
+          });
+        }
+      ));
     });
-
-    });
-
   }
 
-  handleResultClick(evt, geneName){
+  handleResultClick(evt, geneName) {
     evt.preventDefault();
-    if(this.higlassContainer && this.higlassContainer.current){
+    if (this.higlassContainer && this.higlassContainer.current) {
       const hgc = this.higlassContainer.current.getHiGlassComponent();
       const viewconf = hgc.api.getViewConfig();
       const viewId = viewconf.views[0].uid;
@@ -106,166 +126,214 @@ export class GeneSearchBar extends React.PureComponent {
   }
 
   render() {
-      
-      const { results = [], loading, error, info } = this.state;
+    const { results = [], loading, error, info } = this.state;
 
-      const icon = loading ? "icon icon-spinner icon-spin fas" : "icon icon-search fas";
+    const icon = loading
+      ? "icon icon-spinner icon-spin fas"
+      : "icon icon-search fas";
 
-      return (
-        <div>
-          
-          <div className="input-group">
-            <div className="input-group-prepend">
-              <span className="input-group-text" id="basic-addon1"><i className={icon}/></span>
-            </div>
-            <input type="text" onChange={evt => this.updateCurrentSearchTerm(evt)} className="form-control" placeholder="Search for gene" aria-label="Search for gene" aria-describedby="basic-addon1" />
+    return (
+      <div>
+        <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="search-for-gene-addon">
+              <i className={icon} />
+            </span>
           </div>
-          <GeneSearchResult results={results} error={error} info={info} handleResultClick={this.handleResultClick} />
-
-          
+          <input
+            type="text"
+            onChange={(evt) => this.updateCurrentSearchTerm(evt)}
+            className="form-control"
+            placeholder="Search for gene"
+            aria-label="Search for gene"
+            aria-describedby="search-for-gene-addon"
+          />
         </div>
-      );
+        <GeneSearchResult
+          results={results}
+          error={error}
+          info={info}
+          handleResultClick={this.handleResultClick}
+        />
+      </div>
+    );
   }
 }
 
-
-export const GeneSearchResult = React.memo(function GeneSearchResult(props){
-
+const GeneSearchResult = React.memo(function GeneSearchResult(props) {
   const { results, error, info, handleResultClick } = props;
 
-  if(error){
-    return <div><i className="icon icon-exclamation-triangle fas text-warning ml-5 mr-1"></i>{error}</div>;
-  }
-  else if(info){
-    return <div><i className="icon icon-info-circle fas text-info ml-5 mr-1"></i>{info}</div>;
+  if (error) {
+    return (
+      <div>
+        <i className="icon icon-exclamation-triangle fas text-warning ml-5 mr-1"></i>
+        {error}
+      </div>
+    );
+  } else if (info) {
+    return (
+      <div>
+        <i className="icon icon-info-circle fas text-info ml-5 mr-1"></i>
+        {info}
+      </div>
+    );
   }
 
   const resultsFormatted = results.map((result, i) => {
-    const style = i === 0 ? "ml-5" : "";
-    return <div className="mr-3"><a href="#" onClick={(evt) => handleResultClick(evt, result.geneName)} className={style} key={i}>{result.geneName}</a></div>;
+    return (
+      <div className="mr-3" key={result.geneName}>
+        <a
+          href="#"
+          onClick={(evt) => handleResultClick(evt, result.geneName)}
+        >
+          {result.geneName}
+        </a>
+      </div>
+    );
   });
 
-  return (
-    <div className="d-flex flex-row bd-highlight">
-      {resultsFormatted}
-    </div>
-  );
-  
-  
+  return <div className="d-flex flex-row flex-wrap ml-5 bd-highlight">{resultsFormatted}</div>;
 });
 
-
-export class EmbeddedCohortBrowserUnit extends React.PureComponent {
-
+class EmbeddedCohortBrowserComponent extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    const { cohortVcfLocation, cohortDensityBwLocation, higlassContainerCohort, higlassContainerAnnotation } = props;
+    const {
+      cohortVcfLocation,
+      cohortDensityBwLocation,
+      higlassContainerCohort,
+      higlassContainerAnnotation,
+    } = props;
     this.cohortVcfLocation = cohortVcfLocation;
     this.cohortDensityBwLocation = cohortDensityBwLocation;
     this.higlassContainerCohort = higlassContainerCohort;
     this.higlassContainerAnnotation = higlassContainerAnnotation;
 
     this.state = {
-        activeTab: "cohort"
+      activeTab: "cohort",
     };
 
     this.handleTabClick = this.handleTabClick.bind(this);
-
-    
   }
 
-  handleTabClick(evt, target){
+  handleTabClick(evt, target) {
     evt.preventDefault();
-    this.setState({
-      activeTab: target
-    }, () => {
-      if(target === "cohort"){
-        const hgc = this.higlassContainerCohort.current.getHiGlassComponent();
-        if(hgc){
-          // Force Higlass to repaint. Without this the tracks can be positioned incorrectly
-          hgc.boundRefreshView();
-        }
-      }else{
-        const hga = this.higlassContainerAnnotation.current.getHiGlassComponent();
-        const hgc = this.higlassContainerCohort.current.getHiGlassComponent();
-        if(hga){
-          // Force Higlass to repaint. Without this the tracks can be positioned incorrectly
-          //hga.boundRefreshView();
-        }
-        const viewconfCohort = hgc.api.getViewConfig();
-        const xDomain = viewconfCohort.views[0].initialXDomain;
-        const viewconfAnnot = hga.api.getViewConfig();
-        viewconfAnnot.views[0].initialXDomain = [xDomain[0] - 100000, xDomain[1] + 100000 ];
-        viewconfAnnot.views[1].initialXDomain = xDomain;
-        viewconfAnnot.views[1].tracks.top.forEach(track => {
-          if(track.type === "horizontal-transcripts"){
-            track.options["startCollapsed"] = true;
+    this.setState(
+      {
+        activeTab: target,
+      },
+      () => {
+        if (target === "cohort") {
+          const hgc = this.higlassContainerCohort.current.getHiGlassComponent();
+          if (hgc) {
+            // Force Higlass to repaint. Without this the tracks can be positioned incorrectly
+            hgc.boundRefreshView();
           }
-        })
-        // let firstViewLoc = hgc.api.getLocation(viewconfCohort.views[0].uid);
-        // hga.api.zoomTo(
-        //   viewconfAnnot.views[1].uid,
-        //   firstViewLoc["xDomain"][0],
-        //   firstViewLoc["xDomain"][1],
-        //   firstViewLoc["yDomain"][0],
-        //   firstViewLoc["yDomain"][1]
-        // );
-        hga.api.setViewConfig(viewconfAnnot).then(() => {
-          hga.boundRefreshView();
-          //hga.forceRefreshView();
-          this.setState(this.state);
-        });
+        } else {
+          const hga = this.higlassContainerAnnotation.current.getHiGlassComponent();
+          const hgc = this.higlassContainerCohort.current.getHiGlassComponent();
+          if (hga && hgc) {
+            // We should be using the hga.api.zoomTo(...) functionality, but for some reason
+            // it is not working. Modify and set the view conf instead.
+            const viewconfCohort = hgc.api.getViewConfig();
+            const xDomain = viewconfCohort.views[0].initialXDomain;
+            const viewconfAnnot = hga.api.getViewConfig();
+            const xDomainPadding = 100000;
+            viewconfAnnot.views[0].initialXDomain = [
+              xDomain[0] - xDomainPadding,
+              xDomain[1] + xDomainPadding,
+            ];
+            viewconfAnnot.views[1].initialXDomain = xDomain;
+            viewconfAnnot.views[1].tracks.top.forEach((track) => {
+              if (track.type === "horizontal-transcripts") {
+                track.options["startCollapsed"] = true;
+              }
+            });
+            hga.api.setViewConfig(viewconfAnnot).then(() => {
+              // Force Higlass to repaint. Without this the tracks can be positioned incorrectly
+              // Still does not seem enough sometimes...
+              hga.boundRefreshView();
+              this.setState(this.state);
+            });
+          }
+        }
       }
-    });
+    );
   }
-
-
 
   render() {
     const variantPositionAbsCoord = 20000;
     return (
-    <div>
-      <ul className="nav nav-tabs" role="tablist">
-        <li className="nav-item">
-          <a className={this.state.activeTab === "cohort" ? "nav-link active" : "nav-link"}  onClick={(evt) => this.handleTabClick(evt, "cohort")}  href="#">Cohort Browser</a>
-        </li>
-        <li className="nav-item">
-          <a className={this.state.activeTab === "annotation" ? "nav-link active" : "nav-link"} onClick={(evt) => this.handleTabClick(evt, "annotation")}  href="#">Annotation Browser</a>
-        </li>
-      </ul>
-      <div className="tab-content" id="cohortTabContent">
-        <div className={this.state.activeTab === "cohort" ? "tab-pane fade show active" : "tab-pane fade"} role="tabpanel">
-          <div className="row mt-3">
-            <div className="col-12">
-              <GeneSearchBar higlassContainer={this.higlassContainerCohort} />
+      <div>
+        <ul className="nav nav-tabs" role="tablist">
+          <li className="nav-item" key="cohort">
+            <a
+              className={
+                this.state.activeTab === "cohort"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+              onClick={(evt) => this.handleTabClick(evt, "cohort")}
+              href="#"
+            >
+              Cohort Browser
+            </a>
+          </li>
+          <li className="nav-item" key="annotation">
+            <a
+              className={
+                this.state.activeTab === "annotation" ? "nav-link active" : "nav-link"
+              }
+              onClick={(evt) => this.handleTabClick(evt, "annotation")}
+              href="#"
+            >
+              Annotation Browser
+            </a>
+          </li>
+        </ul>
+        <div className="tab-content" id="cohortTabContent">
+          <div
+            className={
+              this.state.activeTab === "cohort" ? "tab-pane fade show active" : "tab-pane fade"
+            }
+            role="tabpanel"
+          >
+            <div className="row mt-3">
+              <div className="col-12">
+                <GeneSearchBar higlassContainer={this.higlassContainerCohort} />
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <HiGlassAjaxLoadContainer
+                  cohortVcfLocation={this.cohortVcfLocation}
+                  cohortDensityBwLocation={this.cohortDensityBwLocation}
+                  ref={this.higlassContainerCohort}
+                  requestingTab="cohort"
+                />
+              </div>
             </div>
           </div>
-          <div className="row">
-            <div className="col-12">
-              <HiGlassAjaxLoadContainer
-                cohortVcfLocation={this.cohortVcfLocation}
-                cohortDensityBwLocation={this.cohortDensityBwLocation}
-                ref={this.higlassContainerCohort}
-                requestingTab="cohort"
-              />
-            </div>
-          </div>
-        </div>
-        <div className={this.state.activeTab === "annotation" ? "tab-pane fade show active" : "tab-pane fade"} role="tabpanel">
-          <div className="row mt-3">
-            <div className="col-12">
-            <HiGlassAjaxLoadContainer 
-              variantPositionAbsCoord={variantPositionAbsCoord} 
-              ref={this.higlassContainerAnnotation} 
-              requestingTab="annotation" />
+          <div
+            className={
+              this.state.activeTab === "annotation" ? "tab-pane fade show active" : "tab-pane fade"
+            }
+            role="tabpanel"
+          >
+            <div className="row mt-3">
+              <div className="col-12">
+                <HiGlassAjaxLoadContainer
+                  variantPositionAbsCoord={variantPositionAbsCoord}
+                  ref={this.higlassContainerAnnotation}
+                  requestingTab="annotation"
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-      
-    </div>
-  );
+    );
   }
 }
 
@@ -277,12 +345,11 @@ export const EmbeddedCohortBrowser = React.memo(function EmbeddedCohortBrowser(
   const higlassContainerAnnotation = useRef(null);
 
   return (
-    <EmbeddedCohortBrowserUnit 
-      cohortVcfLocation={cohortVcfLocation} 
-      cohortDensityBwLocation={cohortDensityBwLocation} 
+    <EmbeddedCohortBrowserComponent
+      cohortVcfLocation={cohortVcfLocation}
+      cohortDensityBwLocation={cohortDensityBwLocation}
       higlassContainerCohort={higlassContainerCohort}
-      higlassContainerAnnotation={higlassContainerAnnotation} />
+      higlassContainerAnnotation={higlassContainerAnnotation}
+    />
   );
 });
-
-
