a53625c52db6740f88c3e737cfe88beb4ad54fcc
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2022-03-03 10:00:37 -0500

    styling fix for interpretation tab getting pushed

--- src/encoded/static/components/item-pages/VariantSampleView/VariantSampleOverview.js
+++ src/encoded/static/components/item-pages/VariantSampleView/VariantSampleOverview.js
@@ -513,7 +513,7 @@ class InterpretationController extends React.PureComponent {
                     </div>
                 </Collapse>
                 <div className="row flex-column-reverse flex-lg-row flex-nowrap">
-                    <div className="col">
+                    <div className={`${showInterpretation || showInterpretationSpace ? "sv-snv-annotation": ""} col`}>
                         {/* Annotation Space passed as child */}
                         { children }
                     </div>
