e9b73ea81dc5e139a06c333aeb51112611c854ab
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2022-04-12 06:57:09 -0400

    Uncomment test

--- src/encoded/tests/test_types_variant_sample.py
+++ src/encoded/tests/test_types_variant_sample.py
@@ -87,18 +87,18 @@ def test_variant_sample_proband_inheritance(bgm_user_testapp, bgm_y_variant_samp
     ])
 
 
-# @pytest.mark.integrated  # uses s3
-# def test_bam_snapshot_download(workbook, es_testapp, test_variant_sample):
-#     """ Tests that we can correctly download an IGV image from the wfoutput bucket. """
-#     test_variant_sample['file'] += '2'
-#     res = es_testapp.post_json(VARIANT_SAMPLE_URL, test_variant_sample, status=[201, 409]).json
-#     uuid = res['@graph'][0]['uuid']
-#     bam_snapshot_location = res['@graph'][0]['bam_snapshot']
-#     assert bam_snapshot_location == test_variant_sample['file'] + '/bamsnap/chr1_12125898.png'
-#     download = es_testapp.get('/' + uuid + '/@@download').location
-#     # download location is https://test-wfout-bucket.s3.amazonaws.com/dummy-file-name2/bamsnap/chr1_12125898.png
-#     resp = requests.get(download)
-#     assert 'hello world' in resp.content.decode('utf-8')
+@pytest.mark.integrated  # uses s3
+def test_bam_snapshot_download(workbook, es_testapp, test_variant_sample):
+    """ Tests that we can correctly download an IGV image from the wfoutput bucket. """
+    test_variant_sample['file'] += '2'
+    res = es_testapp.post_json(VARIANT_SAMPLE_URL, test_variant_sample, status=[201, 409]).json
+    uuid = res['@graph'][0]['uuid']
+    bam_snapshot_location = res['@graph'][0]['bam_snapshot']
+    assert bam_snapshot_location == test_variant_sample['file'] + '/bamsnap/chr1_12125898.png'
+    download = es_testapp.get('/' + uuid + '/@@download').location
+    # download location is https://test-wfout-bucket.s3.amazonaws.com/dummy-file-name2/bamsnap/chr1_12125898.png
+    resp = requests.get(download)
+    assert 'hello world' in resp.content.decode('utf-8')
 
 
 def test_bam_snapshot_presence(
