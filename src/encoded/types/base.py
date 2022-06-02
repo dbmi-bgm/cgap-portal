72754007d0dbc5faf41f7e8219d0ea6b5dd013bf
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2022-06-02 17:07:23 -0400

    Update STATUS_ACL for the archived status

--- src/encoded/types/base.py
+++ src/encoded/types/base.py
@@ -254,6 +254,7 @@ class Item(snovault.Item):
         'in review': ALLOW_PROJECT_MEMBER_EDIT_ACL,
         'uploaded': ALLOW_PROJECT_MEMBER_EDIT_ACL,
         'uploading': ALLOW_PROJECT_MEMBER_EDIT_ACL,
+        'archived': ALLOW_PROJECT_MEMBER_VIEW_ACL,
         'deleted': DELETED_ACL,
         'replaced': ONLY_ADMIN_VIEW_ACL,
         # Everyone can view - restricted to specific items via schemas.
