dfa8a91f50d1d6a7010a1f5ddf3a7548f803c941
Kent Pitman <kent_pitman@hms.harvard.edu>
2022-07-21 11:53:29 -0400

    Make sure IDENTITY is always set to the cgap-devtest application server identity when running tests.

--- conftest.py
+++ conftest.py
@@ -29,33 +29,37 @@ def pytest_configure():
     tempfile.tempdir = '/tmp'
 
 
+PRINT("=" * 80)
+PRINT("Configuring environment variables...")
+
 my_selected_account = os.environ.get("ACCOUNT_NUMBER")
+
+# TODO: Maybe make this test programmable in env_utils sometime. -kmp 21-Jul-2022
+desired_env = 'cgap-devtest'
+
+my_selected_env = os.environ.get("ENV_NAME")
 
 if not my_selected_account or my_selected_account == "643366669028":
     PRINT("The legacy account can no longer be used for testing cgap-portal.")
     exit(1)
-
-desired_env = 'cgap-devtest'
-
-my_selected_env = os.environ.get("ENV_NAME")
-# TODO: Maybe make this test programmable in env_utils sometime. -kmp 21-Jul-2022
-if not my_selected_env:
+elif not my_selected_env:
     print("ENV_NAME was not set. It is being set to ")
     os.environ['ENV_NAME'] = desired_env
 elif my_selected_env != desired_env:
     PRINT(f"ENV_NAME must be set to {desired_env} (or left unset) for testing. (It is set to {my_selected_env}.)")
     exit(1)
+else:
+    PRINT(f"Leaving ENV_NAME set to {desired_env}.")
 
 old_identity = os.environ.get("IDENTITY")
 new_identity = 'C4DatastoreCgapDevtestApplicationConfiguration'
-if old_identity and old_identity != new_identity:
+if old_identity == new_identity:
+    PRINT(f"IDENTITY is already set to the desired value ({new_identity}). That value will be used.")
+elif old_identity:
     PRINT(f"IDENTITY is set incompatibly for ENV_NAME={desired_env}.")
     exit(1)
-
-my_aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
-
-if my_aws_access_key_id.startswith("ASIA"):
-    # When short-term federated credentials are in play, need to set up for assuming identity
-    PRINT(f"Because you have short-term credentials ({my_aws_access_key_id}),"
-          f" the IDENTITY environment variable is being set to {new_identity} so you can assume its credentials.")
+else:
+    PRINT(f"The IDENTITY environment variable is being set to {new_identity} so you can assume its credentials.")
     os.environ['IDENTITY'] = new_identity
+
+PRINT("=" * 80)
