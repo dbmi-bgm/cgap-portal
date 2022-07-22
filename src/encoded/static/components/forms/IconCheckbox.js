b15887d5aeef017bc9084e47cc1bdcad1e129b06
Alex Balashov <alex.balashov@gmail.com>
2022-07-22 14:31:18 -0400

    Quick fix/touchup

--- src/encoded/static/components/forms/IconCheckbox.js
+++ src/encoded/static/components/forms/IconCheckbox.js
@@ -5,15 +5,17 @@ import { console } from '@hms-dbmi-bgm/shared-portal-components/es/components/ut
 /** Must be a controlled checkbox */
 export function IconCheckbox (props) {
     const {
+        // Could also be a radio input, if "name" prop also passed in.
         type = "checkbox",
         checked = false,
         disabled = false,
         className = "mx-0 my-0 d-inline-block",
         iconCommonClassName = "",
         iconOn = "check-square fas",
-        iconOff= "square far",
+        iconOff = "square far",
         iconOnColor = "primary",
         iconOffColor = "secondary",
+        // passProps should include onChange, name, and any other input-element-specific props.
         ...passProps
     } = props;
 
@@ -26,7 +28,7 @@ export function IconCheckbox (props) {
 
     return (
         <label className={className}>
-            <input {...passProps} {...{ type }} className="d-none" />
+            <input {...passProps} {...{ type, checked, disabled }} className="d-none" />
             <i className={iconCls}/>
         </label>
     );
