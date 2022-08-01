bd4c3fa5e1c1595714c02bd2822c4e2dfbb9f6f3
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2022-08-01 13:52:24 -0400

    Fixes for weird spacing in emails

--- src/encoded/static/components/item-pages/components/FeedbackButton.js
+++ src/encoded/static/components/item-pages/components/FeedbackButton.js
@@ -2,47 +2,35 @@ import React, { useMemo, useCallback } from 'react';
 import _ from 'underscore';
 
 import Dropdown from 'react-bootstrap/esm/Dropdown';
-import DropdownButton from 'react-bootstrap/esm/DropdownButton';
-import DropdownItem from 'react-bootstrap/esm/DropdownItem';
 
 const browser = "Chrome";
 const platform = "Desktop";
 const operatingSystem = "MACOS";
 
-const SPECIFICATIONS = `Specifications%3A%0D%0A\
-    Browser%3A%20${browser}%0D%0A\
-    Platform%3A%20${platform}%0D%0A\
-    Operating%20System%3A%20${operatingSystem}%0D%0A`
+const SPECIFICATIONS = `Browser%3A%20${browser}%0D%0A\
+Platform%3A%20${platform}%0D%0A\
+Operating%20System%3A%20${operatingSystem}%0D%0A`;
 
-const QUESTION_MAILTO = `\
-    mailto:cgap-support@hms-dbmi.atlassian.net?\
-        subject=CGAP%20Question\
-        &body=Feedback%20\
-                Type%3A%20Question%0D%0A%0D%0A\
-                Question%3A%0D%0A%0D%0A%0D%0A\
-                ${SPECIFICATIONS}
-
-`;
+const QUESTION_MAILTO = `mailto:cgap-support@hms-dbmi.atlassian.net?\
+subject=CGAP%20Question\
+&body=Feedback%20\
+Type%3A%20Question%0D%0A%0D%0A\
+Question%3A%0D%0A%0D%0A%0D%0A${SPECIFICATIONS}`;
 
 const BUG_REPORT_MAILTO = `\
-    mailto:cgap-support@hms-dbmi.atlassian.net?\
-        subject=CGAP%20Issue\
-        &body=Feedback%20\
-                Type%3A%20Issue%2FBug%20Report%0D%0A%0D%0A\
-                Expected%20Behavior%3A%0D%0A%0D%0A%0D%0A\
-                Actual%20Behavior%3A%0D%0A%0D%0A%0D%0A\
-                Steps%20to%20Reproduce%20the%20Problem%3A%0D%0A%0D%0A%0D%0A\
-                ${SPECIFICATIONS}
-`;
+mailto:cgap-support@hms-dbmi.atlassian.net?\
+subject=CGAP%20Issue\
+&body=Feedback%20\
+Type%3A%20Issue%2FBug%20Report%0D%0A%0D%0A\
+Expected%20Behavior%3A%0D%0A%0D%0A%0D%0A\
+Actual%20Behavior%3A%0D%0A%0D%0A%0D%0A\
+Steps%20to%20Reproduce%20the%20Problem%3A%0D%0A%0D%0A%0D%0A${SPECIFICATIONS}`;
 
-const FEEDBACK_MAILTO = `\
-    mailto:cgap-support@hms-dbmi.atlassian.net?\
-        subject=CGAP%20Feedback\
-        &body=Feedback%20\
-                Type%3A%20General%20Feedback%0D%0A%0D%0A\
-                Comments%3A%0D%0A%0D%0A%0D%0A\
-                ${SPECIFICATIONS}
-`;
+const FEEDBACK_MAILTO = `mailto:cgap-support@hms-dbmi.atlassian.net?\
+subject=CGAP%20Feedback\
+&body=Feedback%20\
+Type%3A%20General%20Feedback%0D%0A%0D%0A\
+Comments%3A%0D%0A%0D%0A%0D%0A${SPECIFICATIONS}`;
 
 export default function FeedbackButton (props) {
     return (
