190bce6d269582245b03b89437e842bc3de8ba93
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2022-09-22 12:20:11 -0400

    Add CohortAnalysis display title

--- src/encoded/types/cohort_analysis.py
+++ src/encoded/types/cohort_analysis.py
@@ -1,4 +1,4 @@
-from snovault import collection, load_schema
+from snovault import calculated_property, collection, load_schema
 
 from .base import Item
 
@@ -14,3 +14,15 @@ class CohortAnalysis(Item):
     item_type = "cohort_analysis"
     schema = load_schema("encoded:schemas/cohort_analysis.json")
     embedded_list = []
+
+    @calculated_property(schema={
+        "title": "Display Title",
+        "description": "Cohort analysis display title",
+        "type": "string"
+    })
+    def display_title(self, accession, title=None):
+        if title:
+            result = f"{title} ({accession})"
+        else:
+            result = accession
+        return result
