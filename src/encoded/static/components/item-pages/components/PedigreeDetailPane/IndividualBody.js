9ea4f41da902ab2bbbeb3a301c8ba477879d742e
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2022-10-14 13:13:12 -0400

    Capitalize each disease list item

--- src/encoded/static/components/item-pages/components/PedigreeDetailPane/IndividualBody.js
+++ src/encoded/static/components/item-pages/components/PedigreeDetailPane/IndividualBody.js
@@ -254,7 +254,7 @@ function DiseaseListItem(props) {
     return (
         <div className="detail-row-list-item disease" key={featureID}>
             <div className="legend-patch" data-disease-index={diseaseIndex} />
-            <span className="title text-truncate"><a href={featureID}>{ title }</a></span>
+            <span className="title text-truncate text-capitalize"><a href={featureID}>{ title }</a></span>
             { onsetAge !== null && onsetAgeUnits !== null ? (
                 <span className="onset" data-tip="Age of onset">
                     <small> @ </small>
