36d4f4ff60d50cfcb07402a66dc8e5da0aea77c3
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2022-12-08 17:19:40 -0500

    Update footer with github and youtube links

--- src/encoded/static/components/Footer.js
+++ src/encoded/static/components/Footer.js
@@ -12,40 +12,23 @@ export const Footer = React.memo(function Footer(){
     return (
         <footer id="page-footer">
             <div className="page-footer px-4">
-                {/* <div className="container"> */}
                 <div className="row">
                     <div className="col-12 col-xl-6">
-                        <div className="footer-section copy-notice d-flex align-items-center">
-                            <div>
-                                Harvard Medical School
+                        <div className="footer-section copy-notice d-flex align-items-center justify-content-between">
+                            <div className="d-flex align-items-center">
+                                Harvard Medical School <span className="px-2">|</span> Brigham &amp; Women&apos;s Hospital
                             </div>
-                            <div className="px-2">
-                                |
-                            </div>
-                            <div>
-                                {"Brigham & Women's Hospital" }
+                            <div className="text-larger">
+                                <a href="https://www.youtube.com/@cgaptraining" target="_blank" rel="noreferrer">
+                                    <i className="icon icon-youtube fab mr-1"></i>
+                                </a>
+                                <a href="https://www.github.com/dbmi-bgm/" target="_blank" rel="noreferrer">
+                                    <i className="icon icon-github fab"></i>
+                                </a>
                             </div>
                         </div>
                     </div>
-                    {/*
-                    <div className="col-12 col-md-6">
-                        <div className="footer-section copy-notice">
-                            <a href="http://dcic.4dnucleome.org" target="_blank" rel="noopener noreferrer" className="text-400">
-                                4D Nucleome Data Coordination and Integration Center
-                            </a>
-                        </div>
-                    </div>
-
-                    <div className="col-12 col-md-6">
-                        <div className="footer-section text-500">
-                            <a href="http://www.harvard.edu/" target="_blank" rel="noopener noreferrer">Harvard University</a>
-                            <span> &nbsp;|&nbsp; </span>
-                            <a href="http://www.mit.edu/" target="_blank" rel="noopener noreferrer">Massachusetts Institute of Technology</a>
-                        </div>
-                    </div>
-                    */}
                 </div>
-                {/* </div> */}
             </div>
         </footer>
     );
