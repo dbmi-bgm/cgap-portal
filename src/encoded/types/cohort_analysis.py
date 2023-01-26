fe52af0aa118cd3b5d966e5f3b08cc36383f853d
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2023-01-26 14:11:53 -0500

    Add calcprop for all samples in analysis

--- src/encoded/types/cohort_analysis.py
+++ src/encoded/types/cohort_analysis.py
@@ -1,3 +1,4 @@
+from typing import Optional, Sequence
 from snovault import calculated_property, collection, load_schema
 
 from .analysis import AbstractAnalysis
@@ -19,3 +20,26 @@ class CohortAnalysis(AbstractAnalysis):
     name_key = "accession"
     schema = load_schema("encoded:schemas/cohort_analysis.json")
     embedded_list = _build_cohort_analysis_embedded_list()
+
+    @calculated_property(
+        schema={
+            "title": "Samples",
+            "description": "The samples used in the analysis",
+            "type": "array",
+            "items": {
+                "type": "string",
+                "linkTo": "Sample",
+            },
+        }
+    )
+    def samples(
+        self,
+        case_samples: Optional[Sequence[str]] = None,
+        control_samples: Optional[Sequence[str]] = None
+    ):
+        """Collects all samples used in the analysis"""
+        if case_samples or control_samples:
+            result = []
+            result.extend(case_samples or [])
+            result.extend(control_samples or [])
+            return result
