824b4447d968200d6e8158302b1c82cedc81f722
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2023-01-26 15:15:31 -0500

    Add abstract analysis embedding list

--- src/encoded/types/analysis.py
+++ src/encoded/types/analysis.py
@@ -1,14 +1,23 @@
 from abc import ABC
+from typing import List, Optional, Union
 
 from snovault import calculated_property, display_title_schema
 
 from .base import Item
 
 
+def _build_abstract_analysis_embedded_list() -> List[str]:
+    return []
+
+
 class AbstractAnalysis(ABC, Item):
 
+    embedded_list = _build_abstract_analysis_embedded_list()
+
     @calculated_property(schema=display_title_schema)
-    def display_title(self, accession, external_identifier=None):
+    def display_title(
+        self, accession: str, external_identifier: Optional[str] = None
+    ) -> Union[str, None]:
         if external_identifier:
             result = external_identifier
         else:
