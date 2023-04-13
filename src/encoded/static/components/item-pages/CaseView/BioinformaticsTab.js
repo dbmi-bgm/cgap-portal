9bcdf3a402ecb22526b1923725e7531731ed6e67
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2023-04-13 13:14:34 -0400

    Move BiostatsEntry to QCM file

--- src/encoded/static/components/item-pages/CaseView/BioinformaticsTab.js
+++ src/encoded/static/components/item-pages/CaseView/BioinformaticsTab.js
@@ -7,8 +7,7 @@ import { decorateNumberWithCommas } from '@hms-dbmi-bgm/shared-portal-components
 
 import { CaseSummaryTable } from './CaseSummaryTable';
 import { flagToBootstrapClass, generateRelationshipMapping, mapLongFormSexToLetter, sortAndAddRolePropsToQCMs } from '../../util/item';
-import QuickPopover from '../components/QuickPopover';
-import { QCMFlag } from '../components/QCM';
+import { BioinfoStatsEntry, QCMFlag } from '../components/QCM';
 
 
 export const BioinformaticsTab = React.memo(function BioinformaticsTab(props) {
@@ -195,25 +194,6 @@ function BioinfoStatTable({ qualityControlMetrics }) {
                 </BioinfoStatsEntry>
             </div>
         </React.Fragment>
-    );
-}
-
-function BioinfoStatsEntry({ tooltip, label, children, popoverContent = null }) {
-    const id = "biostatsentry_" + label.split(" ").join("_");
-    return (
-        <div className="col-12 col-md-6 col-lg-3 col-xl-3 py-2">
-            <div className="qc-summary">
-                <label className="d-inline mb-0" htmlFor={id}>
-                    {label}:
-                    {!popoverContent && tooltip ?
-                        <i className="icon icon-info-circle fas icon-fw ml-05"
-                            data-tip={tooltip} data-place="right" />
-                        : null}
-                </label>
-                {popoverContent ? <QuickPopover popID={label} tooltip={tooltip || "Click for more info"} className="p-0 ml-05">{popoverContent}</QuickPopover> : null}
-                <div {...{ id }}>{children}</div>
-            </div>
-        </div>
     );
 }
 
--- src/encoded/static/components/item-pages/components/QCM.js
+++ src/encoded/static/components/item-pages/components/QCM.js
@@ -5,6 +5,7 @@ import React, { useState } from 'react';
 import { capitalize } from '@hms-dbmi-bgm/shared-portal-components/es/components/util/value-transforms';
 
 import { flagToBootstrapClass } from '../../util/item';
+import QuickPopover from './QuickPopover';
 
 export function QCMFlag({ type, title, cls = "m-0 ml-1" }) {
     if (!title || !type) return null;
@@ -39,4 +40,33 @@ function qcmFieldNameToDisplay(field = "") {
         default:
             return field.split("_").map((word) => capitalize(word)).join(" ");
     }
+}
+
+/**
+ * Snippet for showing a single row in a BioinfoStats table (used in Bioinformatics tabs for displaying
+ * quality control metrics)
+ * @param {string} tooltip - Tooltip data (typically from schema) to display (if blank, no tooltip shown unless popoverContent present)
+ * @param {string} label - What quality metric/stat is being displayed
+ * @param {JSX} children - Value to display
+ * @param {string | JSX} popoverContent - What to display in the popover (if blank, no popover icon is shown)
+ * @returns JSX
+ */
+export function BioinfoStatsEntry(props) {
+    const { tooltip, label = "", children, popoverContent = null } = props;
+    const id = "biostatsentry_" + label.split(" ").join("_");
+    return (
+        <div className="col-12 col-md-6 col-lg-3 col-xl-3 py-2">
+            <div className="qc-summary">
+                <label className="d-inline mb-0" htmlFor={id}>
+                    {label && <>{label}:</>}
+                    {!popoverContent && tooltip ?
+                        <i className="icon icon-info-circle fas icon-fw ml-05"
+                            data-tip={tooltip} data-place="right" />
+                        : null}
+                </label>
+                {popoverContent ? <QuickPopover popID={label} tooltip={tooltip || "Click for more info"} className="p-0 ml-05">{popoverContent}</QuickPopover> : null}
+                <div {...{ id }}>{children}</div>
+            </div>
+        </div>
+    );
 }
