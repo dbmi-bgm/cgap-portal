d91549f081eb785f2f3f1427135439aecf70a6b5
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2023-04-14 11:20:28 -0400

    Add some new nav item stuff

---
+++ src/encoded/static/components/navigation/components/BigDropdown/BigDropdownBigLink.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export function BigDropdownBigLink(props){
+    const {
+        children,
+        titleIcon = null,
+        className = null,
+        isActive = false,
+        ...passProps // Contains: `href`, `rel`, `onClick`, etc.
+    } = props;
+
+    if (!children) return null;
+
+    const textCol = (
+        <div className="col">
+            { children }
+        </div>
+    );
+
+    let iconCol = null;
+
+    if (typeof titleIcon === "string") {
+        iconCol = (
+            <div className="col-auto icon-beside-column text-right">
+                <i className={"icon icon-fw icon-2x icon-" + titleIcon}/>
+            </div>
+        );
+    } else if (React.isValidElement(titleIcon)){
+        iconCol = (
+            <div className="col-auto icon-beside-column text-right">
+                { titleIcon }
+            </div>
+        );
+    }
+
+    return (
+        <a {...passProps} className={"big-link" + (className? " " + className : "") + (isActive? " active" : "")}>
+            <div className="row align-items-center">
+                { iconCol }
+                { textCol }
+            </div>
+        </a>
+    );
+}
