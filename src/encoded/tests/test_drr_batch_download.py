c390fb3d923a265ca73e788b7b922b4955d45bd4
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2023-04-24 10:51:13 -0400

    Add test for form POSTs to case spreadsheet

--- src/encoded/tests/test_drr_batch_download.py
+++ src/encoded/tests/test_drr_batch_download.py
@@ -12,6 +12,7 @@ from ..drr_batch_download import (
     CASE_SPREADSHEET_URL,
     SpreadsheetPost,
 )
+from ..util import APPLICATION_FORM_ENCODED_MIME_TYPE
 
 
 def test_case_search_spreadsheet(es_testapp: TestApp, workbook: None) -> None:
@@ -25,6 +26,24 @@ def test_case_search_spreadsheet(es_testapp: TestApp, workbook: None) -> None:
     response = es_testapp.post_json(
         CASE_SPREADSHEET_URL,
         post_body,
+        status=200,
+    )
+    rows = response.body.decode().split("\n")
+    assert rows
+
+
+def test_case_search_spreadsheet_form_post(html_es_testapp: TestApp, workbook: None) -> None:
+    case_search_compound_filterset = (
+        {
+            "search_type": "Case",
+            "global_flags": "case_id=CASE10254-S1-C1",
+        }
+    )
+    post_body = {"compound_search_request": json.dumps(case_search_compound_filterset)}
+    response = html_es_testapp.post(
+        CASE_SPREADSHEET_URL,
+        post_body,
+        content_type=APPLICATION_FORM_ENCODED_MIME_TYPE,
         status=200,
     )
     rows = response.body.decode().split("\n")
