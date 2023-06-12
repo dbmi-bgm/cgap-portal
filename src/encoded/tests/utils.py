3d20bda96785b82440d37cdf2652504af5b12884
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2023-06-12 13:22:43 -0400

    Reformat with black

--- src/encoded/tests/utils.py
+++ src/encoded/tests/utils.py
@@ -5,29 +5,31 @@ from webtest.app import TestApp
 
 
 def pluralize(name):
-    name = name.replace('_', '-')
+    name = name.replace("_", "-")
     # deal with a few special cases explicitly
-    specials = ['file', 'quality-metric', 'summary-statistic', 'workflow-run', 'note']
+    specials = ["file", "quality-metric", "summary-statistic", "workflow-run", "note"]
     for sp in specials:
-        if name.startswith(sp) and re.search('-(set|flag|format|type)', name) is None:
-            return name.replace(sp, sp + 's')
-        elif name.startswith(sp) and re.search('setting', name):
-            return name.replace(sp, sp + 's')
+        if name.startswith(sp) and re.search("-(set|flag|format|type)", name) is None:
+            return name.replace(sp, sp + "s")
+        elif name.startswith(sp) and re.search("setting", name):
+            return name.replace(sp, sp + "s")
     # otherwise just add 's/es/ies'
-    if name.endswith('ly'):
-        return name[:-1] + 'ies'
-    if name.endswith('sis'):
-        return name[:-2] + 'es'
-    if name.endswith('s'):
-        return name + 'es'
-    return name + 's'
+    if name.endswith("ly"):
+        return name[:-1] + "ies"
+    if name.endswith("sis"):
+        return name[:-2] + "es"
+    if name.endswith("s"):
+        return name + "es"
+    return name + "s"
 
 
 def make_atid(uuid, item_type="sample-processing"):
     return f"/{pluralize(item_type)}/{uuid}/"
 
 
-def get_identifier(testapp: TestApp, identifier: str, frame: str = "object") -> Dict[str, Any]:
+def get_identifier(
+    testapp: TestApp, identifier: str, frame: str = "object"
+) -> Dict[str, Any]:
     identifier_path = get_identifier_path(identifier, frame=frame)
     response = testapp.get(identifier_path, status=[200, 301])
     if response.status_code == 200:
