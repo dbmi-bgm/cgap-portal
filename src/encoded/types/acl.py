0ba1ac45fcbf3e7f1174143381fa6d291e08093d
David Michaels <david_michaels@hms.harvard.edu>
2023-06-13 11:35:45 -0400

    C4-1019: continue ingestion-to-snovault migration work

---
+++ src/encoded/types/acl.py
@@ -0,0 +1,40 @@
+# Considation of ACL related definitions.
+from pyramid.security import Allow, Authenticated, Deny, Everyone
+from snovault.types.acl import Acl, DELETED_ACL, ONLY_ADMIN_VIEW_ACL
+
+""" This acl allows item creation; it should be overwritten with an empty
+    list in Item types a project member user should not be able to create
+    likely worthwhile to review and set it up in the opposite way as there
+    will probably be more items than a regular user shouldn't create
+    this gets added to the Collection class __init__
+"""
+PROJECT_MEMBER_CREATE_ACL: Acl = [
+    (Allow, 'group.project_editor', 'add'),
+    (Allow, 'group.project_editor', 'create'),
+]
+
+# this is for pages that should be visible to public
+ALLOW_EVERYONE_VIEW_ACL: Acl = [
+    (Allow, Everyone, 'view'),
+] + ONLY_ADMIN_VIEW_ACL + PROJECT_MEMBER_CREATE_ACL
+
+# view for shared items - add a status for common cgap items
+# not sure if we want project members to have create on these?
+ALLOW_AUTHENTICATED_VIEW_ACL: Acl = [
+    (Allow, Authenticated, 'view'),
+] + ONLY_ADMIN_VIEW_ACL + PROJECT_MEMBER_CREATE_ACL
+
+ALLOW_PROJECT_MEMBER_EDIT_ACL: Acl = [
+    (Allow, 'role.project_editor', ['view', 'edit']),
+] + ONLY_ADMIN_VIEW_ACL + PROJECT_MEMBER_CREATE_ACL
+
+ALLOW_PROJECT_MEMBER_VIEW_ACL: Acl = [
+    (Allow, 'role.project_editor', 'view'),
+] + ONLY_ADMIN_VIEW_ACL + PROJECT_MEMBER_CREATE_ACL
+
+ALLOW_PROJECT_MEMBER_ADD_ACL: Acl = PROJECT_MEMBER_CREATE_ACL
+
+# Used for 'draft' status
+ALLOW_OWNER_EDIT_ACL: Acl = [
+    (Allow, 'role.owner', ['view', 'edit']),
+] + ONLY_ADMIN_VIEW_ACL + PROJECT_MEMBER_CREATE_ACL
--- src/encoded/types/base.py
+++ src/encoded/types/base.py
@@ -41,59 +41,17 @@ from typing import Any, List, Tuple, Union
 from ..server_defaults import get_userid, add_last_modified
 
 
-Acl = List[Tuple[Any, Any, Union[str, List[str]]]]
-
-# Item acls
-# TODO (C4-332): consolidate all acls into one place - i.e. their own file
-ONLY_ADMIN_VIEW_ACL: Acl = [
-    (Allow, 'group.admin', ['view', 'edit']),
-    (Allow, 'group.read-only-admin', ['view']),
-    (Allow, 'remoteuser.INDEXER', ['view']),
-    (Allow, 'remoteuser.EMBED', ['view']),
-    (Deny, Everyone, ['view', 'edit'])
-]
-
-""" This acl allows item creation; it should be overwritten with an empty
-    list in Item types a project member user should not be able to create
-    likely worthwhile to review and set it up in the opposite way as there
-    will probably be more items than a regular user shouldn't create
-    this gets added to the Collection class __init__
-"""
-PROJECT_MEMBER_CREATE_ACL: Acl = [
-    (Allow, 'group.project_editor', 'add'),
-    (Allow, 'group.project_editor', 'create'),
-]
-
-# this is for pages that should be visible to public
-ALLOW_EVERYONE_VIEW_ACL: Acl = [
-    (Allow, Everyone, 'view'),
-] + ONLY_ADMIN_VIEW_ACL + PROJECT_MEMBER_CREATE_ACL
-
-# view for shared items - add a status for common cgap items
-# not sure if we want project members to have create on these?
-ALLOW_AUTHENTICATED_VIEW_ACL: Acl = [
-    (Allow, Authenticated, 'view'),
-] + ONLY_ADMIN_VIEW_ACL + PROJECT_MEMBER_CREATE_ACL
-
-ALLOW_PROJECT_MEMBER_EDIT_ACL: Acl = [
-    (Allow, 'role.project_editor', ['view', 'edit']),
-] + ONLY_ADMIN_VIEW_ACL + PROJECT_MEMBER_CREATE_ACL
-
-
-ALLOW_PROJECT_MEMBER_VIEW_ACL: Acl = [
-    (Allow, 'role.project_editor', 'view'),
-] + ONLY_ADMIN_VIEW_ACL + PROJECT_MEMBER_CREATE_ACL
-
-DELETED_ACL: Acl = [
-    (Deny, Everyone, 'visible_for_edit')
-] + ONLY_ADMIN_VIEW_ACL
-
-ALLOW_PROJECT_MEMBER_ADD_ACL: Acl = PROJECT_MEMBER_CREATE_ACL
-
-# Used for 'draft' status
-ALLOW_OWNER_EDIT: Acl = [
-    (Allow, 'role.owner', ['view', 'edit']),
-] + ONLY_ADMIN_VIEW_ACL + PROJECT_MEMBER_CREATE_ACL
+from .acl import (
+    ALLOW_AUTHENTICATED_VIEW_ACL,
+    ALLOW_EVERYONE_VIEW_ACL,
+    ALLOW_OWNER_EDIT_ACL,
+    ALLOW_PROJECT_MEMBER_ADD_ACL,
+    ALLOW_PROJECT_MEMBER_EDIT_ACL,
+    ALLOW_PROJECT_MEMBER_VIEW_ACL,
+    DELETED_ACL,
+    ONLY_ADMIN_VIEW_ACL,
+    PROJECT_MEMBER_CREATE_ACL
+)
 
 
 def get_item_or_none(request, value, itype=None, frame='object'):
@@ -260,7 +218,7 @@ class Item(snovault.Item):
         # Everyone can view - restricted to specific items via schemas.
         'public': ALLOW_EVERYONE_VIEW_ACL,
         # Only creator can view - restricted to specific items via schemas.
-        'draft': ALLOW_OWNER_EDIT
+        'draft': ALLOW_OWNER_EDIT_ACL
     }
     FACET_ORDER_OVERRIDE = {}  # empty by default
 
