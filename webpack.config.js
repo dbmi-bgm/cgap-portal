c43f04f2a59e000998677b0dd9409ad25204b203
Alexander Veit <53857412+alexander-veit@users.noreply.github.com>
2023-07-31 16:45:25 -0400

    Polyfill buffer

--- webpack.config.js
+++ webpack.config.js
@@ -110,7 +110,10 @@ const webPlugins = plugins.slice(0);
 const serverPlugins = plugins.slice(0);
 
 webPlugins.push(new webpack.ProvidePlugin({
-    "process": "process/browser"
+    "process": "process/browser",
+}));
+webPlugins.push(new webpack.ProvidePlugin({
+    Buffer: ['buffer', 'Buffer'],
 }));
 
 // Inform our React code of what build we're on.
@@ -227,7 +230,7 @@ module.exports = [
                 "zlib": false,
                 "stream": require.resolve("stream-browserify"),
                 "crypto": false,
-                "buffer": false,
+                "buffer": require.resolve('buffer/'),
                 "events": false,
                 // "path": require.resolve("path-browserify"),
                 "process": require.resolve("process/browser"),
