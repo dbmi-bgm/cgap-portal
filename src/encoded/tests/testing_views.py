b7b246094c881d8df1c93bd9a0723d09a57ab2d7
William Ronchetti <wrr33@cornell.edu>
2023-08-09 16:32:56 -0400

    fix other tests

--- src/encoded/tests/testing_views.py
+++ src/encoded/tests/testing_views.py
@@ -153,7 +153,7 @@ class TestingLinkTarget(Item):
         "type": "array",
         "items": {
             "type": ['string', 'object'],
-            "linkTo": "TestingLinkSourceSno",
+            "linkTo": "TestingLinkSource",
         },
     })
     def reverse(self, request):
