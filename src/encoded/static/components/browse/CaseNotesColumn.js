b56eb58750e5f9f023e1ffeccd3e5d01aa646d34
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2023-08-14 13:55:25 -0400

    feat: passing down props, adding state

--- src/encoded/static/components/browse/CaseNotesColumn.js
+++ src/encoded/static/components/browse/CaseNotesColumn.js
@@ -1,42 +1,71 @@
-import React, { useState, forwardRef } from "react";
+import React, { useState, useRef, useEffect, forwardRef } from "react";
 import { Popover, OverlayTrigger } from "react-bootstrap";
-
-const handleNoteTextChange = (e) => {
-  console.log(e.target.value);
-}
+import { LocalizedTime } from "@hms-dbmi-bgm/shared-portal-components/es/components/ui/LocalizedTime";
 
 
-const CaseNotesPopover = forwardRef((props, ref) => {
-  const [lastSavedText, setLastSavedText] = useState(props.note.note_text || "No text yet");
+const CaseNotesPopover = forwardRef(({
+  note,
+  lastSavedText,
+  setLastSavedText,
+  currentText,
+  setCurrentText,
+  ...popoverProps
+}, ref) => {
+  // Track whether changes have been made to the original text 
+  // (AKA text shown at the beginning of the session)
+  // 
+  // TODO: this does not need to rerender the component, since changes to lastSavedText will already do so.
+  // It will only occur once. Use a ref to keep this from running more than once?
+  // const [originalText, setButtonText] = useState("Save Note");
 
+
+  // Pull out information about previous 
+  const { date_text_edited, text_edited_by } = note.last_text_edited;
+  const prevDate = date_text_edited;
+  const prevEditor = text_edited_by.display_title;
+  
   return (
-    <Popover className="w-[400px]" placement="bottom" {...props} ref={ref}>
+    <Popover data-popover-category="notes" placement="bottom" {...popoverProps} ref={ref}>
       <Popover.Title as="h3">Case Notes</Popover.Title>
       <Popover.Content>
+        <p className="last-saved small">Last Saved: <LocalizedTime timestamp={prevDate} formatType="date-time-sm" /> by {prevEditor}</p>
         <textarea 
           className="form-control" 
           rows={5} 
-          defaultValue={props.note.note_text || ""}
-          onChange={(e) => handleNoteTextChange(e)}
+          defaultValue={currentText}
+          onChange={(e) => setCurrentText(e.target.value)}
         ></textarea>
-        <button type="button" className="btn btn-primary mr-04 w-100">
-            Save
+        <button 
+          type="button" 
+          className="btn btn-primary mr-04 w-100"
+          onClick={() => setLastSavedText(currentText) }
+          disabled={lastSavedText === currentText ? "disabled" : "" }
+        >
+          {lastSavedText === currentText ? "Note saved - edit note to save again" : "Save Note" }
         </button>
       </Popover.Content>
     </Popover>
   )
 });
 
-/**
- * 
- * @param {} param0 
- * @returns 
- */
-const CaseNotesButton = ({note, hasNote, isSaved, setIsSaved}) => (
-  <OverlayTrigger trigger="click" placement="bottom" overlay={<CaseNotesPopover note={note} />}>
+const CaseNotesButton = ({note, lastSavedText,setLastSavedText,currentText,setCurrentText}) => (
+  <OverlayTrigger 
+    trigger="click"  
+    placement="bottom"
+    rootClose
+    overlay={ // Pass Popover as overlay
+      <CaseNotesPopover
+        note={note}
+        lastSavedText={lastSavedText}
+        setLastSavedText={setLastSavedText}
+        currentText={currentText}
+        setCurrentText={setCurrentText}
+      />
+    }
+  >
     <button className="case-notes-button">
-      <i className={`icon text-larger icon-fw icon-sticky-note ${ hasNote ? 'fas' : 'far' } text-muted`}></i>
-      {!isSaved && <i
+      <i className={`icon text-larger icon-fw icon-sticky-note ${ lastSavedText === "" ? 'far' : 'fas' } text-muted`}></i>
+      {currentText !== lastSavedText && <i
         className="status-indicator-dot"
         data-status={"note-dot-unsaved"}
         data-tip={"Unsaved changes on this note."}
@@ -48,26 +77,27 @@ const CaseNotesButton = ({note, hasNote, isSaved, setIsSaved}) => (
 
 
 /**
+ * Top-level component.
  * @param {Object} result
  * @param {Object} result.note - the notes returned as a result
  */
 export const CaseNotesColumn = ({ note }) => {
-  const [hasNote, setHasNote] = useState(note != null) // is the note field defined?
-  const [isSaved, setIsSaved] = useState(false);
-
-  // const [noteText, setNoteText] = useState(note.note_text ?? "no notes");
+  const [lastSavedText, setLastSavedText] = useState(note == null ? "" : note.note_text);
+  const [currentText, setCurrentText] = useState(lastSavedText);
+  
+  console.log(note, note.last_text_edited);
   return (
     <div className="case-notes">
-      <CaseNotesButton note={note} hasNote={hasNote} isSaved={isSaved} setIsSaved={setIsSaved} />
-      <p className="case-notes-text">{note.note_text || ""}</p>
+      {/* Render Notes Button */}
+      <CaseNotesButton
+        note={note}
+        lastSavedText={lastSavedText}
+        setLastSavedText={setLastSavedText}
+        currentText={currentText}
+        setCurrentText={setCurrentText}
+      />
+      {/* Render either empty string or note.note_text */}
+      <p className="case-notes-text">{lastSavedText}</p> 
     </div>
   );
 };
-
-/**
- * - Saved empty string should be considered a non-comment
- * - There needs to be a way to pass information up to the CaseID column
- * 
- * 
- * 
- */
